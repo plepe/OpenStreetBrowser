a59e879fe6362be20eb4b2c98986f2ab672e6a23
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-07-09 08:45:49 +0000

    - Anpassungen zum neuen Test-Environment
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@16401 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ src/combine_cascadenik.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+$root_path=$ENV{ROOT_PATH};
+
+open(O, "python compile.py $root_path/render/global.mml|");
+while($r=<O>) {
+  if($r =~ /<Style /) {
+    $mode=1;
+  }
+  elsif($r =~ /<Layer /) {
+    $mode=2;
+  }
+
+  if($mode>0) {
+    $text[$mode].=$r;
+  }
+
+  if(($r =~ /<\/Style>/)||($r =~ /<\/Layer>/)) {
+    $mode=0;
+  }
+}
+close(O);
+
+open(O, "<osm-publ.mml");
+while($r=<O>) {
+  if($r eq "INSERTSTYLE\n") {
+    print $text[1];
+  }
+  elsif($r eq "INSERTLAYER\n") {
+    print $text[2];
+  }
+  else {
+    print $r;
+  }
+}
+close(O);
