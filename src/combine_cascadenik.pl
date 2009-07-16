401c206c0531eb1645185b8b3773a75f618e6656
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-07-16 08:16:04 +0000

    Fix
    - Somehow there was the wrong file in the tree
    
    
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@16530 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/combine_cascadenik.pl
+++ src/combine_cascadenik.pl
@@ -2,35 +2,80 @@
 
 $root_path=$ENV{ROOT_PATH};
 
-open(O, "python compile.py $root_path/render/routes.mml|");
-while($r=<O>) {
-  if($r =~ /<Style /) {
-    $mode=1;
-  }
-  elsif($r =~ /<Layer /) {
-    $mode=2;
-  }
-
-  if($mode>0) {
-    $text[$mode].=$r;
-  }
-
-  if(($r =~ /<\/Style>/)||($r =~ /<\/Layer>/)) {
-    $mode=0;
-  }
-}
-close(O);
-
-open(O, "<$root_path/render/global.mml");
-while($r=<O>) {
-  if($r eq "INSERTSTYLE\n") {
-    print $text[1];
-  }
-  elsif($r eq "INSERTLAYER\n") {
-    print $text[2];
+$full_text="";
+open(OB, "<$root_path/render/base.mml");
+while($rb=<OB>) {
+  if($rb =~ "INSERTLAYER ([a-zA-Z0-9_]+)\n") {
+    @text=get_mss_mll($1);
+    $full_text.=$text[1];
+    $full_text.=$text[2];
   }
   else {
-    print $r;
+    $full_text.=$rb;
   }
 }
-close(O);
+close(OB);
+
+$full_text=~s/__DBNAME__/$ENV{DB_NAME}/g;
+print $full_text;
+exit;
+
+sub get_mss_mll {
+  my $part=$_[0];
+  my @text;
+  my $mode;
+  my $r;
+  print STDERR "$part\n";
+
+  open(O, "cascadenik-compile.py $root_path/render/$part.mml|");
+
+  while($r=<O>) {
+    if($r =~ /<Style /) {
+      $mode=1;
+    }
+    elsif($r =~ /<Layer /) {
+      $mode=2;
+    }
+
+    if($mode>0) {
+      if($r =~ /^(.*)<StyleName>(.*)<\/StyleName>/) {
+	$text[$mode].="$1<StyleName>$part $2</StyleName>\n";
+      }
+      elsif($r =~ /^(.*)<Style name="(.*)">/) {
+	$text[$mode].="$1<Style name=\"$part $2\">\n";
+      }
+      elsif($r =~ /^(.*)<Layer name="(.*)"(.*)$/) {
+	$text[$mode].="$1<Layer name=\"$part $2\"$3\n";
+      }
+      else {
+	$text[$mode].=$r;
+      }
+    }
+
+    if(($r =~ /<\/Style>/)||($r =~ /<\/Layer>/)) {
+      $mode=0;
+    }
+  }
+  close(O);
+
+  return @text;
+}
+
+sub get_mss {
+  my $part=$_[0];
+  my @text;
+
+  @text=get_mss_mll($part);
+
+  return $text[1];
+}
+
+sub get_mml {
+  my $part=$_[0];
+  my @text;
+
+  @text=get_mss_mll($part);
+
+  return $text[2];
+}
+
