294618536d4cdec64e5b2c6ca3bf08b446934071
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-07-24 10:29:42 +0000

    Bugfix
    - Didn't I make an index for search?
    
    
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@16676 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/sql/01_search.sql
+++ src/sql/01_search.sql
@@ -10,7 +10,8 @@ create table search (
   amenity_val	text
 );
 create index search_type on search(amenity_type);
-create index search_loc_name on search(loc_name);
+-- create index search_loc_name on search(loc_name);
+create index search_idx on search using gin(to_tsvector('english', loc_name));
 
 -- fill places into search
 --insert into search select name, name, null as language, (CASE WHEN rel_id is not null THEN 'rel' WHEN array_dims(stations)!='[1:1]' THEN 'coll' ELSE 'node' END), (CASE WHEN rel_id is not null THEN rel_id ELSE (array_sort(stations))[1] END) as id, 'station', null, (CASE WHEN geometrytype(envelope(way)) in ('POINT', 'LINESTRING') THEN geomfromtext('POLYGON((' || xmin(way)-0.1 || ' ' || ymin(way)-0.1 || ',' || xmax(way)+0.1 || ' ' || ymin(way)-0.1 || ',' || xmax(way)+0.1 || ' ' || ymax(way)+0.1 || ',' || xmin(way)-0.1 || ' ' || ymax(way)+0.1 || ',' || xmin(way)-0.1 || ' ' || ymin(way)-0.1 || '))') ELSE envelope(way) END) as area from planet_osm_stations limit 1;
