4657e3d70b74ebb7a80c017c16f6f825da7578ef
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-08-01 11:22:24 +0000

    New function array_toint
    Converts an array to an int array
    
    
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@16756 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/sql/01_functions.sql
+++ src/sql/01_functions.sql
@@ -12,6 +12,26 @@ SFUNC = array_append,
 STYPE = int4[],
 INITCOND = '{}'); 
 
+create or replace function array_toint(anyarray)
+returns int[]
+as $$
+declare
+  arr alias for $1;
+  dest int[];
+  i int:=1;
+begin
+  if arr is null then
+    return null;
+  end if;
+  for i in array_lower(arr, 1)..array_upper(arr, 1) loop
+    if arr[i] similar to '^[0-9]+$' then
+      dest[i]=cast (arr[i] as int);
+    end if;
+  end loop;
+  return dest;
+end;
+$$ language 'plpgsql';
+
 CREATE OR REPLACE FUNCTION array_sort (ANYARRAY)
 RETURNS ANYARRAY LANGUAGE SQL
 AS $$
