625ecc6c9583d453a3e9bd0a9981714101fba5bb
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-09-24 05:53:32 +0000

    New function: array_unique(text[])
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@17790 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/sql/01_functions.sql
+++ src/sql/01_functions.sql
@@ -74,6 +74,35 @@ return ret;
 end;
 $$ language 'plpgsql';
 
+CREATE OR REPLACE FUNCTION array_unique(text[])
+RETURNS text[]
+AS $$
+declare
+-- src   int[]=array_sort($1);
+src   alias for $1;
+src_i int:=1;
+ret   text[];
+ret_i int:=1;
+found bool;
+begin
+  while src_i<=array_count(src) loop
+    ret_i:=1;
+    found:=false;
+    while ret_i<=array_count(ret) loop
+      if src[src_i]=ret[ret_i] then
+	found:=true;
+      end if;
+      ret_i:=ret_i+1;
+    end loop;
+    if found=false then
+      ret=array_append(ret, src[src_i]);
+    end if;
+    src_i:=src_i+1;
+  end loop;
+  return ret;
+end
+$$ language 'plpgsql';
+
 create function array_explode1(anyarray) returns setof anyelement as
 'begin
 for i in array_lower($1, 1) array_upper($1, 1) loop
