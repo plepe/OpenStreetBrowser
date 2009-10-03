324166cb941dc82c000d9f27e369384e6c587f8f
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2009-10-03 10:43:35 +0000

    Rewritten creation of planet_osm_poipoly
    - Don't take all columns, but only needed
    
    
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@17960 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/sql/01_poipoly.sql
+++ src/sql/01_poipoly.sql
@@ -1,34 +1,36 @@
 drop table if exists planet_osm_poipoly;
-create table planet_osm_poipoly as select * from planet_osm_point where 1=2;
-alter table planet_osm_poipoly drop column way;
+create table planet_osm_poipoly (
+  osm_id	int4		not null,
+  full_id	varchar(32)	not null,
+  id_type	varchar(4)	not null,
+  importance	text		,
+  network	text		,
+  highway	text		,
+  railway	text		,
+  aeroway	text		,
+  aerialway	text		,
+  amenity	text		
+);
 SELECT AddGeometryColumn('planet_osm_poipoly', 'way', 900913, 'GEOMETRY', 2);
-alter table planet_osm_poipoly add column full_id varchar(32);
-alter table planet_osm_poipoly add column id_type varchar(4);
 
 insert into planet_osm_poipoly
-  select *, 'node_' || osm_id, 'node' from planet_osm_point;
+  select osm_id, 'node_' || osm_id, 'node',
+    importance, network, highway, railway, aeroway, aerialway, amenity,
+    way
+  from planet_osm_point;
 insert into planet_osm_poipoly
-  select *, 'way_' || osm_id, 'way' from planet_osm_polygon;
+  select osm_id, 'way_' || osm_id, 'way',
+    importance, network, highway, railway, aeroway, aerialway, amenity, 
+    way
+  from planet_osm_polygon;
 
-create index planet_osm_poipoly_id_type     on planet_osm_poipoly("id_type");
-create index planet_osm_poipoly_full_id     on planet_osm_poipoly("full_id");
 create index planet_osm_poipoly_osm_id      on planet_osm_poipoly("osm_id");
+create index planet_osm_poipoly_full_id     on planet_osm_poipoly("full_id");
+create index planet_osm_poipoly_id_type     on planet_osm_poipoly("id_type");
 create index planet_osm_poipoly_way         on planet_osm_poipoly using gist(way);
 
-create index planet_osm_poipoly_waterway    on planet_osm_poipoly("waterway");
 create index planet_osm_poipoly_railway     on planet_osm_poipoly("railway");
 create index planet_osm_poipoly_highway     on planet_osm_poipoly("highway");
-create index planet_osm_poipoly_barrier     on planet_osm_poipoly("barrier");
-create index planet_osm_poipoly_power       on planet_osm_poipoly("power");
-create index planet_osm_poipoly_man_made    on planet_osm_poipoly("man_made");
-create index planet_osm_poipoly_addr_street on planet_osm_poipoly("addr:street");
-create index planet_osm_poipoly_addr_num    on planet_osm_poipoly("addr:housenumber");
 create index planet_osm_poipoly_amenity     on planet_osm_poipoly("amenity");
-create index planet_osm_poipoly_shop        on planet_osm_poipoly("shop");
-create index planet_osm_poipoly_tourism     on planet_osm_poipoly("tourism");
-create index planet_osm_poipoly_historic    on planet_osm_poipoly("historic");
-create index planet_osm_poipoly_landuse     on planet_osm_poipoly("landuse");
-create index planet_osm_poipoly_cemetery    on planet_osm_poipoly("cemetery");
-
-
-
+create index planet_osm_poipoly_aeroway	    on planet_osm_poipoly("aeroway");
+create index planet_osm_poipoly_aerialway   on planet_osm_poipoly("aerialway");
