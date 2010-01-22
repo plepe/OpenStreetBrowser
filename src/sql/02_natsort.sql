43f11c8095ab5fda0ccb82c6986a7892d7c9b9b9
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-01-22 10:44:35 +0000

    New plsql function: array_nat_sort(text[]) sorts 'naturally'
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@19576 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ src/sql/02_natsort.sql
@@ -0,0 +1,116 @@
+-- returns true if second argument is higher than first argument
+CREATE OR REPLACE FUNCTION nat_cmp (text, text)
+returns bool
+as $$
+declare
+  left	alias for $1;
+  right alias for $2;
+  l1 text;
+  r1 text;
+  l2 text;
+  r2 text;
+  ul1 text;
+  ur1 text;
+begin
+  l1=substring(left  from '^([0-9]+).*$');
+  r1=substring(right from '^([0-9]+).*$');
+  l2=substring(left  from '^[0-9]+(.*)$');
+  r2=substring(right from '^[0-9]+(.*)$');
+
+  if l1 is null then
+    l1:='';
+  end if;
+
+  if r1 is null then
+    r1:='';
+  end if;
+
+  if l1!='' and r1!='' then
+    if cast(l1 as int)<cast(r1 as int) then
+      return true;
+    end if;
+    if l1=r1 then
+      return nat_cmp(l2, r2);
+    end if;
+    if cast(l1 as int)>cast(r1 as int) then
+      return false;
+    end if;
+  end if;
+
+  if l1='' and r1!='' then
+    return false;
+  end if;
+
+  if l1!='' and r1='' then
+    return true;
+  end if;
+
+  l1=substring(left  from '^([^0-9]*)([0-9].*)?$');
+  r1=substring(right from '^([^0-9]*)([0-9].*)?$');
+  l2=substring(left  from '^[^0-9]+([0-9].*)$');
+  r2=substring(right from '^[^0-9]+([0-9].*)$');
+  ul1=upper(l1);
+  ur1=upper(r1);
+
+  if l1 is null then l1:=''; end if;
+  if r1 is null then r1:=''; end if;
+  if l2 is null then l2:=''; end if;
+  if r2 is null then r2:=''; end if;
+
+  if l1!='' and r1!='' then
+    if ul1<ur1 then
+      return true;
+    end if;
+    if ul1=ur1 then
+      if (l1=r1) or (l2!='' and r2!='') then
+	return nat_cmp(l2, r2);
+      else
+        return l1<l2;
+      end if;
+    end if;
+    if ul1>ur1 then
+      return false;
+    end if;
+  end if;
+
+  if l1='' and r1!='' then
+    return true;
+  end if;
+
+  if l1!='' and r1='' then
+    return false;
+  end if;
+
+  return false;
+end
+$$ language 'plpgsql';
+
+CREATE OR REPLACE FUNCTION array_nat_sort (text[])
+RETURNS text[]
+AS $$
+declare
+src     alias for $1;
+list    text[];
+i int:=1;
+j int:=1;
+h text;
+begin
+  list=src;
+  while i<=array_count(list) loop
+    j:=1;
+    while j<i loop
+      if nat_cmp(list[i], list[j]) then
+        h:=list[i];
+	list[i]=list[j];
+	list[j]=h;
+      end if;
+      j:=j+1;
+    end loop;
+    i:=i+1;
+  end loop;
+
+  return list;
+end
+$$ language 'plpgsql';
+
+
