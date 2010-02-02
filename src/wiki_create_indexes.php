d63dff736686aa356ee1d69f779bd8c962814c43
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-02-02 14:57:29 +0000

    Make indexes only once, be verbose
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@19790 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- src/wiki_create_indexes.php
+++ src/wiki_create_indexes.php
@@ -2,12 +2,19 @@
 include("conf.php");
 $sql=pg_connect("dbname=$db_name user=$db_user password=$db_passwd host=$db_host");
 $request=unserialize(file_get_contents("/osm/skunkosm/request.save"));
+$done=array();
 
 foreach($request as $category=>$d1) {
   foreach($d1 as $importance=>$d2) {
     foreach($d2 as $table=>$d3) {
       if($d3['columns']) foreach($d3['columns'] as $column=>$values) {
-	pg_query("create index planet_osm_{$table}_{$column} on planet_osm_{$table}(\"{$column}\");");
+	if((!$done[$table])||(!$done[$table][$column])) {
+	  $qry="create index planet_osm_{$table}_{$column} on planet_osm_{$table}(\"{$column}\");";
+	  print "$qry\n";
+	  pg_query($qry);
+
+	  $done[$table][$column]=1;
+	}
       }
     }
   }
