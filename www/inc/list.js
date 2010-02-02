b297cd202468dbd7c7c230542ecefe2a7f7304d5
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-02-02 14:36:19 +0000

    New view list: load more elements
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@19738 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- www/inc/list.js
+++ www/inc/list.js
@@ -2,6 +2,23 @@ var list_last=[];
 var list_reload_necessary=1;
 var list_reload_working=0;
 
+function list_make_list(cat) {
+  var ret="";
+  var places=cat.getElementsByTagName("place");
+
+  for(var placei=0; placei<places.length; placei++) {
+    var place=places[placei];
+    ret+="<li><element id='"+place.getAttribute("id")+"'>"+place.getAttribute("name")+"</element></li>\n";
+  }
+
+  if(cat.getAttribute("complete")!="true") {
+    var cat_id=cat.getAttribute("id")
+    ret+="<a id='more_"+cat_id+"' href='javascript:list_more(\""+cat_id+"\")'>more</a>\n";
+  }
+
+  return ret;
+}
+
 function list_call_back(response) {
   var data=response.responseXML;
   list_reload_working=0;
@@ -23,14 +40,9 @@ function list_call_back(response) {
     if(!category_leaf[cat_id])
       continue;
     var div=document.getElementById("content_"+cat_id);
-    var ret="<ul>";
     if(div) {
-      var places=cat.getElementsByTagName("place");
-      for(var placei=0; placei<places.length; placei++) {
-	var place=places[placei];
-	ret+="<li>"+place.getAttribute("name")+"</li>\n";
-      }
-
+      var ret="<ul>";
+      ret+=list_make_list(cat);
       ret+="</ul>\n";
       div.innerHTML=ret;
     }
@@ -56,6 +68,70 @@ function list_call_back(response) {
   load_objects_from_xml(osm);
 
   return;
+}
+
+function list_more_call_back(response) {
+  var data=response.responseXML;
+  list_reload_working=0;
+
+  if(!data) {
+    alert("no data\n"+response.responseText);
+    return;
+  }
+
+  var cats=data.getElementsByTagName("category");
+  for(var cati=0; cati<cats.length; cati++) {
+    var cat=cats[cati];
+    var cat_id=cat.getAttribute("id");
+    if(!category_leaf[cat_id])
+      continue;
+    var div=document.getElementById("content_"+cat_id);
+    var more=document.getElementById("more_"+cat_id);
+    more.parentNode.removeChild(more);
+    var ul=div.getElementsByTagName("ul");
+    ul=ul[0];
+
+    if(div) {
+      ul.innerHTML+=list_make_list(cat);
+    }
+  }
+//  map_div.className="map";
+//  var text_node=data.getElementsByTagName("text");
+//  if(text_node) {
+//    if(!text_node[0])
+//      show_msg("Returned data invalid", response.responseText);
+//    var text=get_content(text_node[0]);
+////alert(text_node[0].nodeValue);
+////    info_content.appendChild(text_node[0].cloneNode(true));
+//    info_content.innerHTML=text;
+//  }
+
+  if(list_reload_necessary) {
+    list_reload();
+  }
+
+  var osm=data.getElementsByTagName("osm");
+  load_objects_from_xml(osm);
+
+  return;
+}
+
+function list_more(cat) {
+  var x=map.calculateBounds();
+
+  var div=document.getElementById("more_"+cat);
+  if(div)
+    div.innerHTML="<img src='img/ajax_loader.gif'> loading";
+
+  var there=[];
+  div=document.getElementById("content_"+cat);
+  var obs=div.getElementsByTagName("element");
+  for(var i=0; i<obs.length; i++) {
+    var ob=obs[i];
+    there.push(ob.getAttribute("id"));
+  }
+
+  ajax_direct("list.php", { "viewbox": x.left +","+ x.top +","+ x.right +","+ x.bottom, "zoom": map.zoom, "exclude": there.join(","), "category": cat, "lang": lang }, list_more_call_back);
 }
 
 function list_reload(info_lists) {
