3de7caba7586e2411af6e4161047865526cf122a
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-02-02 14:36:35 +0000

    new list: show icon
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@19741 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- www/inc/layout.js
+++ www/inc/layout.js
@@ -20,9 +20,20 @@ function box_click(boxname, subboxname) {
 
 function list_entry(ob) {
   var add="";
+  var li_style="";
 
   if(ob.getAttribute("description"))
     add=" ("+ob.getAttribute("description")+")";
 
-  return "<li class='listitem' id='list_"+ob.getAttribute("id")+"'><element id='"+ob.getAttribute("id")+"'><a href='#"+ob.getAttribute("id")+"' onMouseOver='set_highlight([\""+ob.getAttribute("id")+"\"])' onMouseOut='unset_highlight()'>"+ob.getAttribute("name")+"</a>"+add+"</element></li>\n";
+  if(ob.getAttribute("icon")) {
+    var icon=ob.getAttribute("icon");
+    var icon_data;
+
+    if(icon_data=icon.match(/^\[\[Image:(.*)\]\]$/)) {
+      icon_data=icon_data[1].replace(/ /g, "_");
+      li_style+="list-style-image: url('symbols/"+icon_data+"'); ";
+    }
+  }
+
+  return "<li class='listitem' style=\""+li_style+"\" id='list_"+ob.getAttribute("id")+"'><element id='"+ob.getAttribute("id")+"'><a href='#"+ob.getAttribute("id")+"' onMouseOver='set_highlight([\""+ob.getAttribute("id")+"\"])' onMouseOut='unset_highlight()'>"+ob.getAttribute("name")+"</a>"+add+"</element></li>\n";
 }
