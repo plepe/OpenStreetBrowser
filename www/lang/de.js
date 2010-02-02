e31d250bfe5b93c6a39cb6ed25a7e3b5ce09b50b
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-02-02 14:37:05 +0000

    JS language: first strings
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@19748 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

---
+++ www/lang/de.js
@@ -0,0 +1,4 @@
+lang_str["amenity=restaurant"]=[ "Restaurant", "Restaurants" ];
+lang_str["amenity=cafe"]      =[ "Kaffehaus", "Kaffeehäuser" ];
+lang_str["amenity=pub"]       =[ "Pub", "Pubs" ];
+
---
+++ www/lang/en.js
@@ -0,0 +1,4 @@
+lang_str["amenity=restaurant"]=[ "restaurant", "restaurants" ];
+lang_str["amenity=cafe"]      =[ "cafe", "cafes" ];
+lang_str["amenity=pub"]       =[ "pub", "pubs" ];
+lang_str["religion=christian"]=[ "christian" ];
