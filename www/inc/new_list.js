2407990bd85a9de1d2d147367863877c1e91f144
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-02-02 14:35:26 +0000

    New list: show boxes
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@19731 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- www/inc/new_list.js
+++ www/inc/new_list.js
@@ -1,23 +1,51 @@
-var list_open={"culture": 1, "culture/culture": 1};
+var list_open={"culture": 1, "culture/religion": 1};
+
+function box_open(head, path, content) {
+  var ret="";
+  var level=path.split(/\//).length;
+
+  ret+="<div class='box_open_"+level+"' id='list_"+path+"'>\n";
+  ret+="<h"+level+"><input type='checkbox' name='"+path+"' "+
+       (content==null?"":" checked='checked'")+
+       " onChange='box_change(this)' />"+
+       "<a href='javascript:box_click(\""+path+"\")'>"+
+       head+"</a></h"+level+">\n";
+  if(content!=null)
+    ret+=content;
+  ret+="</div>\n";
+
+  return ret;
+}
+
+function box_closed(head, path) {
+  return box_open(head, path, null);
+}
 
 function show_list(path, _list) {
   var ret="";
   var p;
+  var level;
   if(!path) {
     path="";
     _list=category_list;
+    level=0;
+  }
+  else {
+    level=path.split(/\//);
   }
 
   for(var i in _list) {
-    ret+=i+"<br>\n";
     if(path!="")
       p=path+"/"+i;
     else
       p=i;
 
     if(list_open[p]) {
-      ret+=show_list(p, _list[i]);
+      var r=show_list(p, _list[i]);
+      ret+=box_open(i, p, r);
     }
+    else
+      ret+=box_closed(i, p);
   }
 
   if(path=="") {
