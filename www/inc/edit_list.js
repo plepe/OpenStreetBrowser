2ccff021fcb16126131bf2447c25a5be7e0643d0
Skunk for OSM <skunkosm@pitr.(none)>
2010-02-12 09:30:27 +0100

    Edit list: Enter data for an element

--- www/inc/edit_list.js
+++ www/inc/edit_list.js
@@ -21,13 +21,19 @@ function edit_list_element_set() {
 }
 
 function edit_list_new_element() {
-  var data={};
+  var data={ tag: "", description: "", name: "", icon: "" };
   var ret="";
 
   ret ="<form id='edit_element' action='javascript: edit_list_element_set()'>\n";
   ret+="<table>\n";
   ret+="<tr><td colspan='2'>New element</td></tr>\n";
-  ret+="<tr><td>Foo:</td><td><input name='foo' value='"+data.bar+"'/></td></tr>\n";
+  ret+="<tr><td>Name:</td><td><input name='name' value='"+data.name+"'/></td></tr>\n";
+  ret+="<tr><td>Tag:</td><td><input name='tag' value='"+data.tag+"'/></td></tr>\n";
+  ret+="<tr><td colspan='2' class='help'>Please insert a tag/value-pair, e.g. \"amenity=bar\". If you want to match on several tags, e.g. christian church, write \"amenity=place_of_worship religion=christian\". If a tag can hold on of several values, write \"amenity=bar;restaurant\". See <a href='http://wiki.openstreetmap.org/wiki/Map_Features' target='_new'>Map Features</a> for possible values.</td></tr>\n";
+  ret+="<tr><td>Further description:</td><td><input name='description' value='"+data.description+"'/></td></tr>\n";
+  ret+="<tr><td colspan='2' class='help'>Insert a tag-name which further describes this element, e.g. \"cuisine\" for restaurants. You you don't know, rather leave this field empty.</td></tr>\n";
+  ret+="<tr><td>Icon:</td><td><input name='icon' value='"+data.icon+"'/></td></tr>\n";
+
   ret+="</table>\n";
   ret+="<input type='submit' value='Ok'>\n";
   ret+="</form>\n";
