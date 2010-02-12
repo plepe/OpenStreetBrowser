bc3aeecfeebd88ae3a15187cbe60cb34aa6ed90d
Skunk for OSM <skunkosm@pitr.(none)>
2010-02-12 10:17:29 +0100

    Edit list: edit and re-edit elements

--- www/inc/edit_list.js
+++ www/inc/edit_list.js
@@ -1,48 +1,76 @@
+var edit_list_highest_element_id=0;
+var edit_list_form;
+
 function edit_list_set_list_data(ob) {
-  var data=document.getElementById("edit_list");
-//  alert(print_r(ob));
-  alert("gogo");
 }
 
-function edit_list_element_set() {
-  var f=document.getElementById("edit_element");
-  var div=f.parentNode;
+function edit_list_element_set(id) {
+  var div=document.getElementById("edit_list_element_"+id);
+  var data=edit_list_get_data(id);
 
-  div.data={};
-  for(var i=0; i<f.elements.length; i++) {
-    var fi=f.elements[i];
+  div.innerHTML="<a href='javascript: edit_list_edit_element("+data.id+")'>(foo) "+data.name+"</a>"+edit_list_hidden_form(data);
+}
+
+function edit_list_get_data(id) {
+  var div=document.getElementById("edit_list_element_"+id);
+
+  var data={};
+  for(var i=0; i<edit_list_form.elements.length; i++) {
+    var fi=edit_list_form.elements[i];
 
     if(fi.name)
-      div.data[fi.name]=fi.value;
+      if(m=fi.name.match("^"+id+"\.([a-z_]+)")) {
+	data[m[1]]=fi.value;
+      }
   }
 
-  alert(print_r(div.data));
-  div.innerHTML="(foo) "+div.data.foo;
+  return data;
+}
+
+function edit_list_hidden_form(data) {
+  ret="";
+
+  for(var i in data) {
+    ret+="<input type='hidden' name='"+data.id+"."+i+"' value='"+data[i]+"'/>\n";
+  }
+
+  return ret;
+}
+
+function edit_list_element_form(data) {
+  var ret="";
+
+  ret ="<div id='edit_element_"+data.id+"'>\n";
+  ret+="<input type='hidden' name='"+data.id+".id' value='"+data.id+"'/></td></tr>\n";
+  ret+="<table>\n";
+  ret+="<tr><td colspan='2'>New element</td></tr>\n";
+  ret+="<tr><td>Name:</td><td><input name='"+data.id+".name' value='"+data.name+"'/></td></tr>\n";
+  ret+="<tr><td>Tag:</td><td><input name='"+data.id+".tag' value='"+data.tag+"'/></td></tr>\n";
+  ret+="<tr><td colspan='2' class='help'>Please insert a tag/value-pair, e.g. \"amenity=bar\". If you want to match on several tags, e.g. christian church, write \"amenity=place_of_worship religion=christian\". If a tag can hold on of several values, write \"amenity=bar;restaurant\". See <a href='http://wiki.openstreetmap.org/wiki/Map_Features' target='_new'>Map Features</a> for possible values.</td></tr>\n";
+  ret+="<tr><td>Further description:</td><td><input name='"+data.id+".description' value='"+data.description+"'/></td></tr>\n";
+  ret+="<tr><td colspan='2' class='help'>Insert a tag-name which further describes this element, e.g. \"cuisine\" for restaurants. You you don't know, rather leave this field empty.</td></tr>\n";
+  ret+="<tr><td>Icon:</td><td><input name='"+data.id+".icon' value='"+data.icon+"'/></td></tr>\n";
+
+  ret+="</table>\n";
+  ret+="<input type='button' value='Ok' onClick='edit_list_element_set("+data.id+")'>\n";
+  ret+="</div>\n";
+
+  return ret;
 }
 
 function edit_list_new_element() {
-  var data={ tag: "", description: "", name: "", icon: "" };
-  var ret="";
-
-  ret ="<form id='edit_element' action='javascript: edit_list_element_set()'>\n";
-  ret+="<table>\n";
-  ret+="<tr><td colspan='2'>New element</td></tr>\n";
-  ret+="<tr><td>Name:</td><td><input name='name' value='"+data.name+"'/></td></tr>\n";
-  ret+="<tr><td>Tag:</td><td><input name='tag' value='"+data.tag+"'/></td></tr>\n";
-  ret+="<tr><td colspan='2' class='help'>Please insert a tag/value-pair, e.g. \"amenity=bar\". If you want to match on several tags, e.g. christian church, write \"amenity=place_of_worship religion=christian\". If a tag can hold on of several values, write \"amenity=bar;restaurant\". See <a href='http://wiki.openstreetmap.org/wiki/Map_Features' target='_new'>Map Features</a> for possible values.</td></tr>\n";
-  ret+="<tr><td>Further description:</td><td><input name='description' value='"+data.description+"'/></td></tr>\n";
-  ret+="<tr><td colspan='2' class='help'>Insert a tag-name which further describes this element, e.g. \"cuisine\" for restaurants. You you don't know, rather leave this field empty.</td></tr>\n";
-  ret+="<tr><td>Icon:</td><td><input name='icon' value='"+data.icon+"'/></td></tr>\n";
-
-  ret+="</table>\n";
-  ret+="<input type='submit' value='Ok'>\n";
-  ret+="</form>\n";
-
+  var data={ id: edit_list_highest_element_id++, tag: "", description: "", name: "", icon: "" };
   var div=document.createElement("div");
-  div.innerHTML=ret;
+  div.innerHTML=edit_list_element_form(data);
+  div.id="edit_list_element_"+data.id;
 
   var ellist=document.getElementById("edit_list_element_list");
   ellist.appendChild(div);
+}
+
+function edit_list_edit_element(id) {
+  div=document.getElementById("edit_list_element_"+id);
+  div.innerHTML=edit_list_element_form(edit_list_get_data(id));
 }
 
 function edit_list() {
@@ -63,8 +91,10 @@ function edit_list() {
   ret+="<a href='javascript: edit_list_new_element()'>Add an element</a>\n";
   ret+="<br/>\n";
 
-  ret+="<input type='submit' value='Save'>\n";
-  ret+="<form>\n";
+  ret+="<input type='submit' value='Save'/>\n";
+  ret+="</form>\n";
 
   w.content.innerHTML=ret;
+
+  edit_list_form=document.getElementById("edit_list");
 }
