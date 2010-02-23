1859916f7e701904de38eb56f5f1cf501af509a1
Skunk for OSM <skunkosm@pitr.(none)>
2010-02-23 08:04:26 +0100

    Categories: Check table-join, table-columns

--- www/inc/categories_sql.php
+++ www/inc/categories_sql.php
@@ -1,11 +1,11 @@
 <?
 // Parses a matching string as used in categories
-function parse_match($match) {
+function parse_match($match, $table="point") {
   $match_parts=parse_explode($match);
   $parts=array();
 
   foreach($match_parts as $part) {
-    $parts[]=build_match_part($part);
+    $parts[]=build_match_part($part, $table);
   }
 
   print_r($parts);
@@ -15,12 +15,20 @@ function postgre_escape($str) {
   return "E'".strtr($str, array("'"=>"\\'"))."'";
 }
 
-function build_match_part($part) {
+function build_match_part($part, $table) {
   $c_not=null;
+  global $postgis_tables;
+  $table_def=$postgis_tables[$table];
+  $allow_null=false;
 
   for($i=0; $i<sizeof($part['operators']); $i++) {
     $operator=$part['operators'][$i];
     $values=$part['values'][$i];
+
+    $col_name="\"$part[key]\"";
+    if(!in_array($part['key'], $table_def[index])) {
+      $col_name="\"$part[key]_table\".v";
+    }
 
     // case-statement
     $c_prevnot=$c_not;
@@ -29,7 +37,7 @@ function build_match_part($part) {
       case "!=":
         $c_not=true;
       case "=":
-	$c="\"$part[key]\"";
+	$c=$col_name;
 	$c1="";
 	$ccount=0;
         $c1.=($c_not?" not":"")." in (";
@@ -47,15 +55,20 @@ function build_match_part($part) {
 	}
 
 	foreach($values as $v) {
-	  if($v=="*")
-	    $c.=" is ".($c_not?"":"not ")."null";
+	  if(($v=="*")&&($c_not==false)) {
+	    $c.=" is null";
+	    $allow_null=true;
+	  }
+	  elseif(($v=="*")&&($c_not==true)) {
+	    $c.=" is not null";
+	  }
 	}
 	
 	if($c_prevnot===true) {
-          $case.=" or ";
+          $case.=" and ";
 	}
 	elseif($c_prevnot===false) {
-          $case.=" and ";
+          $case.=" or ";
 	}
 	$case.=$c;
 	break;
@@ -65,15 +78,16 @@ function build_match_part($part) {
       case "<=":
         if(sizeof($values)>1)
 	  print "Operator $operator , more than one value supplied\n";
+	$c_not=true;
 
 	if($c_prevnot===true) {
-	  $case.=" or ";
-	}
-	elseif($c_prevnot===false) {
 	  $case.=" and ";
 	}
+	elseif($c_prevnot===false) {
+	  $case.=" or ";
+	}
 
-	$c.="parse_number(\"$part[key]\")";
+	$c.="parse_number($col_name)";
 	$c.="{$operator}parse_number(".postgre_escape($values[0]).")";
 
 	$case.=$c;
@@ -84,7 +98,18 @@ function build_match_part($part) {
     // where-statement
   }
 
-  return array("case"=>$case, "where"=>$where);
+  // join-statement
+  $join="";
+  $select="\"{$part['key']}\"";
+  if(!in_array($part['key'], $table_def[index])) {
+    if($allow_null)
+      $join.="left ";
+    $join.="join {$table_def[id_type]}_tags \"{$part[key]}_table\" on planet_osm_{$table}.osm_id=\"{$part[key]}_table\".{$table_def[id_type]}_id and \"{$part[key]}_table\".k='$part[key]'";
+    $select="\"{$part[key]}_table\".v as \"{$part['key']}\"";
+  }
+
+
+  return array("case"=>$case, "where"=>$case, "join"=>$join, "columns"=>$part['key'], "select"=>$select);
 }
 
 function parse_explode($match) {
