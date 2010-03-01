68c3cf6b2df0aa931c1ec47797ed43ff9c0f014b
Skunk for OSM <skunkosm@pitr.(none)>
2010-03-01 16:32:48 +0100

    Options: Some more code
    - read values from radio-buttons
    - save and load to cookie

--- www/inc/options.js
+++ www/inc/options.js
@@ -1,9 +1,37 @@
 var options_win;
+var options_list=[ "autozoom" ];
+var options_values={};
 
-function set_options() {
+function options_set(key, value) {
+  var expiry=new Date();
+  expiry.setTime(expiry.getTime()+365*86400000);
+
+  document.cookie="option:"+key+"="+value+"; expires="+expiry.toGMTString()+"; path=/";
+}
+
+function options_get(key) {
+  return options_values[key];
+}
+
+function options_load() {
+  var cookie;
+  if(cookie=document.cookie) {
+    var cookies=cookie.split(/;/);
+
+    for(var i=0; i<cookies.length; i++) {
+      var m;
+
+      if(m=cookies[i].match(/^ *option:([a-zA-Z0-9_]+)=(.*)$/)) {
+	options_values[m[1]]=m[2];
+      }
+    }
+  }
+}
+
+function save_options() {
   var form=document.getElementById("options_form");
 
-  alert(form.elements.autozoom[0].checked);
+  options_set("autozoom", options_radio_get("autozoom"));
 }
 
 function close_options() {
@@ -32,12 +60,23 @@ function options_radio(key, current_value, values) {
   return ret;
 }
 
+function options_radio_get(key) {
+  var form=document.getElementById("options_form");
+
+  for(var i=0; i<form.elements[key].length; i++) {
+    if(form.elements[key][i].checked)
+      return form.elements[key][i].value;
+  }
+
+  return null;
+}
+
 function show_options() {
   var ret;
 
   options_win=new win("options_win");
 
-  ret ="<form action='javascript:set_options()' id='options_form'>\n";
+  ret ="<form action='javascript:save_options()' id='options_form'>\n";
 
   ret+=options_radio("autozoom", "move", [ "pan", "move", "stay" ]);
 
@@ -47,3 +86,5 @@ function show_options() {
 
   options_win.content.innerHTML=ret;
 }
+
+options_load();
