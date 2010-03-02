1f0d85ed4b3f63e19105946176b56fbd94f7a544
Skunk for OSM <skunkosm@pitr.(none)>
2010-03-02 06:37:06 +0100

    Options/Lang: Include a list of available languages

---
+++ www/lang/list.js
@@ -0,0 +1,9 @@
+// A list of all languages we know about
+var language_list={
+  "en": "English",
+  "de": "German",
+  "bg": "Български"
+}
+
+// The UI has been translated to following languages
+var ui_langs= [ "en", "de" ];
