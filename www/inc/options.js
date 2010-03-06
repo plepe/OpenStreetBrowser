a08e7d206aa98b39514e850bea20cf4b8fb72bbe
skunk <skunk@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2010-03-06 09:04:25 +0000

    Options: Help for options
    git-svn-id: http://svn.openstreetmap.org/sites/www.openstreetbrowser.org@20304 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- www/inc/options.js
+++ www/inc/options.js
@@ -118,9 +118,11 @@ function show_options() {
   ret ="<form action='javascript:save_options()' id='options_form'>\n";
 
   ret+="<h4>"+t("options:autozoom")+"</h4>\n";
+  ret+="<div class='options_help'>"+t("help:autozoom")+"</div>\n";
   ret+=options_radio("autozoom", [ "pan", "move", "stay" ]);
 
   ret+="<h4>"+t("options:language_support")+"</h4>\n";
+  ret+="<div class='options_help'>"+t("help:language_support")+"</div>\n";
   ret+="<p>\n";
   var ui_langs_x={};
   for(var i=0; i<ui_langs.length; i++)
