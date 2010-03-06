88f4abb47fc26e080f715368e5f3a2077f79ccd6
Stephan Plepelits <skunk@xover.mud.at>
2010-03-06 09:58:19 +0100

    Languages of European Union: http://publications.europa.eu/code/de/de-370200.htm

--- www/lang/list.js
+++ www/lang/list.js
@@ -2,9 +2,31 @@
 var language_list={
   "":   "Local language",
   "en": "English",
+  "bg": "български",
+  "es": "español",
+  "cs": "čeština",
+  "da": "dansk",
   "de": "Deutsch",
-  "bg": "Български"
+  "et": "eesti keel",
+  "el": "ελληνικά",
+  "fr": "français",
+  "ga": "Gaeilge",
+  "it": "italiano",
+  "lv": "latviešu valoda",
+  "lt": "lietuvių kalba",
+  "hu": "magyar",
+  "mt": "Malti",
+  "nl": "Nederlands",
+  "pl": "polski",
+  "pt": "português",
+  "ro": "română",
+  "sk": "slovenčina",
+  "sl": "slovenščina",
+  "fi": "suomi",
+  "sv": "svenska",
 }
 //for(var i in language_list)
 //  lang_str["lang:"+i]=[ language_list[i] ];
 
+// Sources:
+// - Languages of European Union: http://publications.europa.eu/code/de/de-370200.htm
