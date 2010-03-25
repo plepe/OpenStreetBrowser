d9b04a178a417e34fe006403d02a6944d81cf953
Stephan Plepelits <skunk@xover.mud.at>
2010-03-25 14:29:13 +0100

    Match/SQL: Get rid of "old" SQL-Match-Code

--- www/inc/categories_sql.php
+++ www/inc/categories_sql.php
@@ -38,50 +38,6 @@ function parse_match($match, $table="point") {
     return array("case"=>array());
   }
 
-  $parts=array();
-
-  foreach($match_parts as $part) {
-    $b=build_match_part($part, $table);
-
-    if(is_string($b))
-      error("Error: $b");
-    else
-      $parts[]=$b;
-  }
-
-  $ret=array();
-  foreach($parts as $def) {
-    foreach($def as $part=>$text) {
-      if(isset($text))
-	$ret[$part][]=$text;
-    }
-  }
-
-  $where=array();
-  if($ret['where']) foreach($ret['where'] as $w) {
-    foreach($w as $k=>$vs) {
-      $where[$k]=$vs;
-    }
-  }
-  $ret['where']=$where;
-
-  if(sizeof($ret['case']))
-    $ret['case']=array(implode(" and ", $ret['case']));
-  else
-    $ret['case']=array("1=1");
-
-  return $ret;
-}
-
-// Parses a matching string as used in categories
-function parse_match_toarray($match, $table="point") {
-  $match_parts=parse_explode($match);
-
-  if(is_string($match_parts)) {
-    error("Error: $match_parts");
-    return array("case"=>array());
-  }
-
   $or_parts=array("or");
   $parts=array("and");
 
@@ -94,7 +50,7 @@ function parse_match_toarray($match, $table="point") {
       $parts=array("and");
     }
     else {
-      $b=build_match_toarray_part($part, $table);
+      $b=build_match_part($part, $table);
 
       if(is_string($b))
 	error("Error: $b");
@@ -112,28 +68,6 @@ function parse_match_toarray($match, $table="point") {
     return $or_parts[1];
 
   return $or_parts;
-//  $ret=array();
-//  foreach($parts as $def) {
-//    foreach($def as $part=>$text) {
-//      if(isset($text))
-//	$ret[$part][]=$text;
-//    }
-//  }
-//
-//  $where=array();
-//  if($ret['where']) foreach($ret['where'] as $w) {
-//    foreach($w as $k=>$vs) {
-//      $where[$k]=$vs;
-//    }
-//  }
-//  $ret['where']=$where;
-//
-//  if(sizeof($ret['case']))
-//    $ret['case']=array(implode(" and ", $ret['case']));
-//  else
-//    $ret['case']=array("1=1");
-//
-//  return $ret;
 }
 
 function postgre_escape($str) {
@@ -274,7 +208,7 @@ function match_collect_values($arr) {
   return $ret;
 }
 
-function build_match_toarray_part($part) {
+function build_match_part($part) {
   $c_not=null;
   $where=array();
   $case=array();
@@ -355,108 +289,6 @@ function build_match_toarray_part($part) {
   }
 
   return $case;
-}
-
-function build_match_part($part, $table) {
-  $c_not=null;
-  global $postgis_tables;
-  $table_def=$postgis_tables[$table];
-  $where=array();
-
-  for($i=0; $i<sizeof($part['operators']); $i++) {
-    $operator=$part['operators'][$i];
-    $values=$part['values'][$i];
-
-    $col_name="\"$part[key]\"";
-    if(!in_array($part['key'], $table_def[index])) {
-      $col_name="\"$part[key]_table\".v";
-    }
-
-    // case-statement
-    $c_prevnot=$c_not;
-    $c_not=false;
-    $where_not="";
-    switch($operator) {
-      case "!=":
-        $c_not=true;
-	$where_not="!";
-      case "=":
-	$c=$col_name;
-	$c1="";
-	$ccount=0;
-        $c1.=($c_not?" not":"")." in (";
-	$c2=array();
-	foreach($values as $v) {
-	  if($v!="*") {
-	    $c2[]=postgre_escape($v);
-	    $ccount++;
-	    $where[$col_name][]=$where_not.postgre_escape($v);
-	  }
-	}
-	$c1.=implode(", ", $c2).")";
-
-	if($ccount>0) {
-	  $c.=$c1;
-	}
-
-	foreach($values as $v) {
-	  if(($v=="*")&&($c_not==false)) {
-	    $c.=" is not null";
-	    $where[$col_name][]="!null";
-	  }
-	  elseif(($v=="*")&&($c_not==true)) {
-	    $c.=" is null";
-	    $where[$col_name][]="null";
-	  }
-	}
-	
-	if($c_prevnot===true) {
-          $case.=" or ";
-	}
-	elseif($c_prevnot===false) {
-          $case.=" and ";
-	}
-	$case.=$c;
-	break;
-      case ">":
-      case "<":
-      case ">=":
-      case "<=":
-        if(sizeof($values)>1)
-	  print "Operator $operator , more than one value supplied\n";
-	$c_not=false;
-	$where[$col_name][]="!null";
-
-	if($c_prevnot===true) {
-	  $case.=" or ";
-	}
-	elseif($c_prevnot===false) {
-	  $case.=" and ";
-	}
-
-	$c.="parse_number($col_name)";
-	$c.="{$operator}parse_number(".postgre_escape($values[0]).")";
-
-	$case.=$c;
-
-        break;
-    }
-    $c="";
-    // where-statement
-  }
-
-  // join-statement
-  $join="";
-  $select="\"{$part['key']}\"";
-  if(!in_array($part['key'], $table_def[index])) {
-    $join.="left join {$table_def[id_type]}_tags \"{$part[key]}_table\" on planet_osm_{$table}.osm_id=\"{$part[key]}_table\".{$table_def[id_type]}_id and \"{$part[key]}_table\".k='$part[key]'";
-    $select="\"{$part[key]}_table\".v as \"{$part['key']}\"";
-  }
-
-  $case="($case)";
-
-  $ret=array("case"=>$case, "where"=>$where, "join"=>$join, "columns"=>$part['key'], "select"=>$select);
-  return $ret;
 }
 
 function parse_explode($match) {
