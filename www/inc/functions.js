9e2696d2938f4718f77b0ccf9fad65d0c4e92446
Stephan Plepelits <skunk@xover.mud.at>
2010-07-11 08:07:44 +0200

    function array_remove: remove an element from an array

--- www/inc/functions.js
+++ www/inc/functions.js
@@ -27,3 +27,13 @@ function split_semicolon(str) {
 
   return ret;
 }
+
+// array_delete removes an element from the array at the pos position
+// example:
+//   array_remove([ 1, 2, 3, 4, 5 ], 2)
+//   -> [ 1, 2, 4, 5 ]
+function array_remove(arr, pos) {
+  var part1=arr.concat([]);
+  var part2=part1.splice(pos+1);
+  return part1.splice(0, pos).concat(part2);
+}
