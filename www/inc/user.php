cb572a06f9a9d21bd3a0b5a3a70a1cf2c3859f86
Stephan Plepelits <skunk@xover.mud.at>
2010-07-22 09:08:13 +0200

    User: get rid of unnecessary code

--- www/inc/user.php
+++ www/inc/user.php
@@ -90,106 +90,17 @@ class User {
   function valid_user() {
     return ($this->auth);
   }
-  
-  function authenticate($pass) {
-    if($this->username=="anonymous")
-      return 1;
-
-    if(crypt($pass, $this->pass)==$this->pass) {
-      $this->auth=1;
-      return 1;
-    }
-    return 0;
-  }
-
-  function toolbox() {
-    global $lang_str;
-
-    print "<div class='userinfo' onClick='authenticate_user(this)'>\n";
-    if($this->username=="anonymous")
-      print "$lang_str[tool_userauth_login]";
-    else
-      print "$lang_str[tool_userauth_loggedin] $this->username";
-    print "</div>\n";
-  }
 };
 
-$user_list=0;
-
 function user_list() {
-  global $passwd_file;
-  global $user_list;
+  $user_list=array();
 
-  if($user_list)
-    return $user_list;
-  else
-    $user_list=array();
-
-  $f=fopen($passwd_file, "r");
-  while($r=fgets($f)) {
-    $r=chop($r);
-    if(($r!="")&&(substr($r, 0, 1)!="#")) {
-      $r=explode(":", $r);
-
-      $user_list[$r[0]]=0;
-    }
+  $res=sql_query("select * from user_list");
+  while($elem=pg_fetch_assoc($res)) {
+    $user_list[]=$elem['username'];
   }
 
   call_hooks("user_list", &$user_list);
 
   return $user_list;
-}
-
-function get_user($username) {
-  global $user_list;
-
-  user_list();
-
-  if(!$user_list[$username])
-    $user_list[$username]=new User($username);
-
-  return $user_list[$username];
-}
-
-function small_login_form() {
-  global $lang_str;
-  global $url_page;
-  global $page;
-
-  $login_form=
-    "<form action='' method='post' id='small_login_form'>\n".
-    "<table>\n".
-    "<tr><td>$lang_str[tool_userauth_username]:</td><td><input name='username' id='small_login_username'></td></tr>\n".
-    "<tr><td>$lang_str[tool_userauth_password]:</td><td><input name='password' id='small_login_password' type='password'></td></tr>\n".
-    "<tr><td><input type='submit' id='user_login_submit' value='$lang_str[tool_userauth_ok]'></td><td align='right'><input type='submit' id='user_login_logout' value='$lang_str[tool_userauth_logout]'></td></tr>\n".
-    "</table>\n".
-    "</form>\n";
-
-  call_hooks("small_login_form", &$login_form);
-  $login_form="<span class='small_login_form' id='small_login'>$login_form</span>";
-
-  print $login_form;
-}
-
-function login_form() {
-global $lang_str;
-global $page;
-
-  $login_form=
-    "<form action='' method='post' id='login_form'>\n".
-    "<table>\n".
-    "<tr><td>$lang_str[tool_userauth_username]:</td><td><input name='username' id='small_login_username'></td></tr>\n".
-    "<tr><td>$lang_str[tool_userauth_password]:</td><td><input name='password' id='small_login_password' type='password'></td></tr>\n".
-    "<tr><td><input type='submit' id='user_login_submit' value='$lang_str[tool_userauth_ok]'></td><td align='right'><input type='submit' id='user_login_logout' value='$lang_str[tool_userauth_logout]'></td></tr>\n".
-    "</table>\n".
-    "</form>\n";
-
-  call_hooks("login_form", &$login_form);
-  $login_form="$lang_str[tool_userauth_norights]\n".
-    "<div class='login_form' id='login'>$login_form</div>";
-
-  print $login_form;
-}
-
-function new_user() {
 }
