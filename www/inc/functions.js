40d542002cec3b00b12347503065d69396bb1a05
Stephan Plepelits <skunk@xover.mud.at>
2010-07-25 14:13:21 +0200

    Function array_merge merges two javascript assoc. arrays
    Source: phpjs.org

--- www/inc/functions.js
+++ www/inc/functions.js
@@ -38,6 +38,61 @@ function array_remove(arr, pos) {
   return part1.splice(0, pos).concat(part2);
 }
 
+// source: phpjs.org
+function array_merge () {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Nate
+    // +   input by: josh
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+    // *     example 1: arr1 = {"color": "red", 0: 2, 1: 4}
+    // *     example 1: arr2 = {0: "a", 1: "b", "color": "green", "shape": "trapezoid", 2: 4}
+    // *     example 1: array_merge(arr1, arr2)
+    // *     returns 1: {"color": "green", 0: 2, 1: 4, 2: "a", 3: "b", "shape": "trapezoid", 4: 4}
+    // *     example 2: arr1 = []
+    // *     example 2: arr2 = {1: "data"}
+    // *     example 2: array_merge(arr1, arr2)
+    // *     returns 2: {0: "data"}
+    
+    var args = Array.prototype.slice.call(arguments),
+                            retObj = {}, k, j = 0, i = 0, retArr = true;
+    
+    for (i=0; i < args.length; i++) {
+        if (!(args[i] instanceof Array)) {
+            retArr=false;
+            break;
+        }
+    }
+    
+    if (retArr) {
+        retArr = [];
+        for (i=0; i < args.length; i++) {
+            retArr = retArr.concat(args[i]);
+        }
+        return retArr;
+    }
+    var ct = 0;
+    
+    for (i=0, ct=0; i < args.length; i++) {
+        if (args[i] instanceof Array) {
+            for (j=0; j < args[i].length; j++) {
+                retObj[ct++] = args[i][j];
+            }
+        } else {
+            for (k in args[i]) {
+                if (args[i].hasOwnProperty(k)) {
+                    if (parseInt(k, 10)+'' === k) {
+                        retObj[ct++] = args[i][k];
+                    } else {
+                        retObj[k] = args[i][k];
+                    }
+                }
+            }
+        }
+    }
+    return retObj;
+}
+
 function dom_create_append(parent, type, xml) {
   if(!xml)
     xml=document;
