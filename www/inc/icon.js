05e2b25da225aff871a5b641468136c2c5d714b5
Stephan Plepelits <skunk@xover.mud.at>
2010-07-28 08:00:52 +0200

    Icon: Choose and edit icons (stimple simple)

--- www/inc/icon.js
+++ www/inc/icon.js
@@ -1,3 +1,103 @@
-function icon_chooser(current, callback) {
-  this.win=new win();
+var icon_git;
+
+function icon_obj(dir, id, files) {
+  this.inheritFrom=git_obj;
+  this.inheritFrom(dir, id, files);
+
+  this.callback=function() {
+    this.save_callback(this.id);
+  }
+
+  this.write_chooser=function(ul, callback) {
+    var li=dom_create_append(ul, "li");
+    var img=dom_create_append(li, "img");
+    img.src=this.url("preview.png");
+    //var txt=dom_create_append_text(li, this.files[0]);
+    this.save_callback=callback;
+    img.onclick=this.callback.bind(this);
+  }
 }
+
+function icon_editor(icon, callback) {
+  this.new_icon_cancel=function() {
+    data_dir.commit_cancel();
+    this.win.close();
+    delete(this.win);
+  }
+
+  this.new_icon_finish=function() {
+    this.obj.save("einedatei", "huhu");
+    var ret=data_dir.commit_end("foobar");
+    callback(this.obj.id);
+    this.win.close();
+    delete(this.win);
+  }
+
+  this.win=new win("icon_editor");
+
+  data_dir.commit_start();
+  this.obj=icon_git.create_obj();
+  var x=this.obj.upload_form("file.src");
+  this.win.content.appendChild(x);
+
+  var input=dom_create_append(this.win.content, "input");
+  input.type="button";
+  input.value="Save";
+  input.onclick=this.new_icon_finish.bind(this);
+
+  var input=dom_create_append(this.win.content, "input");
+  input.type="button";
+  input.value="Cancel";
+  input.onclick=this.new_icon_cancel.bind(this);
+}
+
+function icon_chooser(current, callback) {
+  this.new_icon_callback=function(id) {
+    callback(id);
+    this.win.close();
+    delete(this.win);
+  }
+
+  this.new_icon=function() {
+    this.icon_editor=new icon_editor(null, this.new_icon_callback.bind(this));
+  }
+
+  this.choose_callback=function(id) {
+    callback(id);
+    this.win.close();
+    delete(this.win);
+  }
+
+  this.cancel=function() {
+    this.win.close();
+    delete(this.win);
+  }
+
+  this.win=new win("edit_icon");
+  this.win.content.innerHTML="Loading ...";
+
+  var obj_list=icon_git.obj_list();
+
+  this.win.content.innerHTML="";
+  var ul=dom_create_append(this.win.content, "ul");
+  
+  for(var i=0; i<obj_list.length; i++) {
+    obj_list[i].write_chooser(ul, this.choose_callback.bind(this));
+  }
+
+  var a=dom_create_append(this.win.content, "input");
+  a.type="button";
+  a.value="Create new icon";
+  a.onclick=this.new_icon.bind(this);
+
+  var a=dom_create_append(this.win.content, "input");
+  a.type="button";
+  a.value=t("cancel");
+  a.onclick=this.cancel.bind(this);
+}
+
+function icon_init() {
+  icon_git=new git_dir(data_dir, "icons", icon_obj);
+}
+
+register_hook("init", icon_init);
--- www/inc/icon.php
+++ www/inc/icon.php
@@ -1,16 +1,32 @@
 <?
-require_once("inc/git_directory.php");
+require_once("inc/data_dir.php");
 $icon_dir;
 
-class icon_file extends git_file {
+class icon_obj extends git_obj {
   function preprocess($files) {
+    $srcfile=$this->path("file.src");
+    $dstfile=$this->path("preview.png");
+
+    $finfo=finfo_open(FILEINFO_MIME_TYPE);
+    $mime=finfo_file($finfo, $srcfile);
+    finfo_close($finfo);
+
+    switch($mime) {
+      case "image/svg+xml":
+        // imagemagick in Ubuntu Jaunty/Lucid is broken ... use rsvg instead
+	$this->exec("rsvg {$srcfile} {$dstfile}");
+	break;
+      default:
+	$this->exec("convert {$srcfile} png:{$dstfile}");
+    }
   }
 }
 
 function icon_init() {
   global $icon_dir;
+  global $data_dir;
 
-  $icon_dir=new git_directory("icons", "icon_file");
+  $icon_dir=new git_dir($data_dir, "icons", "icon_obj");
 }
 
 register_hook("html_start", "icon_init");
