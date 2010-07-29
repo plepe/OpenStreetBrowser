26171cb488cdfcf3eaf28c980db63be6be1fbef8
Stephan Plepelits <skunk@xover.mud.at>
2010-07-29 08:35:20 +0200

    Icon Editor: Add tags and a commit message

--- www/inc/icon.js
+++ www/inc/icon.js
@@ -1,4 +1,5 @@
 var icon_git;
+var icon_tags_default={ "name": "", "source": "" };
 
 function icon_obj(dir, id, files) {
   this.inheritFrom=git_obj;
@@ -29,9 +30,32 @@ function icon_editor(icon, callback) {
     delete(this.win);
   }
 
+  this.check=function() {
+    if(!this.summary.value) {
+      alert("Please enter a summary of your changes!");
+      return false;
+    }
+
+    return true;
+  }
+
   this.new_icon_finish=function() {
-    this.obj.save("einedatei", "huhu");
-    var ret=data_dir.commit_end("foobar");
+    if(!this.check()) {
+      return;
+    }
+
+    // generate and save tags.xml file
+    var ret="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    ret+="<tags>\n";
+    this.tags.editor_update();
+    ret+=this.tags.xml("  ");
+    ret+="</tags>\n";
+    this.obj.save("tags.xml", ret);
+
+    // end commit with some message
+    var ret=data_dir.commit_end(this.summary.value);
+
+    // clean up
     callback(this.obj.id);
     this.win.close();
     delete(this.win);
@@ -41,8 +65,30 @@ function icon_editor(icon, callback) {
 
   data_dir.commit_start();
   this.obj=icon_git.create_obj();
+
+  var comment=dom_create_append(this.win.content, "div");
+  dom_create_append_text(comment, "Upload Icon:");
+
   var x=this.obj.upload_form("file.src");
   this.win.content.appendChild(x);
+
+  var comment=dom_create_append(this.win.content, "div");
+  dom_create_append_text(comment, "Tags");
+  
+  var a=dom_create_append(comment, "a");
+  a.target="_new";
+  a.href="http://wiki.openstreetmap.org/wiki/OpenStreetBrowser/Icons";
+  dom_create_append_text(a, "(Help)");
+
+  dom_create_append_text(comment, ":");
+
+  this.div_tags=dom_create_append(this.win.content, "div");
+  this.tags=new tags(icon_tags_default);
+  this.tags.editor(this.div_tags);
+
+  dom_create_append_text(this.win.content, "Edit summary:");
+  this.summary=dom_create_append(this.win.content, "input");
+  this.summary.name="summary";
 
   var input=dom_create_append(this.win.content, "input");
   input.type="button";
