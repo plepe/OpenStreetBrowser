ad03a67733a36f2d6bc5f48a494177da70c1277b
Stephan Plepelits <skunk@xover.mud.at>
2010-09-08 08:33:11 +0200

    Plugins: Export global plugin-vars to JS and PHP
    - global vars:
      {$plugin}_active
      {$plugin}_depend
      {$plugin}_tags

--- www/inc/plugins.php
+++ www/inc/plugins.php
@@ -8,14 +8,18 @@ function plugins_include($plugin) {
      (!file_exists("plugins/$plugin/conf.php")))
     return false;
 
-  include_once("plugins/$plugin/conf.php");
-
   $var_active="{$plugin}_active";
+  $var_depend="{$plugin}_depend";
+  $var_tags="{$plugin}_tags";
+
+  global $$var_active;
+  global $$var_depend;
+  global $$var_tags;
+
+  include_once("plugins/$plugin/conf.php");
 
   if(!$$var_active)
     return false;
-
-  $var_depend="{$plugin}_depend";
 
   if(is_array($$var_depend))
     foreach($$var_depend as $inc) {
@@ -25,7 +29,6 @@ function plugins_include($plugin) {
       }
     }
 
-  $var_tags="{$plugin}_tags";
   $plugins_list[$plugin]=$$var_tags;
 
   if(file_exists("plugins/$plugin/code.php"))
@@ -36,13 +39,40 @@ function plugins_include($plugin) {
 
 function plugins_html_head($plugin) {
   global $plugins_list;
+  global $plugins;
+  $plugins_script="";
+  $str="";
 
   foreach($plugins_list as $plugin=>$tags) {
+    $var_active="{$plugin}_active";
+    $var_depend="{$plugin}_depend";
+    $var_tags="{$plugin}_tags";
+    global $$var_active;
+    global $$var_depend;
+    global $$var_tags;
+
+    $plugins_script.="var {$var_active}=true;\n";
+    $plugins_script.="var {$var_depend}=".html_var_to_js($$var_depend).";\n";
+    $plugins_script.="var {$var_tags}=new tags(".html_var_to_js($$var_tags->data()).");\n";
+
     if(file_exists("plugins/$plugin/code.js"))
-      print "<script type='text/javascript' src='plugins/$plugin/code.js'></script>\n";
+      $str.="<script type='text/javascript' src='plugins/$plugin/code.js'></script>\n";
     if(file_exists("plugins/$plugin/style.css"))
-      print "<link rel='stylesheet' type='text/css' href=\"plugins/$plugin/style.css\">\n";
+      $str.="<link rel='stylesheet' type='text/css' href=\"plugins/$plugin/style.css\">\n";
   }
+
+  $plugins_script.="var plugins=".html_var_to_js($plugins).";\n";
+
+  $plugins_script.="var plugins_list={\n  ";
+  $list=array();
+  foreach($plugins_list as $plugin=>$tags) {
+    $list[]="$plugin: {$plugin}_tags";
+  }
+  $plugins_script.=implode(",\n  ", $list);
+  $plugins_script.="\n};\n";
+
+  print "<script type='text/javascript'>\n$plugins_script\n</script>\n";
+  print $str;
 }
 
 function plugins_init() {
@@ -54,6 +84,8 @@ function plugins_init() {
   foreach($plugins as $plugin) {
     plugins_include($plugin);
   }
+
+  $plugins=array_keys($plugins_list);
 
   closedir($d);
 }
