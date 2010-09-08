bbd0a8e29ed2850cfc4a731f163a98c64d0adeb3
Stephan Plepelits <skunk@xover.mud.at>
2010-09-08 07:40:46 +0200

    Plugins: Automatically include dependend plugins

--- www/inc/plugins.php
+++ www/inc/plugins.php
@@ -2,8 +2,36 @@
 $plugins_list=array();
 
 function plugins_include($plugin) {
+  global $plugins_list;
+
+  if((!file_exists("plugins/$plugin"))&&
+     (!file_exists("plugins/$plugin/conf.php")))
+    return false;
+
+  include_once("plugins/$plugin/conf.php");
+
+  $var_active="{$plugin}_active";
+
+  if(!$$var_active)
+    return false;
+
+  $var_depend="{$plugin}_depend";
+
+  if(is_array($$var_depend))
+    foreach($$var_depend as $inc) {
+      if(!$plugins_list[$inc]) {
+	if(!plugins_include($inc))
+	  return false;
+      }
+    }
+
+  $var_tags="{$plugin}_tags";
+  $plugins_list[$plugin]=$$var_tags;
+
   if(file_exists("plugins/$plugin/code.php"))
     include_once("plugins/$plugin/code.php");
+
+  return true;
 }
 
 function plugins_html_head($plugin) {
@@ -23,18 +51,8 @@ function plugins_init() {
   $plugins_list=array();
 
   $d=opendir("plugins/");
-  foreach($plugins as $plugin)
-    if((file_exists("plugins/$plugin"))&&
-       (file_exists("plugins/$plugin/conf.php"))) {
-    include_once("plugins/$plugin/conf.php");
-
-    $var="{$plugin}_active";
-    if($$var) {
-      plugins_include($plugin);
-
-      $var="{$plugin}_tags";
-      $plugins_list[$plugin]=$$var;
-    }
+  foreach($plugins as $plugin) {
+    plugins_include($plugin);
   }
 
   closedir($d);
