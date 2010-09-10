f467c594dec78de48e9314f7eb2b3197281b8b85
Stephan Plepelits <skunk@xover.mud.at>
2010-09-10 18:14:30 +0200

    Plugin 'search': Convert search field to plugin

--- www/plugins/search/code.php
+++ www/plugins/search/code.php
@@ -70,3 +70,13 @@ function ajax_search($param, $xml) {
   objects_to_xml($load_xml, $xml, $osm, 1, $bounds);
 }
 
+function search_menu_show($list) {
+  $menu_list[]=array(-6,
+    "<div id='search' class='search' style='position:absolute; top:143px;'>\n".
+    "<form name='osb_search_form_name' id='osb_search_form' action='javascript:search()'>\n".
+    "<input name='osb_search' id='search' style='border-color:#999999;' value='".lang('search_field')."' onFocus='search_focus(this)' onkeyup='search_brush(this,event)' onblur='search_onblur(this)' 'title='".lang('search_tip')."'/>\n".
+    "<img name='brush' src='plugins/search/brush.png' border='0' alt='' title='".lang('search_clear')."' style='position:absolute; right:3px; top:6px; visibility:hidden; cursor:pointer;' onclick='search_clear(document.osb_search_form_name.osb_search)' onmousedown='if (event.preventDefault) event.preventDefault()'>\n".
+    "</form></div>\n");
+}
+
+register_hook("menu_show", search_menu_show);
