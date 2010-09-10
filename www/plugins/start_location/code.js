3ff8df0cfa3f7bda4537de48c7f45b00d09dcff8
Stephan Plepelits <skunk@xover.mud.at>
2010-09-10 16:49:52 +0200

    Plugin 'start_location': Toolbox to change viewport after startup
    - Converted from former functionality map_location

--- www/plugins/start_location/code.js
+++ www/plugins/start_location/code.js
@@ -1,6 +1,6 @@
-var map_location_toolbox;
+var start_location_toolbox;
 
-function map_location_options() {
+function start_location_options() {
   var form=document.getElementById("startform");
   var start_value=null;
   for(var i=0; i<form.elements["start_value"].length; i++) {
@@ -13,11 +13,11 @@ function map_location_options() {
   } else {
     cookie_delete("start_value");
   }
-  map_location_start(start_value);
-  map_location_toolbox.deactivate();
+  start_location_start(start_value);
+  start_location_toolbox.deactivate();
 }
 
-function map_location_start(start_value) {
+function start_location_start(start_value) {
   switch (start_value) {
     case "geolocation":
       geo_init();
@@ -42,7 +42,7 @@ function map_location_start(start_value) {
   }
 }
 
-function map_location_activate() {
+function start_location_activate() {
   var text = "<i>"+t("start:choose")+":</i><br><form id=\"startform\" style=\"margin-bottom:3px;\">";
   if (navigator.geolocation) {
     text += "<input type=\"radio\" name=\"start_value\" id=\"geolocation\" value=\"geolocation\"><label for=\"geolocation\">"+t("start:geolocation")+"</label></br>";
@@ -54,9 +54,9 @@ function map_location_activate() {
     text += "<input type=\"radio\" name=\"start_value\" id=\"savedview\" value=\"savedview\"><label for=\"savedview\">"+t("start:savedview")+"</label></br>";
   }
   text += "<input type=\"radio\" name=\"start_value\" id=\"startnormal\" value=\"startnormal\"><label for=\"startnormal\">"+t("start:startnormal")+"</label></br>";
-  text += "</br><input type=\"button\" name=\"start\" value=\"ok\" onclick=\"map_location_options()\"><input type=\"checkbox\" name=\"start_save\" id=\"save\" value=\"save\"><label for=\"save\">"+t("start:remember")+"</label></br></form>";
+  text += "</br><input type=\"button\" name=\"start\" value=\"ok\" onclick=\"start_location_options()\"><input type=\"checkbox\" name=\"start_save\" id=\"save\" value=\"save\"><label for=\"save\">"+t("start:remember")+"</label></br></form>";
 
-  map_location_toolbox.content.innerHTML=text;
+  start_location_toolbox.content.innerHTML=text;
 
   var c=cookie_read('start_value');
   if(c) {
@@ -67,22 +67,22 @@ function map_location_activate() {
   }
 }
 
-function map_location_init() {
-  map_location_toolbox=new toolbox({
-    icon: "img/toolbox_map.png",
+function start_location_init() {
+  start_location_toolbox=new toolbox({
+    icon: "plugins/start_location/icon.png",
     icon_title: "map position",
     weight: -5,
-    callback_activate: map_location_activate
+    callback_activate: start_location_activate
   });
-  register_toolbox(map_location_toolbox);
+  register_toolbox(start_location_toolbox);
 
   if(((location.hash=="") || (location.hash=="#")) && (cookie_read("start_value")==null)) {
-    map_location_toolbox.activate();
+    start_location_toolbox.activate();
   }
   else {
     lastview=cookie_read("_osb_location");
-    map_location_start(cookie_read("start_value"));
+    start_location_start(cookie_read("start_value"));
   }
 }
 
-register_hook("init", map_location_init);
+register_hook("init", start_location_init);
