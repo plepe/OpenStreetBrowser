dc253d921c4cfe1061d7dc5388558b1cbdda3e24
Stephan Plepelits <skunk@xover.mud.at>
2010-09-11 10:21:08 +0200

    DOM-Functions: Imported dom_* functions from category-tree
    - dom_create_append(parent, type[, root])
      Append an element of type 'type' to parent
      E.g. div=dom_create_append(parent_node, 'div');
    - dom_create_append_text(parent, text[, root])
      Append a text node with text 'text' to parent
      E.g. dom_create_append(parent_node, "Comment");
    - dom_clean(parent)
      Removes all children of parent

--- www/inc/functions.js
+++ www/inc/functions.js
@@ -65,3 +65,26 @@ function weight_sort(arr) {
 
   return ret2;
 }
+
+function dom_create_append(parent, type, xml) {
+  if(!xml)
+    xml=document;
+
+  var x=xml.createElement(type);
+  parent.appendChild(x);
+  return x;
+}
+
+function dom_create_append_text(parent, text, xml) {
+  if(!xml)
+    xml=document;
+
+  var x=xml.createTextNode(text);
+  parent.appendChild(x);
+  return x;
+}
+
+function dom_clean(parent) {
+  while(parent.firstChild)
+    parent.removeChild(parent.firstChild);
+}
--- www/inc/functions.php
+++ www/inc/functions.php
@@ -33,3 +33,21 @@ function weight_sort($arr) {
 
   return $ret2;
 }
+
+function dom_create_append($parent, $type, $xml) {
+  $x=$xml->createElement($type);
+  $parent->appendChild($x);
+  return $x;
+}
+
+function dom_create_append_text($parent, $text, $xml) {
+  $x=$xml->createTextNode($text);
+  $parent->appendChild($x);
+  return $x;
+}
+
+function dom_clean($parent) {
+  while($parent->firstChild)
+    $parent->removeChild($parent->firstChild);
+}
+
