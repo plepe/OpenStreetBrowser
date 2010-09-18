4422e81c5bf338cfe14def836cf88641612e9926
Stephan Plepelits <skunk@xover.mud.at>
2010-09-18 08:49:01 +0200

    Plugin 'tab': Create a tabbed menu for windows etc.

---
+++ www/plugins/tab/code.js
@@ -0,0 +1,94 @@
+function tab(options) {
+  // activate
+  this.activate=function() {
+    this.manager.activate(this);
+  }
+
+  // deactivate
+  this.deactivate=function() {
+    this.manager.deactivate(this);
+  }
+
+  // constructor
+  this.content=document.createElement("div");
+  this.content.className="tab_content";
+  this.header=document.createElement("span");
+  this.header.className="tab_header";
+  this.header.onclick=this.activate.bind(this);
+  this.manager=null;
+
+  this.options=options;
+  if(!this.options)
+    this.options={};
+
+  if(!this.options.name)
+    this.options.name=t("unnamed");
+  dom_create_append_text(this.header, this.options.name);
+
+  if(!this.options.weight)
+    this.options.weight=0;
+}
+
+function tab_manager(div) {
+  // register_tab
+  this.register_tab=function(tab) {
+    this.tabs.push(tab);
+
+    this.header.appendChild(tab.header);
+    this.content.appendChild(tab.content);
+    tab.manager=this;
+  }
+
+  // show
+  this.show=function() {
+    var list=[];
+    for(var i=0; i<this.tabs.length; i++) {
+      list.push([ this.tabs[i].options.weight, this.tabs[i] ]);
+    }
+    list=weight_sort(list);
+
+    for(var i=0; i<list.length; i++) {
+      this.header.appendChild(list[i].header);
+    }
+  }
+
+  // activate
+  this.activate=function(tab) {
+    if(this.active_tab)
+      this.deactivate(this.active_tab);
+
+    this.active_tab=tab;
+    tab.header.className="tab_header_active";
+    tab.content.className="tab_content_active";
+
+    if(this.active_tab&&this.active_tab.on_activate)
+      this.active_tab.on_activate();
+  }
+
+  // deactivate
+  this.deactivate=function(tab) {
+    if(this.active_tab!=tab)
+      return;
+
+    if(tab.on_deactivate)
+      tab.on_deactivate();
+
+    tab.header.className="tab_header";
+    tab.content.className="tab_content";
+  }
+  
+  // constructor
+  this.tabs=[];
+  this.active_tab=null;
+  this.div=document.createElement("div");
+  this.div.className="tab_manager";
+  if(div)
+    div.appendChild(this.div);
+  this.content=document.createElement("div");
+  this.content.className="tab_manager_content";
+  this.header=document.createElement("div");
+  this.header.className="tab_manager_header";
+  this.div.appendChild(this.header);
+  this.div.appendChild(this.content);
+}
+
