052ce668a756daa03672f4361db70e226de812eb
Stephan Plepelits <skunk@xover.mud.at>
2010-09-23 13:04:48 +0200

    Cache: Some documentation

--- src/sql/01_cache.sql
+++ src/sql/01_cache.sql
@@ -1,3 +1,25 @@
+-- Cache functions --
+-- cache_insert(id, k, v[, depend])
+--   inserts a key/value pair into the cache
+--   - id is the string representation of an object (e.g. 'rel_1234'). If this
+--     object already exists in the cache, it will be updated
+--   - k is the key (a string)
+--   - v is a value and will be handled as string
+--   - depend is an array of depending objects, e.g. a the geometric
+--     representation of a way depends on the way and it's nodes. You don't
+--     have to add the id of the object to the depend-array.
+--   - returns the value
+-- Example: cache_insert('rel_1234', 'foo', 'bar', Array['node_1']);
+-- 
+-- cache_search(id, k)
+--   returns a cached value for this object, if no value exists 'null' is
+--   returned
+-- Example: cache_search('rel_1234', 'foo') -> 'bar'
+--
+-- cache_remove(id)
+--   deletes all values which belong to this object or depend on it
+-- Example: cache_remove('node_1')
+
 drop table if exists osm_cache;
 create table osm_cache (
   osm_id		text		not null,
--- www/inc/cache.php
+++ www/inc/cache.php
@@ -1,4 +1,26 @@
 <?
+// Cache functions //
+// cache_insert($id, $k, $v[, $depend])
+//   inserts a key/value pair into the cache
+//   - $id is the string representation of an object (e.g. 'rel_1234'). If this
+//     object already exists in the cache, it will be updated
+//   - $k is the key (a string)
+//   - $v is a value and will be handled as string
+//   - $depend is an array of depending objects, e.g. a the geometric
+//     representation of a way depends on the way and it's nodes. You don't
+//     have to add the id of the object to the depend-array.
+//   - returns the value
+// Example: cache_insert('rel_1234', 'foo', 'bar', Array['node_1']);
+// 
+// cache_search($id, $k)
+//   returns a cached value for this object, if no value exists 'null' is
+//   returned
+// Example: cache_search('rel_1234', 'foo') -> 'bar'
+//
+// cache_remove($id)
+//   deletes all values which belong to this object or depend on it
+// Example: cache_remove('node_1')
+
 function cache_search($osm_id, $k) {
   $pg_osm_id=postgre_escape($osm_id);
   $pg_k=postgre_escape($k);
