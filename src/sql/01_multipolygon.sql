94ff64e9d165ce3fa74a3a087c032bfd328d98e4
Stephan Plepelits <skunk@xover.mud.at>
2010-09-23 10:07:53 +0200

    DB/Create Multipolygons: Fix creation

--- src/sql/01_multipolygon.sql
+++ src/sql/01_multipolygon.sql
@@ -24,8 +24,12 @@ begin
 
   -- first find all closed geometries in array and push into done
   for i in array_lower(src, 1)..array_upper(src, 1) loop
-    if (IsClosed(src[i])) and NPoints(src[i])>3 then
+    if src[i] is null then
+      raise notice 'got null geometry, index %', i;
+    elsif (IsClosed(src[i])) and NPoints(src[i])>3 then
       done:=array_append(done, ST_MakePolygon(src[i]));
+    elsif not ST_IsValid(src[i]) then
+      raise notice 'ignore invalid line %', i;
     else
       todo:=array_append(todo, src[i]);
     end if;
@@ -37,7 +41,7 @@ begin
   -- if those build a closed geometry
   if ST_NumGeometries(cur) is null then
     if IsClosed(cur) and NPoints(cur)>3 then
-      done:=array_append(done, cur);
+      done:=array_append(done, MakePolygon(cur));
     end if;
   else
   -- several geometries? check each of them ...
@@ -60,10 +64,13 @@ as $$
 declare
   outer_ways	geometry;
   inner_ways	geometry;
+  next		geometry;
+  i		int;
 begin
   if $1 is null then
     return null;
   end if;
+
   outer_ways:=make_multipolygon($1);
 
   if outer_ways is null then
@@ -82,10 +89,16 @@ begin
     return outer_ways;
   end if;
 
-  if not IsValid(inner_ways) then
-    return outer_ways;
-  end if;
+  -- raise notice E'outer_ways (%): %\ninner_ways (%): %', NumGeometries(outer_ways), astext(outer_ways), NumGeometries(inner_ways), astext(inner_ways);
 
-  return ST_Difference(outer_ways, inner_ways);
+  -- substract the inner ways one by one
+  for i in 1..ST_NumGeometries(inner_ways) loop
+    next:=ST_GeometryN(inner_ways, i);
+    if ST_IsValid(next) then
+      outer_ways:=ST_Difference(outer_ways, next);
+    end if;
+  end loop;
+
+  return outer_ways; -- ST_Difference(outer_ways, inner_ways);
 end;
 $$ language 'plpgsql';
