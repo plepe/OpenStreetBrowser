80089de114be93ff8a66e5976966cd29fec811a9
Stephan Plepelits <skunk@xover.mud.at>
2010-09-23 17:42:57 +0200

    Cache: Add an interval when values should be outdated

--- www/inc/cache.php
+++ www/inc/cache.php
@@ -1,6 +1,6 @@
 <?
 // Cache functions //
-// cache_insert($id, $k, $v[, $depend])
+// cache_insert($id, $k, $v[, $depend][, $outdate])
 //   inserts a key/value pair into the cache
 //   - $id is the string representation of an object (e.g. 'rel_1234'). If this
 //     object already exists in the cache, it will be updated
@@ -9,8 +9,11 @@
 //   - $depend is an array of depending objects, e.g. a the geometric
 //     representation of a way depends on the way and it's nodes. You don't
 //     have to add the id of the object to the depend-array.
+//   - $outdate specifies an interval after which the content will be
+//     deprecated. Specify the interval as pgsql interval, 
+//     e.g. '5 days 3 hours'. Defaults to '1 month'.
 //   - returns the value
-// Example: cache_insert('rel_1234', 'foo', 'bar', Array['node_1']);
+// Example: cache_insert('rel_1234', 'foo', 'bar', Array['node_1'], '1 hour');
 // 
 // cache_search($id, $k)
 //   returns a cached value for this object, if no value exists 'null' is
@@ -20,6 +23,9 @@
 // cache_remove($id)
 //   deletes all values which belong to this object or depend on it
 // Example: cache_remove('node_1')
+//
+// cache_clean()
+//   deletes all outdated entries
 
 function cache_search($osm_id, $k) {
   $pg_osm_id=postgre_escape($osm_id);
@@ -32,7 +38,7 @@ function cache_search($osm_id, $k) {
   return null;
 }
 
-function cache_insert($osm_id, $k, $content, $depend=array()) {
+function cache_insert($osm_id, $k, $content, $depend=array(), $interval=null) {
   $pg_osm_id=postgre_escape($osm_id);
   $pg_k=postgre_escape($k);
   $pg_content=postgre_escape($content);
@@ -47,7 +53,12 @@ function cache_insert($osm_id, $k, $content, $depend=array()) {
     $pg_depend="null::text[]";
   }
 
-  sql_query("select cache_insert({$pg_osm_id}, {$pg_k}, {$pg_content}, {$pg_depend})");
+  if($interval)
+    $pg_interval=postgre_escape($interval);
+  else
+    $pg_interval="null";
+
+  sql_query("select cache_insert({$pg_osm_id}, {$pg_k}, {$pg_content}, {$pg_depend}, {$pg_interval}::interval)");
 
   return $content;
 }
@@ -56,4 +67,8 @@ function cache_remove($depend_id) {
   $pg_depend_id=postgre_escape($depend_id);
 
   sql_query("select cache_remove($pg_depend_id)");
+}
+
+function cache_clean() {
+  sql_query("select cache_clean()");
 }
