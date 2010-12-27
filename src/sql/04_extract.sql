95d0ad74e6ce9ad130a3e74549fd4cac8bf50207
Stephan Plepelits <skunk@xover.mud.at>
2010-12-27 07:13:26 +0100

    Extract: Waterways with >10m, osm_polygon_extract_idx > 1mio area

--- src/sql/04_extract.sql
+++ src/sql/04_extract.sql
@@ -1,9 +1,11 @@
 -- osm_polygon_extract
 create view osm_polygon_extract as
   select * from osm_polygon where ST_Area(osm_way)>1000000;
-create index osm_polygon_extract_idx on osm_polygon using gist(osm_way, ST_Area(osm_way));
+create index osm_polygon_extract_idx on osm_polygon using gist(osm_way, osm_tags) where
+  ST_Area(osm_way)>1000000;
 
 -- osm_line_extract
+drop view osm_line_extract;
 create view osm_line_extract as
   select * from osm_line where
     osm_tags->'highway' in ('motorway', 'trunk', 'primary', 'secondary', 'tertiary') or
@@ -11,14 +13,15 @@ create view osm_line_extract as
     osm_tags->'power' in ('line') or
     osm_tags->'man_made' in ('pipeline') or
     osm_tags->'aeroway' in ('runway') or
-    osm_tags->'waterway' in ('river', 'canal');
+    (osm_tags->'waterway' in ('river', 'canal') and coalesce(parse_number(osm_tags->'width'), parse_number(osm_tags->'est_width'))>10);
+drop index osm_line_extract_idx;
 create index osm_line_extract_idx on osm_line using gist(osm_way, osm_tags) where
     osm_tags->'highway' in ('motorway', 'trunk', 'primary', 'secondary', 'tertiary') or
     osm_tags->'railway' in ('rail') or
     osm_tags->'power' in ('line') or
     osm_tags->'man_made' in ('pipeline') or
     osm_tags->'aeroway' in ('runway') or
-    osm_tags->'waterway' in ('river', 'canal');
+    (osm_tags->'waterway' in ('river', 'canal') and coalesce(parse_number(osm_tags->'width'), parse_number(osm_tags->'est_width'))>10);
 
 -- osm_polygon_water
 create view osm_polygon_water as
