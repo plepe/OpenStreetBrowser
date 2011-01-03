997224f55951dd9c77ef394943fece486741371c
OpenStreetMap <osm@pitr.(none)>
2011-01-03 07:51:09 +0100

    Hide JavaScript-load info when design hidden

--- www/plugins/cloudmade_basemap/code.php
+++ www/plugins/cloudmade_basemap/code.php
@@ -1,8 +1,10 @@
-<script type="text/javascript" src="plugins/cloudmade_basemap/cloudmade.js"></script>
 <?
 function cloudmade_basemap_init() {
   global $key_cloudmade_api;
+  global $design_hidden;
 
+  if(!$design_hidden)
+    print '<script type="text/javascript" src="plugins/cloudmade_basemap/cloudmade.js"></script>\n';
   html_export_var(array("key_cloudmade_api"=>$key_cloudmade_api));
 }
 
