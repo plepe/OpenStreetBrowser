a9ce90fe762430fec71eea30f09a7404232ea8d5
Stephan Plepelits <skunk@xover.mud.at>
2011-01-07 18:58:54 +0100

    Download Lang: Process all available languages

--- www/scripts/download_lang.php
+++ www/scripts/download_lang.php
@@ -2,34 +2,50 @@
 <?
 require "../../conf.php";
 
-$lang="fr";
-$f=fopen("http://wiki.openstreetmap.org/w/index.php?title=OpenStreetBrowser/Languages/French&action=raw", "r");
+$languages=array(
+  "en"=>"English",
+  "de"=>"German",
+  "fr"=>"French",
+  "ja"=>"Japanese",
+  "uk"=>"Ukrainian",
+  "it"=>"Italian",
+);
 
-unset($file);
-while($r=fgets($f)) {
-  if(eregi("==== (File: )?(.*) ====", $r, $m)) {
-    if($m[2]=="Statistics") {
+function parse($lang, $wikipage) {
+  global $root_path;
+
+  $f=fopen("http://wiki.openstreetmap.org/w/index.php?title=OpenStreetBrowser/Languages/$wikipage&action=raw", "r");
+  unset($file);
+  while($r=fgets($f)) {
+    if(eregi("==== (File: )?(.*) ====", $r, $m)) {
+      if($m[2]=="Statistics") {
+	if($w)
+	  fclose($w);
+	continue;
+      }
+
+      $file=$m[2];
+      if(eregi("^(.*)en\.(.*)$", $file, $m)) {
+	$file="$m[1]$lang.$m[2]";
+      }
+      print "Writing to $file\n";
       if($w)
 	fclose($w);
-      continue;
+      $w=fopen("$root_path/$file", "w");
     }
+    elseif(eregi("<\/?syntaxhigh", $r)) {
+    }
+    else {
+      if($w)
+	fwrite($w, $r);
+    }
+  }
 
-    $file=$m[2];
-    if(eregi("^(.*)en\.(.*)$", $file, $m)) {
-      $file="$m[1]$lang.$m[2]";
-    }
-    print "Writing to $file\n";
-    if($w)
-      fclose($w);
-    $w=fopen("$root_path/$file", "w");
-  }
-  elseif(eregi("<\/?syntaxhigh", $r)) {
-  }
-  else {
-    if($w)
-      fwrite($w, $r);
-  }
+  if($w)
+    fclose($w);
 }
 
-if($w)
-  fclose($w);
+foreach($languages as $lang=>$wikipage) {
+  parse($lang, $wikipage);
+}
+
