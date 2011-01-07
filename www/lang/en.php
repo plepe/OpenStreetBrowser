c6e45de5681f17b5934b159feeb9b3a25f2d4352
Stephan Plepelits <skunk@xover.mud.at>
2011-01-07 20:26:03 +0100

    Language Strings: Add new translations to list

--- www/lang/en.php
+++ www/lang/en.php
@@ -123,6 +123,10 @@ $lang_str["lang:bg"]="Bulgarian";
 $lang_str["lang:en"]="English";
 $lang_str["lang:es"]="Spanish";
 $lang_str["lang:it"]="Italian";
+$lang_str["lang:fr"]="French";
+$lang_str["lang:uk"]="Ukrainian";
+$lang_str["lang:ru"]="Russian";
+$lang_str["lang:ja"]="Japanese";
 
 // the following language strings are deprecated
 $lang_str["cat:leisure"]=array( "Leisure, Sport and Shopping " );
