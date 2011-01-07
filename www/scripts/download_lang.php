4d844d2b4ec08e196e0437339d99c1e5158ca855
Stephan Plepelits <skunk@xover.mud.at>
2011-01-07 18:47:47 +0100

    Download Lang: Download a language from OSM Wiki

---
+++ www/scripts/download_lang.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?
+require "../../conf.php";
+
+$lang="fr";
+$f=fopen("http://wiki.openstreetmap.org/w/index.php?title=OpenStreetBrowser/Languages/French&action=raw", "r");
+
+unset($file);
+while($r=fgets($f)) {
+  if(eregi("==== (.*) ====", $r, $m)) {
+    $file=$m[1];
+    print "Writing to $file\n";
+    if($w)
+      fclose($w);
+    $w=fopen("$root_path/$file", "w");
+  }
+  elseif(eregi("<\/?syntaxhigh", $r)) {
+  }
+  else {
+    if($w)
+      fwrite($w, $r);
+  }
+}
+
+if($w)
+  fclose($w);
