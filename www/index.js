de7517eefd510c4d2e1725ee625b69b8fa8a1df8
Stephan Plepelits <skunk@xover.mud.at>
2011-01-09 20:29:58 +0100

    Map: Set the ScaleLine to 'geodesic' (it will re-calculate after move)

--- www/index.js
+++ www/index.js
@@ -304,7 +304,7 @@ function init() {
   };
 
   map.addControl(new OpenLayers.Control.MousePosition());
-  map.addControl(new OpenLayers.Control.ScaleLine());
+  map.addControl(new OpenLayers.Control.ScaleLine({ geodesic: true }));
 
   redraw_timer=setTimeout("check_redraw()", 300);
   register_hook("hash_changed", redraw);
