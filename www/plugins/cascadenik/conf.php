b868fce80e883dc0ad24e1ad6df4e0b978112009
Stephan Plepelits <skunk@xover.mud.at>
2011-01-10 19:55:16 +0100

    Plugin 'Cascadenik': Initial version, compile files to .mapnik

---
+++ www/plugins/cascadenik/conf.php
@@ -0,0 +1,11 @@
+<?
+$cascadenik_active=true;
+
+$cascadenik_depend=array("db", "mapnik");
+
+$cascadenik_tags=new tags(array(
+  "name"=>"Cascadenik",
+  "Author"=>"Stephan Plepelits",
+));
+
+
---
+++ www/plugins/cascadenik/mcp.php
@@ -0,0 +1,21 @@
+<?
+function cascadenik_compile() {
+  global $plugins_list;
+  global $plugins_dir;
+
+  print "MAPNIK BUILD RENDERD\n";
+  foreach($plugins_list as $plugin=>$tags) {
+    $d=opendir("$plugins_dir/$plugin");
+    while($r=readdir($d)) {
+      if((substr($r, 0, 1)!=".")&&(preg_match("/^(.*)\.mml$/", $r, $m))) {
+	print "Cascadenik: Found $plugin -> $m[1]\n";
+
+	system("cascadenik-compile.py $plugins_dir/$plugin/$m[1].mml $plugins_dir/$plugin/$m[1].xml");
+	rename("$plugins_dir/$plugin/$m[1].xml", "$plugins_dir/$plugin/$m[1].mapnik");
+      }
+    }
+    closedir($d);
+  }
+}
+
+register_hook("mapnik_compile", "cascadenik_compile");
