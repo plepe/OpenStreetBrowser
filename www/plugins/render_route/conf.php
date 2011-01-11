9311316416ae1aeb8657005084445a90ee0017b6
Stephan Plepelits <skunk@xover.mud.at>
2011-01-11 06:10:50 +0100

    Plugin 'Render Route': Add the stations_top-layer

--- www/plugins/render_route/conf.php
+++ www/plugins/render_route/conf.php
@@ -1,7 +1,7 @@
 <?
 $render_route_active=true;
 
-$render_route_depend=array("db", "route", "cascadenik");
+$render_route_depend=array("db", "route", "cascadenik", "stops");
 
 $render_route_tags=new tags(array(
   "name"=>"Render Route",
