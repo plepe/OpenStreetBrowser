3b7c3851294452902310148111ae9a5e28b9827f
Stephan Plepelits <skunk@xover.mud.at>
2011-01-24 07:05:50 +0100

    Plugin 'Cascadenik SVG': Make use of $tmp_dir
    - Save modified mml and mss file to $tmp_dir
    - When converting svg to png, save copy of source and png-file to $tmp_dir
    - For all non-svg-images write the full path to source to mss file

--- www/plugins/cascadenik_svg/mcp.php
+++ www/plugins/cascadenik_svg/mcp.php
@@ -1,13 +1,20 @@
 <?
 function cascadenik_svg_process($infile, $outfile, $path) {
+  global $tmp_dir;
   $f1=fopen("$path/$infile", "r");
-  $f2=fopen("$path/$outfile", "w");
+  $f2=fopen("$tmp_dir/$outfile", "w");
 
   while($r=fgets($f1)) {
-    while(preg_match("/^(.*)url\(\'(.*)\.svg\'\)(.*)$/", $r, $m)) {
-      print "Converting $m[2].svg to $m[2].png\n";
-      system("rsvg $path/$m[2].svg $path/$m[2].png");
-      $r="$m[1]url('$m[2].png')$m[3]";
+    if(preg_match("/^(.*)url\(\'(.*)\/([^\/]*)\.([a-zA-Z]{3})\'\)(.*)$/", $r, $m)) {
+      if($m[4]=="svg") {
+	print "Converting $m[2]/$m[3].svg to $m[3].png\n";
+	copy("$path/$m[2]/$m[3].svg", "$tmp_dir/$m[3].svg");
+	system("rsvg $tmp_dir/$m[3].svg $tmp_dir/$m[3].png");
+	$r="$m[1]url('$tmp_dir/$m[3].png')$m[5]";
+      }
+      else {
+	$r="$m[1]url('$path/$m[2]/$m[3].$m[4]')$m[5]";
+      }
     }
 
     fwrite($f2, $r);
@@ -18,22 +25,23 @@ function cascadenik_svg_process($infile, $outfile, $path) {
 }
 
 function cascadenik_svg_compile($file) {
-  $nfile="$file.cascadenik_svg.mml";
-  if(!preg_match("/^(.*\/)[^\/]*$/", $file, $m)) {
+  global $tmp_dir;
+
+  if(!preg_match("/^(.*\/)([^\/]*)$/", $file, $m)) {
     print "Mapnik Rotate: No path found???\n";
     return;
   }
   $path=$m[1];
+  $nfile="$tmp_dir/$m[2]";
 
   $dom=new DOMDocument();
   $dom->load($file);
 
   $list=$dom->getElementsByTagName("Stylesheet");
   for($i=0; $i<$list->length; $i++) {
-    print "$i\n";
     $mss_file=$list->item($i)->getAttribute("src");
-    cascadenik_svg_process($mss_file, $mss_file.".cascadenik_svg", $path);
-    $list->item($i)->setAttribute("src", $mss_file.".cascadenik_svg");
+    cascadenik_svg_process($mss_file, $mss_file, $path);
+    $list->item($i)->setAttribute("src", "$tmp_dir/$mss_file");
   }
 
   $dom->save($nfile);
