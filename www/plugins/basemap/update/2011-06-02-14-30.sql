8df1262e18e2d6ede5fbf64320dd1f993dda26ba
Stephan Plepelits <skunk@xover.mud.at>
2011-02-06 14:38:32 +0100

    Plugin 'Basemap': Use classify_hmatch for landuse layers

---
+++ www/plugins/basemap/update/2011-06-02-14-30.sql
@@ -0,0 +1,443 @@
+-- basemap_landuse
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>park',
+  null,
+  '#landuse=>park'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>village_green',
+  null,
+  '#landuse=>park'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>recreation_ground',
+  null,
+  '#landuse=>park'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>grass',
+  null,
+  '#landuse=>park'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>golf_course',
+  null,
+  '#landuse=>sport, #landuse_subtype=>t0'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>playground',
+  null,
+  '#landuse=>sport, #landuse_subtype=>t0'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>sports_centre',
+  null,
+  '#landuse=>sport, #landuse_subtype=>t0'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>track',
+  null,
+  '#landuse=>sport, #landuse_subtype=>t0'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>pitch',
+  null,
+  '#landuse=>sport, #landuse_subtype=>t0'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>water_park',
+  null,
+  '#landuse=>sport, #landuse_subtype=>t0'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>piste',
+  null,
+  '#landuse=>sport, #landuse_subtype=>t1'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>nature_reserve',
+  null,
+  '#landuse=>nature_reserve'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>wood',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>forest',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>wood',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>scrub',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>wetland',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t1'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>marsh',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t1'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>glacier',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t2'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>scree',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t3'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>heath',
+  null,
+  '#landuse=>natural0, #landuse_subtype=>t3'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>mud',
+  null,
+  '#landuse=>natural1, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>mud',
+  null,
+  '#landuse=>natural1, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>beach',
+  null,
+  '#landuse=>natural1, #landuse_subtype=>t1'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>cliff',
+  null,
+  '#landuse=>natural1, #landuse_subtype=>t2'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>rock',
+  null,
+  '#landuse=>natural1, #landuse_subtype=>t2'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>cemetery',
+  null,
+  '#landuse=>cemetery'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>common',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'leisure=>garden',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>meadow',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>meadow',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'natural=>fell',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>farm',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>greenhouse_horticulture',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>farmyard',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>farmland',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>vineyard',
+  null,
+  '#landuse=>garden'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>orchard',
+  null,
+  '#landuse=>garden'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>quarry',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>landfill',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>brownfield',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>railway',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>construction',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>industrial',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>bus_station',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'aeroway=>aerodrome',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'aeroway=>apron',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'power=>station',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'power=>sub_station',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'power=>generator',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t0'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>military',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t1'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'military=>barracks',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t1'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'military=>airfield',
+  null,
+  '#landuse=>industrial, #landuse_subtype=>t1'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>residential',
+  null,
+  '#landuse=>residential'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>allotments',
+  null,
+  '#landuse=>residential'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  '',
+  'historic',
+  '#landuse=>historic'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  '',
+  'tourism',
+  '#landuse=>tourism'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'landuse=>school',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>college',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>cinema',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>kindergarten',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>library',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>school',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>theatre',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>arts_centre',
+  null,
+  '#landuse=>education'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>university',
+  null,
+  '#landuse=>education'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>hospital',
+  null,
+  '#landuse=>emergency'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>emergency_phone',
+  null,
+  '#landuse=>emergency'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>fire_station',
+  null,
+  '#landuse=>emergency'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>police',
+  null,
+  '#landuse=>emergency'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>pharmacy',
+  null,
+  '#landuse=>health'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>baby_hatch',
+  null,
+  '#landuse=>health'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>dentist',
+  null,
+  '#landuse=>health'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>doctors',
+  null,
+  '#landuse=>health'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>veterinary',
+  null,
+  '#landuse=>health'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>government',
+  null,
+  '#landuse=>public'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>gouvernment',
+  null,
+  '#landuse=>public'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>public_building',
+  null,
+  '#landuse=>public'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>court_house',
+  null,
+  '#landuse=>public'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>embassy',
+  null,
+  '#landuse=>public'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>prison',
+  null,
+  '#landuse=>public'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>townhall',
+  null,
+  '#landuse=>public'
+);
+
+insert into classify_hmatch values ( 'basemap_landuse',
+  'amenity=>marketplace',
+  null,
+  '#landuse=>shop'
+);
+insert into classify_hmatch values ( 'basemap_landuse',
+  '',
+  'shop',
+  '#landuse=>shop'
+);
