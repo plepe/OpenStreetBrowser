bd38aa064e4ebe1c59b9d8cb32bc8b18df007851
Stephan Plepelits <skunk@xover.mud.at>
2011-02-06 17:23:13 +0100

    Plugin 'Basemap': Use classify_hmatch to process buildings and squares

---
+++ www/plugins/basemap/update/2011-06-02-17-30.sql
@@ -0,0 +1,436 @@
+-- basemap_building
+insert into classify_hmatch values ('basemap_building',
+  'building=>no',
+  null,
+  '',
+  2
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>place_of_worship',
+  null,
+  '#building=>worship',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'railway=>station',
+  null,
+  '#building=>road_amenities',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'railway=>platform',
+  null,
+  '#building=>road_amenities',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'aeroway=>terminal',
+  null,
+  '#building=>road_amenities',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'aeroway=>helipad',
+  null,
+  '#building=>road_amenities',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'aerialway=>station',
+  null,
+  '#building=>road_amenities',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>ferry_terminal',
+  null,
+  '#building=>road_amenities',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'barrier=>hedge',
+  null,
+  '#building=>nature_building',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'barrier=>fence',
+  null,
+  '#building=>nature_building',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'power=>generator',
+  null,
+  '#building=>industrial',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'man_made=>gasometer',
+  null,
+  '#building=>industrial',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'man_made=>wastewater_plant',
+  null,
+  '#building=>industrial',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'man_made=>watermill',
+  null,
+  '#building=>industrial',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'man_made=>water_tower',
+  null,
+  '#building=>industrial',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'man_made=>windmill',
+  null,
+  '#building=>industrial',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'man_made=>works',
+  null,
+  '#building=>industrial',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'man_made=>reservoir_covered',
+  null,
+  '#building=>industrial',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>college',
+  null,
+  '#building=>education',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>cinema',
+  null,
+  '#building=>education',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>kindergarten',
+  null,
+  '#building=>education',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>library',
+  null,
+  '#building=>education',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>school',
+  null,
+  '#building=>education',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>university',
+  null,
+  '#building=>education',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>theatre',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>arts_centre',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>cinema',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>fountain',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>studio',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'tourism=>museum',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'tourism=>artwork',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'tourism=>attraction',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'tourism=>viewpoint',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'tourism=>theme_park',
+  null,
+  '#building=>culture',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'tourism=>zoo',
+  null,
+  '#building=>culture',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  '',
+  'shop',
+  '#building=>shop',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>hospital',
+  null,
+  '#building=>emergency',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>emergency_phone',
+  null,
+  '#building=>emergency',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>fire_station',
+  null,
+  '#building=>emergency',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>police',
+  null,
+  '#building=>emergency',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>pharmacy',
+  null,
+  '#building=>health',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>baby_hatch',
+  null,
+  '#building=>health',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>dentist',
+  null,
+  '#building=>health',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>doctors',
+  null,
+  '#building=>health',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>veterinary',
+  null,
+  '#building=>health',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>government',
+  null,
+  '#building=>public',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>gouvernment',
+  null,
+  '#building=>public',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>public_building',
+  null,
+  '#building=>public',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>court_house',
+  null,
+  '#building=>public',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>embassy',
+  null,
+  '#building=>public',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>prison',
+  null,
+  '#building=>public',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>townhall',
+  null,
+  '#building=>public',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>post_office',
+  null,
+  '#building=>communication',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  '',
+  'military',
+  '#building=>military',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  '',
+  'historic',
+  '#building=>historic',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'building=>residential',
+  null,
+  '#building=>residential',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'building=>apartments',
+  null,
+  '#building=>residential',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'building=>appartments',
+  null,
+  '#building=>residential',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'building=>block',
+  null,
+  '#building=>residential',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'building=>flats',
+  null,
+  '#building=>residential',
+  1
+);
+
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>bicycle_parking',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>bicycle_rental',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'amenity=>shelter',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'leisure=>sports_centre',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'leisure=>stadium',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'leisure=>track',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'leisure=>pitch',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  'leisure=>ice_rink',
+  null,
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  '',
+  'sport',
+  '#building=>sport',
+  1
+);
+insert into classify_hmatch values ('basemap_building',
+  '',
+  null,
+  '#building=>default',
+  0
+);
+
