d39bda69d6388fc41ebca8ca67894c8f985a1f2f
Stephan Plepelits <skunk@xover.mud.at>
2011-02-07 21:46:12 +0100

    Plugin 'Basemap': Bugfix, tracks are assumed to be double for tram and light_rail

--- www/plugins/basemap/db.sql
+++ www/plugins/basemap/db.sql
@@ -375,8 +375,20 @@ insert into classify_hmatch values ( 'basemap_railway_tracks',
   0
 );
 insert into classify_hmatch values ( 'basemap_railway_tracks',
-  '',
+  'railway=>tram',
   null,
+  '#railway_tracks=>double',
+  0
+);
+insert into classify_hmatch values ( 'basemap_railway_tracks',
+  'railway=>light_rail',
+  null,
+  '#railway_tracks=>double',
+  0
+);
+insert into classify_hmatch values ( 'basemap_railway_tracks',
+  '',
+  'railway',
   '#railway_tracks=>single',
   -1 
 );
---
+++ www/plugins/basemap/update/2011-02-07-21-30.sql
@@ -0,0 +1,12 @@
+insert into classify_hmatch values ( 'basemap_railway_tracks',
+  'railway=>tram',
+  null,
+  '#railway_tracks=>double',
+  0
+);
+insert into classify_hmatch values ( 'basemap_railway_tracks',
+  'railway=>light_rail',
+  null,
+  '#railway_tracks=>double',
+  0
+);
