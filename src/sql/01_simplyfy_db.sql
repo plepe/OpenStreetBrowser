3c3b5acc876ef92244b53ed88fb492840185d3e0
Stephan Plepelits <skunk@xover.mud.at>
2011-02-08 08:49:55 +0100

    Get rid of deprecated code

--- src/sql/01_simplyfy_db.sql
+++
@@ -1,3 +0,0 @@
-update planet_osm_point set "highway"='mountain_pass' from node_tags where osm_id=node_id and k='mountain_pass' and v='yes';
-update planet_osm_line set "tunnel"='yes' from way_tags where "man_made"='pipeline' and osm_id=way_id and k='location' and v in ('underground', 'underwater');
-update planet_osm_line set "voltage"=v from way_tags where "man_made"='pipeline' and osm_id=way_id and k='type';
