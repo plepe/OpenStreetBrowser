bcab8f47d1c1b52d23932904ea4a7f832f51fdef
Stephan Plepelits <skunk@xover.mud.at>
2011-02-10 07:08:55 +0100

    Plugin 'Cascadenik': Use correct path when accessing included mss

--- www/plugins/basemap/mcp.php
+++ www/plugins/basemap/mcp.php
@@ -3,10 +3,11 @@ function basemap_init($renderd) {
   global $plugins_dir;
 
   $prefix="$plugins_dir/basemap/base";
+  $path="$plugins_dir/basemap";
 
   if(filemtime("$prefix.mml")>filemtime("$prefix.mapnik")) {
     print "Recompiling basemap/base\n";
-    cascadenik_compile("$prefix.mml");
+    cascadenik_compile("$prefix.mml", $path);
   }
 
   renderd_register(&$renderd, "basemap_base", "$prefix.mapnik");
