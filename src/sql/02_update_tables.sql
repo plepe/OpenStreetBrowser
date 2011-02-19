9c8155bdd3faa539bb312686ef90a0d88ba7f0a0
Stephan Plepelits <skunk@xover.mud.at>
2011-02-19 11:31:54 +0100

    Update Tables: Simplify update procedure by marking implicitly changed objects
    - mark ways and relations which were implicitly changed by nodes with 'M'
    - mark relations which were implicitly changed by ways with 'M'

--- src/sql/02_update_tables.sql
+++ src/sql/02_update_tables.sql
@@ -17,6 +17,69 @@ BEGIN
 
   perform call_hooks('osmosis_update_start');
 
+  ---- simplify table actions ----
+  -- mark all ways as 'M' which were implicitly changed (because nodes of the
+  -- way were changed)
+  insert into actions
+   (select
+      'W' as "data_type",
+      'M' as "action",
+      way_nodes.way_id as "id"
+    from
+      actions node_actions
+      left join way_nodes
+        on way_nodes.node_id=node_actions.id
+      left join actions way_actions
+        on way_nodes.way_id=way_actions.id and way_actions.data_type='W'
+    where
+      node_actions.data_type='N' and
+      way_nodes.way_id is not null and
+      way_actions.id is null);
+
+  -- mark all relations as 'n' which were implicitly changed (because nodes of
+  -- the relation were changed)
+  insert into actions
+   (select
+      'R' as "data_type",
+      'M' as "action",
+      relation_members.relation_id as "id"
+    from
+      actions node_actions
+      left join relation_members
+        on relation_members.member_id=node_actions.id and
+	  relation_members.member_type='N'
+      left join actions rel_actions
+        on relation_members.relation_id=rel_actions.id and
+	  rel_actions.data_type='R'
+    where
+      node_actions.data_type='N' and
+      relation_members.relation_id is not null and
+      rel_actions.id is null);
+
+  -- mark all relations as 'w' which were implicitly changed (because ways of
+  -- the relation were changed)
+  insert into actions
+   (select
+      'R' as "data_type",
+      'M' as "action",
+      relation_members.relation_id as "id"
+    from
+      actions way_actions
+      left join relation_members
+        on relation_members.member_id=way_actions.id and
+	  relation_members.member_type='W'
+      left join actions rel_actions
+        on relation_members.relation_id=rel_actions.id and
+	  rel_actions.data_type='R'
+    where
+      way_actions.data_type='W' and
+      relation_members.relation_id is not null and
+      rel_actions.id is null);
+
+  -- we should also mark relations where relations were changed, but currently
+  -- we don't do recursive relations
+  raise notice 'calculated implicit changes';
+
   -- for later check make a copy of actions
   -- delete from save_actions;
   insert into save_actions (select now(), * from actions);
