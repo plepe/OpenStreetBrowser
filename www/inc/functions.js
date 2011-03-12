33e4f045f345afd803e28b44b40cee4650abd4c8
Stephan Plepelits <skunk@xover.mud.at>
2011-03-12 10:37:27 +0100

    JS functions: Import array_search from phpjs.org project

--- www/inc/functions.js
+++ www/inc/functions.js
@@ -499,3 +499,22 @@ function strtr (str, from, to) {
 
     return ret;
 }
+
+function array_search (needle, haystack, argStrict) {
+    // Searches the array for a given value and returns the corresponding key if successful  
+    // 
+    // version: 1102.614
+    // discuss at: http://phpjs.org/functions/array_search    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Brett Zamir (http://brett-zamir.me)
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // *     example 1: array_search('zonneveld', {firstname: 'kevin', middle: 'van', surname: 'zonneveld'});
+    // *     returns 1: 'surname'    var strict = !! argStrict;
+    var key = '', strict = !!argStrict;
+ 
+    for (key in haystack) {
+        if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {            return key;
+        }
+    }
+ 
+    return false;
+}
