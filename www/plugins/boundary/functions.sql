e14cd7c053a049f7d1000b1f14eb6e3aad659699
Stephan Plepelits <skunk@xover.mud.at>
2011-03-22 07:15:51 +0100

    Plugin 'Boundary': Bugfix: Redesign update hooks
    - Make use of simplified update procedure introduced in commit
      9c8155bdd3faa539bb312686ef90a0d88ba7f0a0

--- www/plugins/boundary/functions.sql
+++ www/plugins/boundary/functions.sql
@@ -16,7 +16,7 @@ BEGIN
 
   geom:=way_get_geom(id);
 
-  raise notice 'insert id=% level=% full level=(%)', id, min_admin_level, tags->'admin_level';
+  raise notice 'assemble_boundary(%): level=% full level=(%)', id, min_admin_level, tags->'admin_level';
    
   insert into osm_boundary
     values (
@@ -34,11 +34,9 @@ CREATE OR REPLACE FUNCTION boundary_update_delete() RETURNS boolean AS $$
 DECLARE
 BEGIN
   delete from osm_boundary using
-    (select way_id as id from way_nodes join actions on way_nodes.node_id=actions.id and actions.data_type='N' and actions.action='M' group by way_id
+    (select id from actions where data_type='W'
      union
-     select id from actions where data_type='W'
-     union
-     select member_id from relation_members rm join actions on rm.relation_id=actions.id and actions.data_type='R' and actions.action in ('M', 'D') where member_type='W'
+     select member_id from actions join relation_members rm on rm.relation_id=actions.id and member_type='W' where actions.data_type='R'
      ) actions 
   where osm_id='way_'||id;
 
@@ -53,11 +51,9 @@ CREATE OR REPLACE FUNCTION boundary_update_insert() RETURNS boolean AS $$
 DECLARE
 BEGIN
   perform assemble_boundary(id) from
-    (select way_id as id from actions join way_tags on actions.id=way_tags.way_id and actions.data_type='W' and actions.action in ('C', 'M') where k='boundary' and v in ('administrative', 'political')
+    (select way_id as id from actions join way_tags on actions.id=way_tags.way_id and actions.data_type='W' and actions.action not in ('D') where k='boundary' and v in ('administrative', 'political')
     union
-    select relation_members.member_id from (select * from actions where actions.data_type='R' and actions.action in ('C', 'M')) actions join osm_rel on 'rel_'||actions.id=osm_rel.osm_id  join relation_members on actions.id=relation_members.relation_id and relation_members.member_type='W' where osm_tags->'type'='boundary' and osm_tags->'boundary'='administrative'
-    union
-    select way_nodes.way_id as id from way_nodes join actions on way_nodes.node_id=actions.id and actions.data_type='N' and actions.action='M' join way_tags on actions.id=way_tags.way_id and k='boundary' and v in ('administrative', 'political') group by way_nodes.way_id
+    select relation_members.member_id from actions join relation_tags on actions.id=relation_tags.relation_id and actions.data_type='R' and actions.action not in ('D') join relation_members on relation_tags.relation_id=relation_members.relation_id and relation_members.member_type='W' where k='boundary' and v in ('administrative', 'political')
     ) x;
 
   raise notice 'inserted to osm_boundary';
