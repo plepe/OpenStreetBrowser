0977128308b3dba434d075d37552ead5fe4337df
Stephan Plepelits <skunk@xover.mud.at>
2011-03-24 22:20:02 +0100

    Category: Structure for saving categories in database

--- src/sql/04_categories.sql
+++ src/sql/04_categories.sql
@@ -1,8 +1,25 @@
-drop table if exists categories_def;
-create table categories_def (
-  rule_id		text	not null,
+drop table if exists category;
+create table category (
   category_id		text	not null,
-  rule_tags		hstore,
-  primary key(rule_id, category_id)
+  tags			hstore,
+  version		text	not null,
+  parent_versions	text[]	null,
+  primary key(category_id, version)
 );
-create index categories_def_category_id on categories_def(category_id);
+
+drop table if exists category_rule;
+create table category_rule (
+  category_id		text	not null,
+  rule_id		text	not null,
+  tags			hstore,
+  version		text	not null,
+  primary key(category_id, version, rule_id)
+);
+
+drop table if exists category_current;
+create table category_current (
+  category_id		text	not null,
+  version		text	not null,
+  now			timestamp with time zone	not null,
+  primary key(category_id)
+);
