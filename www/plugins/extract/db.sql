22c87561b29aca1e5b77254387dc01824db3625d
Stephan Plepelits <skunk@xover.mud.at>
2011-03-24 22:32:14 +0100

    Plugin 'Extract': Initialize table with all matching objects

--- www/plugins/extract/db.sql
+++ www/plugins/extract/db.sql
@@ -16,3 +16,32 @@ insert into classify_hmatch values ('extract', 'highway=>primary'::hstore, null,
 insert into classify_hmatch values ('extract', 'highway=>primary_link'::hstore, null, '#extract=>roads'::hstore, 0);
 insert into classify_hmatch values ('extract', 'highway=>secondary'::hstore, null, '#extract=>roads'::hstore, 0);
 insert into classify_hmatch values ('extract', 'highway=>tertiary'::hstore, null, '#extract=>roads'::hstore, 0);
+
+
+CREATE OR REPLACE FUNCTION extract_init() RETURNS boolean AS $$
+DECLARE
+  num_rows  int;
+BEGIN
+  raise notice '%', 'insert into osm_all_extract ( '
+    || 'select * '
+    || 'from ( '
+    || '  select '
+    || '    osm_id, '
+    || '    classify_hmatch(osm_id, osm_tags, osm_way, Array[''extract'']) as osm_tags, '
+    || '    osm_way '
+    || '  from '
+    || '    osm_all '
+    || '  where '
+    || classify_hmatch_sqlwhere('extract')
+    || ') x '
+    || 'where '
+    || '  osm_tags ? ''#extract'');';
+
+  GET DIAGNOSTICS num_rows = ROW_COUNT;
+  raise notice 'inserted to osm_all_extract (%)', num_rows;
+
+  return true;
+END;
+$$ language 'plpgsql';
+
+select extract_init();
