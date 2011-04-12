6584a46492c75a2da2482f94689d45238c9fd40a
Stephan Plepelits <skunk@xover.mud.at>
2011-04-12 13:05:40 +0200

    Boundary: Don't rely on osm_rel, use base tables

--- www/plugins/boundary/db.sql
+++ www/plugins/boundary/db.sql
@@ -10,6 +10,7 @@ select AddGeometryColumn('osm_boundary', 'osm_way', 900913, 'LINESTRING', 2);
 select assemble_boundary(id) from
   (select way_id as id from way_tags where k='boundary' and v in ('administrative', 'political')
   union
-  select relation_members.member_id from osm_rel join relation_members on cast(substr(osm_rel.osm_id, 5) as int)=relation_members.relation_id and relation_members.member_type='W' where osm_tags@>'type=>boundary' and osm_tags@>'boundary=>administrative') x;
+  select relation_members.member_id from relation_tags join relation_members on relation_tags.relation_id=relation_members.relation_id and relation_members.member_type='W' where k='boundary' and v in ('administrative', 'political')
+) x;
 
 create index osm_boundary_way_tags on osm_boundary using gist(osm_way, osm_tags);
