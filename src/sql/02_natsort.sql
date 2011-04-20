b05d2e4dd836bf26bda3155f14861f5ea6d0fefe
Stephan Plepelits <skunk@xover.mud.at>
2011-04-20 07:27:54 +0200

    DB Functions: Bugfix invalid variable name
    - 'left' and 'right' are no longer valid variable name in plpgsql. Rename to
      'val_left' resp. 'val_right'

--- src/sql/02_natsort.sql
+++ src/sql/02_natsort.sql
@@ -3,8 +3,8 @@ CREATE OR REPLACE FUNCTION nat_cmp (text, text)
 returns bool
 as $$
 declare
-  left	alias for $1;
-  right alias for $2;
+  val_left	alias for $1;
+  val_right 	alias for $2;
   l1 text;
   r1 text;
   l2 text;
@@ -12,10 +12,10 @@ declare
   ul1 text;
   ur1 text;
 begin
-  l1=substring(left  from '^([0-9]+).*$');
-  r1=substring(right from '^([0-9]+).*$');
-  l2=substring(left  from '^[0-9]+(.*)$');
-  r2=substring(right from '^[0-9]+(.*)$');
+  l1=substring(val_left  from '^([0-9]+).*$');
+  r1=substring(val_right from '^([0-9]+).*$');
+  l2=substring(val_left  from '^[0-9]+(.*)$');
+  r2=substring(val_right from '^[0-9]+(.*)$');
 
   if l1 is null then
     l1:='';
@@ -45,10 +45,10 @@ begin
     return true;
   end if;
 
-  l1=substring(left  from '^([^0-9]*)([0-9].*)?$');
-  r1=substring(right from '^([^0-9]*)([0-9].*)?$');
-  l2=substring(left  from '^[^0-9]+([0-9].*)$');
-  r2=substring(right from '^[^0-9]+([0-9].*)$');
+  l1=substring(val_left  from '^([^0-9]*)([0-9].*)?$');
+  r1=substring(val_right from '^([^0-9]*)([0-9].*)?$');
+  l2=substring(val_left  from '^[^0-9]+([0-9].*)$');
+  r2=substring(val_right from '^[^0-9]+([0-9].*)$');
   ul1=upper(l1);
   ur1=upper(r1);
 
