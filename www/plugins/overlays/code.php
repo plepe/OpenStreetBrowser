6228391514e06f023f188f7ed737a5586d1875c5
Stephan Plepelits <skunk@xover.mud.at>
2011-04-20 07:49:37 +0200

    Plugin 'Overlays': Define additional overlays from conf.php

--- www/plugins/overlays/code.php
+++ www/plugins/overlays/code.php
@@ -19,4 +19,14 @@ function overlays_html_done() {
   html_export_var(array("overlays_add"=>$overlays_layers));
 }
 
+function overlays_init() {
+  global $overlays_add;
+
+  if(isset($overlays_add))
+    foreach($overlays_add as $id=>$layer_conf) {
+      overlays_register($id, $layer_conf[0], $layer_conf[1]);
+    }
+}
+
 register_hook("html_done", "overlays_html_done");
+register_hook("init", "overlays_init");
