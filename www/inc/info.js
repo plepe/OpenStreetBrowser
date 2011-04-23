628f116baf620c08f2381065f8a90e056389239d
Stephan Plepelits <skunk@xover.mud.at>
2011-04-23 09:33:59 +0200

    Info: array as content of chapter - will be put in ul-list

--- www/inc/info.js
+++ www/inc/info.js
@@ -10,12 +10,15 @@
 // info calls the following hooks:
 // "info" (chapters[], object)
 //   please add one or more chapter(s) to the chapters array like this:
-//   example: { head: 'tags', weight: 5, content: dom node }
+//   example: { head: 'tags', weight: 5, content: dom node / string / array }
 //    .head: if there are several chapters with the same head, they will be
 //      concatenated to one at the position of the highest weight.
 //    .weight: The higher the weight, the lower the position of the content
 //    .content: A string with the text, which will be added to a new div or a
 //      dom node, which will be append the the chapter (recommended)
+//      if it is an array, it's elements (dom nodes or strings) will be put
+//      into an <ul> list.
+//      
 //   object is the reference to the geo object
 //
 //   chapter example: { head: "actions", weight: 5, content: "Some Action")
@@ -97,6 +100,7 @@ function info(ob) {
     this.div_chapter={};
     for(var i=0; i<data.length; i++) {
       var head="";
+      ul_open=null;
       if(data[i][0].head)
         head=data[i][0].head;
 
@@ -116,6 +120,20 @@ function info(ob) {
           div.innerHTML=data[i][j].content;
           data[i][j].content_node=div;
         }
+	else if(data[i][j].content.length) {
+	  if(!ul_open) {
+	    ul_open=dom_create_append(this.div_chapter[head], "ul");
+	    data[i][j].content_node=ul_open;
+	  }
+
+	  for(var k=0; k<data[i][j].content.length; k++) {
+	    var li=dom_create_append(ul_open, "li");
+	    if(typeof data[i][j].content[k]=="string")
+	      li.innerHTML=data[i][j].content[k];
+	    else
+	      li.appendChild(data[i][j].content[k]);
+	  }
+	}
         else {
           this.div_chapter[head].appendChild(data[i][j].content);
           data[i][j].content_node=data[i][j].content;
