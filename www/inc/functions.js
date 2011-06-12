10dc4f76601390bf4ea17eab7fda6329a00a06e2
Stephan Plepelits <skunk@xover.mud.at>
2011-06-12 07:48:47 +0200

    Functions: use urlencode() and urldecode() from phpjs project

--- www/inc/functions.js
+++ www/inc/functions.js
@@ -1078,3 +1078,19 @@ function setlocale (category, locale) {
     }
     return locale;
 }
+
+// Source: http://phpjs.org/functions/urldecode:572
+function urldecode (str) {
+    return decodeURIComponent((str + '').replace(/\+/g, '%20'));
+}
+
+// Source: http://phpjs.org/functions/urlencode:573
+function urlencode (str) {
+    str = (str + '').toString();
+
+    // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+    // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+    return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+    replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
+}
+
