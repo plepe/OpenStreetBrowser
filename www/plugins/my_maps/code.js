2e5cb4aa2ced7d9ee9e6b367b7fe8d73ab88a539
Stephan Plepelits <skunk@xover.mud.at>
2011-06-13 10:44:54 +0200

    Plugin 'My Maps': Basic structure for maps and items

--- www/plugins/my_maps/code.js
+++ www/plugins/my_maps/code.js
@@ -1,5 +1,38 @@
 var my_maps_toolbox;
+var my_maps_default;
+var my_maps_current;
 var my_maps_control;
+
+function my_maps_item(data, feature) {
+  this.inheritFrom=geo_object;
+  this.inheritFrom();
+  this.type="my_maps_item";
+
+  // constructor
+  this.id=data.id;
+  this.tags=new tags(data);
+  this.map=null;
+  if(!feature) {
+    // create feature from tags
+  }
+  else
+    this.feature=feature;
+
+  this.feature.style={ strokeWidth: 2, strokeColor: '#ff0000' };
+  vector_layer.redraw();
+}
+
+function my_maps_map(data) {
+  // add_item
+  this.add_item=function(item) {
+    this.items.push(item);
+    item.map=this;
+  }
+
+  // constructor
+  this.tags=new tags();
+  this.items=[];
+}
 
 function my_maps_activate() {
   my_maps_control.activate();
@@ -7,6 +40,10 @@ function my_maps_activate() {
 
 function my_maps_deactivate() {
   my_maps_control.deactivate();
+}
+
+function my_maps_add_feature(feature) {
+  my_maps_current.add_item(new my_maps_item({}, feature));
 }
 
 function my_maps_init() {
@@ -20,10 +57,14 @@ function my_maps_init() {
   });
   register_toolbox(my_maps_toolbox);
 
+  // create a default map
+  my_maps_default=new my_maps_map({});
+  my_maps_current=my_maps_default;
+
   // add a control to the map - to be (de)activated when toolbox is
   // (de)activated
   my_maps_control=new OpenLayers.Control.DrawFeature(vector_layer,
-    OpenLayers.Handler.Path);
+    OpenLayers.Handler.Path, {'featureAdded': my_maps_add_feature });
   map.addControl(my_maps_control);
 }
 
