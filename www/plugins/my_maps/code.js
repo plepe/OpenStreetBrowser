0dfa8b1ce9bff052faaeca3ca64559206f7a71f8
Stephan Plepelits <skunk@xover.mud.at>
2011-06-13 09:46:00 +0200

    Plugin 'My Maps': Add a control to create paths

--- www/plugins/my_maps/code.js
+++ www/plugins/my_maps/code.js
@@ -1,13 +1,30 @@
 var my_maps_toolbox;
+var my_maps_control;
+
+function my_maps_activate() {
+  my_maps_control.activate();
+}
+
+function my_maps_deactivate() {
+  my_maps_control.deactivate();
+}
 
 function my_maps_init() {
+  // create toolbox
   my_maps_toolbox=new toolbox({
     icon: "plugins/my_maps/icon.png",
     icon_title: "my_maps",
     weight: 5,
+    callback_activate: my_maps_activate,
+    callback_deactivate: my_maps_deactivate,
   });
   register_toolbox(my_maps_toolbox);
 
+  // add a control to the map - to be (de)activated when toolbox is
+  // (de)activated
+  my_maps_control=new OpenLayers.Control.DrawFeature(vector_layer,
+    OpenLayers.Handler.Path);
+  map.addControl(my_maps_control);
 }
 
 register_hook("init", my_maps_init);
