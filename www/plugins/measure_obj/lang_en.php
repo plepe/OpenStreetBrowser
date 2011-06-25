32581a9274d90929d8886f35ff1ab61393d25e94
Stephan Plepelits <skunk@xover.mud.at>
2011-06-25 11:56:54 +0200

    Plugin 'Measure Obj': Bugfix formating values

--- www/plugins/measure_obj/lang_en.php
+++ www/plugins/measure_obj/lang_en.php
@@ -1,4 +1,4 @@
 <?
 $lang_str['measure_obj:head']="Measurements";
-$lang_str['measure_obj:length']="Length: %s m";
-$lang_str['measure_obj:area']="Area: %s m²";
+$lang_str['measure_obj:length']="Length: %s";
+$lang_str['measure_obj:area']="Area: %s";
