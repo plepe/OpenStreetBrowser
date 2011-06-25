efb92cff1a2d4a85f8afd29b6064c43c0b68c5ac
Stephan Plepelits <skunk@xover.mud.at>
2011-06-25 18:01:03 +0200

    Languages: Add czech language

---
+++ www/plugins/search/lang_cz.php
@@ -0,0 +1,11 @@
+<?
+#$lang_str["search_process"]="searching";
+#$lang_str["search_more"]="more results";
+#$lang_str["search_results"]="Search results";
+#$lang_str["search_nominatim"]="search provided by";
+#$lang_str["search_field"]="Search...";
+#$lang_str["search_tip"]="e.g. 'London', 'Cromwell Road', 'post box near Hyde Park',...";
+#$lang_str["search_clear"]="Clear search field";
+
+
+
