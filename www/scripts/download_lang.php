10654d2d6107a2c4656986ca15168ca08a855bbc
Stephan Plepelits <skunk@xover.mud.at>
2011-06-25 19:48:18 +0200

    Languages: Update language strings in categories

--- www/scripts/download_lang.php
+++ www/scripts/download_lang.php
@@ -3,6 +3,8 @@
 require "../../conf.php";
 require "../inc/sql.php";
 require "../inc/tags.php";
+require "../inc/hooks.php";
+require "../inc/categories.php";
 
 $languages=array(
   "en"=>"English",
@@ -121,16 +123,79 @@ function parse($lang, $wikipage) {
 
 // read all categories
 $lang_cat_list=array();
+foreach($languages as $lang=>$wikipage) {
+  parse($lang, $wikipage);
+}
+
+$lang_new_list=array();
+foreach($lang_cat_list as $lang=>$cat_list) {
+  foreach($cat_list as $str=>$translation) {
+    if(preg_match("/^(.+):([^:]+)$/", $str, $m)) {
+      $lang_new_list[$m[1]][$m[2]][$lang]=$translation;
+    }
+  }
+}
+$lang_cat_list=$lang_new_list;
+
 $categories=array();
 $res_all=sql_query("select * from category_current", $db_central);
 while($elem_all=pg_fetch_assoc($res_all)) {
-  $categories[$elem_all['category_id']]=array("version"=>$elem_all['version']);
-  $res_cat=sql_query("select * from category_rule where category_id='{$elem_all['category_id']}' and version='{$elem_all['version']}'", $db_central);
-  while($elem_cat=pg_fetch_assoc($res_cat)) {
-    $categories[$elem_all['category_id']]['rule'][$elem_cat['rule_id']]=array();
-  }
-}
+  $category_id=$elem_all['category_id'];
+  $version=$elem_all['version'];
+  $new_version=uniqid();
+  $change=false;
+  $sql_str="begin;";
 
-foreach($languages as $lang=>$wikipage) {
-  parse($lang, $wikipage);
+  $res_cat=sql_query("select * from category where category_id='$category_id' and version='$version'", $db_central);
+  $elem_cat=pg_fetch_assoc($res_cat);
+
+  $tags_cat=parse_hstore($elem_cat['tags']);
+  $lang=$tags_cat['lang'];
+  if(!$lang)
+    $lang="en";
+
+  $tags_old=$tags_cat;
+
+  foreach($lang_cat_list[$category_id] as $tag=>$dummy) {
+    foreach($dummy as $l=>$value) {
+      if($l==$lang)
+	$tags_cat["$tag"]=$value;
+      else
+	$tags_cat["$tag:$l"]=$value;
+    }
+  }
+
+  if(sizeof(array_diff_assoc($tags_cat, $tags_old)))
+    $change=true;
+ 
+  $sql_str.="insert into category values ( '$category_id', ".array_to_hstore($tags_cat).", '$new_version', Array[ '$version' ], ''::hstore );\n";
+  $sql_str.="update category_current set version='$new_version' where category_id='$category_id';\n";
+
+  $res_rule=sql_query("select * from category_rule where category_id='$category_id' and version='$version'");
+  while($elem_rule=pg_fetch_assoc($res_rule)) {
+    $tags_rule=parse_hstore($elem_rule['tags']);
+
+    $tags_old=$tags_rule;
+
+    foreach($lang_cat_list["$category_id:{$elem_rule['rule_id']}"] as $tag=>$dummy) {
+      foreach($dummy as $l=>$value) {
+	if($l==$lang)
+	  $tags_rule["$tag"]=$value;
+	else
+	  $tags_rule["$tag:$l"]=$value;
+      }
+    }
+
+    if(sizeof(array_diff_assoc($tags_rule, $tags_old)))
+      $change=true;
+   
+    $sql_str.="insert into category_rule values ( '$category_id', '{$elem_rule['rule_id']}', ".array_to_hstore($tags_rule).", '$new_version');\n";
+  }
+
+  $sql_str.="commit;\n";
+
+  if($change) {
+    print "Update category $category_id\n";
+    sql_query($sql_str, $db_central);
+  }
 }
