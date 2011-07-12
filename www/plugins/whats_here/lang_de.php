79eb93517d3791370b5c8af9936e92e4f92b14f2
Stephan Plepelits <skunk@xover.mud.at>
2011-07-12 10:57:23 +0200

    Plugin 'Whats here': Move lang strings for head to plugin

--- www/plugins/whats_here/lang_de.php
+++ www/plugins/whats_here/lang_de.php
@@ -1,2 +1,3 @@
 <?
-$lang_str['whats_here:contextmenu']="What's here?";
+$lang_str['whats_here:contextmenu']="Was ist hier?";
+$lang_str['head:whats_here']="Was ist hier?";
--- www/plugins/whats_here/lang_en.php
+++ www/plugins/whats_here/lang_en.php
@@ -1,2 +1,3 @@
 <?
 $lang_str['whats_here:contextmenu']="What's here?";
+$lang_str['head:whats_here']="What's here?";
