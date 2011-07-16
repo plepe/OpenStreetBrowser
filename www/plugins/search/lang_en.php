e092f588c4cb9fa08279482a54e3873a6470c455
Stephan Plepelits <skunk@xover.mud.at>
2011-07-16 18:29:59 +0200

    Plugin 'Search': Improve Translation

--- www/plugins/search/lang_en.php
+++ www/plugins/search/lang_en.php
@@ -1,12 +1,9 @@
 <?
-$lang_str["search_process"]="searching";
-$lang_str["search_more"]="more results";
-$lang_str["search_results"]="Search results";
-$lang_str["search_nominatim"]="search provided by";
-$lang_str["search_field"]="Search...";
-$lang_str["search_tip"]="e.g. 'London', 'Cromwell Road', 'post box near Hyde Park',...";
-$lang_str["search_clear"]="Clear search field";
-
-
-
-
+$lang_str["search:name"]="Search";
+$lang_str["search:process"]="searching";
+$lang_str["search:more"]="more results";
+$lang_str["search:results"]="Search results";
+$lang_str["search:nominatim"]="search provided by";
+$lang_str["search:field"]="Search...";
+$lang_str["search:tip"]="e.g. 'London', 'Cromwell Road', 'post box near Hyde Park',...";
+$lang_str["search:clear"]="Clear search field";
