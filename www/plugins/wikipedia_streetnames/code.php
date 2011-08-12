a7ec05edc79aca522a8f74798fc833c589b2e007
Stephan Plepelits <skunk@xover.mud.at>
2011-08-12 09:04:07 +0200

    Plugin 'Wikipedia Streetnames': Initial code

---
+++ www/plugins/wikipedia_streetnames/code.php
@@ -0,0 +1,6 @@
+<?
+function wikipedia_streetnames_info($info_ret, $object) {
+}
+
+
+register_hook("info", "wikipedia_streetnames_info");
---
+++ www/plugins/wikipedia_streetnames/conf.php
@@ -0,0 +1,9 @@
+<?
+$wikipedia_streetnames_active=true;
+
+$wikipedia_streetnames_depend=array("wikipedia");
+
+$wikipedia_streetnames_tags=new tags(array(
+  "name"=>"Wikipedia Streetnames",
+));
+
