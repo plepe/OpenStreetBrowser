plepe/OpenStreetBrowser

commit b6200ba2263adf4de14a701f878ea787f3fec2de

Plugins '(Map Key) Basemap': Improve map key in low zoom levels

www/plugins/basemap/land.mss
@@ -1,5 +1,5 @@
 /*
-alias landuse=water|landuse_sub_type=t0 landuse=water
+hide  landuse=water|landuse_sub_type=t0
 hide  landuse=water|landuse_sub_type=t1
 hide  landuse=water|landuse_sub_type=t2
 hide  landuse=water|landuse_sub_type=t3
@@ -76,8 +76,12 @@ hide  landuse=public|landuse_sub_type=t1
 hide  landuse=public|landuse_sub_type=t2
 hide  landuse=public|landuse_sub_type=t3
 */
-Map {
+Map,
   map-bgcolor: #7eb9e3;
+}
+/* .Map is fake, just for map key */
+.Map {
+  polygon-fill: #7eb9e3;
 }
 .world1[zoom<5] {
   polygon-fill: #f2efd9;

www/plugins/map_key_basemap/code.php
@@ -59,17 +59,21 @@ class map_key_basemap extends map_key_cascadenik {
 
     $ret.="<h4>".lang("head:borders")."</h4>\n";
     $ret.="<table>\n";
-    $ret.=$this->show_mss(array("world1"), 
-      array(), $bounds, array("prefix"=>"admin_level=2", "img_base_path"=>"plugins/basemap"));
+    $ret.=$this->show_mss(array("bound_world1"), 
+      array("admin_level"=>array("=2")), $bounds, array("img_base_path"=>"plugins/basemap"));
     $ret.=$this->show_mss(array("admin"), 
       array("admin_level"=>array("=2", "=3", "=4", "=5", "=6", "=8", "=10")), $bounds, array("img_base_path"=>"plugins/basemap"));
     $ret.="</table>\n";
 
     $ret.="<h4>".lang("head:landuse")."</h4>\n";
     $ret.="<table>\n";
-    $ret.=$this->show_mss(array("landuse_extract"), 
+    $ret.=$this->show_mss(array("Map"),
+      array(), $bounds, array("name_prefix"=>"tag:landuse=water", "img_base_path"=>"plugins/basemap"));
+    $ret.=$this->show_mss(array("world1"),
+      array(), $bounds, array("name_prefix"=>"tag:landuse=land", "img_base_path"=>"plugins/basemap"));
+    $ret.=$this->show_mss(array("landuse_extract"),
       array("landuse"=>"*", "landuse_sub_type"=>"*"), $bounds, array("prefix"=>"", "geom"=>array("poly"=>1), "img_base_path"=>"plugins/basemap", "name_prefix"=>"tag:", "name_explode"=>false));
-    $ret.=$this->show_mss(array("landuse"), 
+    $ret.=$this->show_mss(array("landuse"),
       array("landuse"=>"*", "landuse_sub_type"=>"*"), $bounds, array("prefix"=>"", "geom"=>array("poly"=>1), "img_base_path"=>"plugins/basemap", "name_prefix"=>"tag:", "name_explode"=>false));
     $ret.="</table>\n";
     

commit 44b31cf02e5d90761cabdc6a6688ae34eb0c36cd

Plugin 'Basemap': Fix typos in land.mss
- Bug was introduced in commit b6200ba2263adf4de14a701f878ea787f3fec2de

www/plugins/basemap/land.mss
@@ -76,13 +76,14 @@ hide  landuse=public|landuse_sub_type=t1
 hide  landuse=public|landuse_sub_type=t2
 hide  landuse=public|landuse_sub_type=t3
 */
-Map,
+Map {
   map-bgcolor: #7eb9e3;
 }
-/* .Map is fake, just for map key */
+/* .Map is fake, just for map key
 .Map {
   polygon-fill: #7eb9e3;
 }
+*/
 .world1[zoom<5] {
   polygon-fill: #f2efd9;
 }