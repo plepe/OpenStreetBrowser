3ea2f21947355f3bdf83128673ec16e19d8e8fae
Stephan Plepelits <skunk@xover.mud.at>
2011-09-10 12:19:04 +0200

    Translation System: Currently no need to automatically rewrite lang strings

--- www/scripts/download_lang.php
+++ www/scripts/download_lang.php
@@ -22,25 +22,7 @@ $languages=array(
 );
 
 function rewrite_str($str) {
-  if($str=="head_action")
-    return "head:actions";
-  if(eregi("^head_(.*)$", $str, $m))
-    return "head:$m[1]";
-  if($str=="read_more")
-    return "wikipedia:read_more";
-  if(eregi("^map_key_(.*)$", $str, $m))
-    return "map_key:$m[1]";
-  if(eregi("^yes/(.*)$", $str, $m))
-    return "$m[1]";
-  if(eregi("^highway_type_(.*)$", $str, $m))
-    return "tag:highway=$m[1]";
-  if(eregi("^admin_level=(.*)$", $str, $m))
-    return "tag:admin_level=$m[1]";
-  if(eregi("^tag\/(.*)$", $str, $m))
-    return "tag:$m[1]";
-  if(eregi("^tag_(.*)\/(.*)$", $str, $m))
-    return "tag:$m[1]=$m[2]";
-
+  // Currently no need to rewrite strings
   return $str;
 }
 
