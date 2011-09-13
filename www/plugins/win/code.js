3e9060ea3eeb8c86b450c9c4159d8dc7dd1b9811
Stephan Plepelits <skunk@xover.mud.at>
2011-09-13 16:48:11 +0200

    Plugin 'Win': Put all windows in a div on top of page content

--- www/plugins/win/code.js
+++ www/plugins/win/code.js
@@ -1,14 +1,23 @@
 var windows=[];
+var win_root;
 
 function win(cl) {
   this.win=document.createElement("div");
   this.win.className=cl;
+
+  // Add window to div win_root (create if it doesn't exist)
+  if(!win_root) {
+    win_root=dom_create_append(document.body, "div");
+    win_root.className="win_root";
+  }
+  win_root.appendChild(this.win);
+
+  // Create div for content
   this.content=document.createElement("div");
+  this.win.appendChild(this.content);
+
   this.id=uniqid();
   windows[this.id]=this;
-
-  document.body.appendChild(this.win);
-  this.win.appendChild(this.content);
 
   this.close=function() {
     this.win.parentNode.removeChild(this.win);
