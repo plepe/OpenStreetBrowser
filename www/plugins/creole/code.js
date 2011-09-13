485afbbf1f39515979e6380ce32f17268abe8447
Stephan Plepelits <skunk@xover.mud.at>
2011-09-13 16:16:43 +0200

    Plugin 'Creole': Process interwiki-links

--- www/plugins/creole/code.js
+++ www/plugins/creole/code.js
@@ -1,4 +1,23 @@
 var creole_parser;
+var creole_interwiki_links={
+  "osm": "http://wiki.openstreetmap.org/wiki/%s"
+};
+
+// Processes interwiki-links and such
+function creole_preprocess(div) {
+  var m;
+
+  var as=div.getElementsByTagName("a");
+  for(var i=0; i<as.length; i++) {
+    var a=as[i];
+    if(m=a.href.match(/^([^:]*):(.*)$/)) {
+      if(creole_interwiki_links[m[1]]) {
+	a.href=sprintf(creole_interwiki_links[m[1]], m[2]);
+	a.target="_new";
+      }
+    }
+  }
+}
 
 function creole(txt) {
   if(!creole_parser)
@@ -7,6 +26,7 @@ function creole(txt) {
   var div=document.createElement("div");
 
   creole_parser.parse(div, txt);
+  creole_preprocess(div);
 
   return div;
 }
