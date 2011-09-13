052fd745dde2802ba4366268c9e3f133b1b879d9
Stephan Plepelits <skunk@xover.mud.at>
2011-09-13 07:48:07 +0200

    Plugin 'Doc': Improve placement of info-icon

--- www/plugins/doc/code.js
+++ www/plugins/doc/code.js
@@ -4,12 +4,19 @@ function doc_show(plugin) {
 
 function doc_chapter(info, ob, div, data) {
   if(data.doc) {
-    var doc_div=dom_create_append(div, "div");
+    var head=div.getElementsByTagName("h2");
+    if(head.length==0)
+      return;
+
+    var doc_div=document.createElement("div");
+    head[0].appendChild(doc_div);
     doc_div.className="doc_link";
 
     var a=dom_create_append(doc_div, "a");
-    dom_create_append_text(a, "i");
     a.href="javascript:doc_show(\""+data.doc+"\")";
+
+    var img=dom_create_append(a, "img");
+    img.src="img/info.png";
   }
 }
 
