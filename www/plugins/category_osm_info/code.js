31665be5a1cb5fb76ce727a430b8aa29cdf6ec8b
Stephan Plepelits <skunk@xover.mud.at>
2011-09-15 06:39:00 +0200

    Plugin 'Category Info': Initial code ; open tab and paste dummy text

---
+++ www/plugins/category_osm_info/code.js
@@ -0,0 +1,8 @@
+function category_osm_info_show(cat_win, category) {
+  var t=new tab({ name: lang("info") });
+  cat_win.tab_manager.register_tab(t);
+
+  t.content.innerHTML="foo bar";
+}
+
+register_hook("category_window_show", category_osm_info_show);
