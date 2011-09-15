03eb2aab0298812b9a92126c94e46ef01fec6508
Stephan Plepelits <skunk@xover.mud.at>
2011-09-15 12:48:40 +0200

    Plugin 'Category OSM Info': Show category title and name of all rules

--- www/plugins/category_osm_info/code.js
+++ www/plugins/category_osm_info/code.js
@@ -2,7 +2,13 @@ function category_osm_info_show(cat_win, category) {
   var t=new tab({ title: lang("info") });
   cat_win.tab_manager.register_tab(t);
 
-  t.content.innerHTML="foo bar";
+  var div=dom_create_append(t.content, "div");
+  div.innerHTML=category.tags.get_lang("name", ui_lang);
+
+  for(var i=0; i<category.rules.length; i++) {
+    var div=dom_create_append(t.content, "div");
+    dom_create_append_text(div, category.rules[i].name());
+  }
 }
 
 register_hook("category_window_show", category_osm_info_show);
