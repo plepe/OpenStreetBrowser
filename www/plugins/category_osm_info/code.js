116668a4189803a4f2c183fbd70bb98661a13859
Stephan Plepelits <skunk@xover.mud.at>
2011-09-18 17:48:20 +0200

    Plugin 'Category OSM Info': Show heading + category description

--- www/plugins/category_osm_info/code.js
+++ www/plugins/category_osm_info/code.js
@@ -3,7 +3,15 @@ function category_osm_info_show(cat_win, category) {
   cat_win.tab_manager.register_tab(t);
 
   var div=dom_create_append(t.content, "div");
-  div.innerHTML=category.tags.get_lang("name", ui_lang);
+
+  var h1=dom_create_append(div, "h1");
+  dom_create_append_text(h1, lang("category")+" \""+category.tags.get_lang("name", ui_lang)+"\"");
+
+  var desc;
+  if(desc=category.tags.get_lang("description", ui_lang)) {
+    var div_desc=dom_create_append(div, "div");
+    dom_create_append_text(div_desc, desc);
+  }
 
   for(var i=0; i<category.rules.length; i++) {
     var div=dom_create_append(t.content, "div");
