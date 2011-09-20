f0ff6dd6a49826557f9ce9c7e748492230fec7b2
Stephan Plepelits <skunk@xover.mud.at>
2011-09-20 07:13:52 +0200

    Plugin 'Importance': Initial code ; define importance levels

---
+++ www/plugins/importance/code.php
@@ -0,0 +1,16 @@
+<?
+// TODO: This value is also defined in the file www/inc/categories.php . It
+// should be removed there, as soon as categories are converted to plugins.
+$importance_levels=array("global", "international", "national", "regional", "urban", "suburban", "local");
+
+// TODO: This values are not used yet, but the variables $scale_icon and
+// $scale_text in www/inc/categories.php . This should be changed sometime.
+$importance_zoom=array(
+  "global"        =>array("list"=> 1, "icon"=> 2, "label"=> 4),
+  "international" =>array("list"=> 4, "icon"=> 5, "label"=> 8),
+  "national"	  =>array("list"=> 7, "icon"=> 8, "label"=>10),
+  "regional"	  =>array("list"=>10, "icon"=>11, "label"=>13),
+  "urban"	  =>array("list"=>12, "icon"=>13, "label"=>15),
+  "suburban"	  =>array("list"=>14, "icon"=>15, "label"=>16),
+  "local" 	  =>array("list"=>16, "icon"=>17, "label"=>18),
+);
---
+++ www/plugins/importance/conf.php
@@ -0,0 +1,10 @@
+<?
+$importance_active=true;
+
+$importance_depend=array();
+
+$importance_tags=new tags(array(
+  "name"=>"Importance",
+));
+
+
