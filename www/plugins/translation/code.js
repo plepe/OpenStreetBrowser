951f3d8023c90d3ee15f894236a55782137d05eb
Stephan Plepelits <skunk@xover.mud.at>
2011-09-21 09:18:21 +0200

    Plugin 'Translation': Show form with all strings

--- www/plugins/translation/code.js
+++ www/plugins/translation/code.js
@@ -1,5 +1,43 @@
 var translation_win;
 
+function translation_show(data) {
+  var form=dom_create_append(translation_win.content, "form");
+  var tab=dom_create_append(form, "table");
+
+  var tr=dom_create_append(tab, "tr");
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation:string_id"));
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation:translation"));
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation:compare"));
+
+  for(var i in data) {
+    var tr=dom_create_append(tab, "tr");
+    var th=dom_create_append(tr, "th");
+    th.colSpan=3;
+    dom_create_append_text(th, i);
+
+    if(data[i].order)
+    for(var j=0; j<data[i].order.length; j++) {
+      var str=data[i].order[j];
+      var d=data[i].list[str];
+      var tr=dom_create_append(tab, "tr");
+
+      var td=dom_create_append(tr, "td");
+      dom_create_append_text(td, str);
+
+      var td=dom_create_append(tr, "td");
+      var input=dom_create_append(td, "input");
+      input.value=d.value;
+      input.name=str;
+
+//      var td=dom_create_append(tr, "td");
+//      dom_create_append_text(td, str);
+    }
+  }
+}
+
 function translation_open() {
   translation_win=new win({ title: lang("translation:name"), class: 'translation_win' });
   ajax("translation_read", { lang: "en" }, translation_open_next1);
@@ -7,7 +45,8 @@ function translation_open() {
 
 function translation_open_next1(ret) {
   ret=ret.return_value;
-  alert(print_r(ret));
+
+  translation_show(ret);
 }
 
 function translation_files_list() {
--- www/plugins/translation/lang_en.php
+++ www/plugins/translation/lang_en.php
@@ -1,2 +1,5 @@
 <?
 $lang_str["translation:name"]="Translation";
+$lang_str["translation:string_id"]="ID";
+$lang_str["translation:translation"]="Translation";
+$lang_str["translation:compare"]="Compare";
