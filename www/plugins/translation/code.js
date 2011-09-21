050412154b165dd58e129d0439b53065b11dadd7
Stephan Plepelits <skunk@xover.mud.at>
2011-09-21 17:25:20 +0200

    Plugin 'Translation': Add styling

--- www/plugins/translation/code.js
+++ www/plugins/translation/code.js
@@ -4,44 +4,61 @@ function translation_show(data) {
   var form=dom_create_append(translation_win.content, "form");
   var tab=dom_create_append(form, "table");
 
-  var tr=dom_create_append(tab, "tr");
+  // header
+  var thead=dom_create_append(tab, "thead");
+  var tr=dom_create_append(thead, "tr");
   var th=dom_create_append(tr, "th");
+  th.className="id_help";
   dom_create_append_text(th, lang("translation:string_id"));
+
   var th=dom_create_append(tr, "th");
+  th.className="value";
   dom_create_append_text(th, lang("translation:translation"));
+
   var th=dom_create_append(tr, "th");
+  th.className="compare";
   dom_create_append_text(th, lang("translation:compare"));
 
+  var tbody=dom_create_append(tab, "tbody");
   for(var i in data) {
-    var tr=dom_create_append(tab, "tr");
+    var tr=dom_create_append(tbody, "tr");
     var th=dom_create_append(tr, "th");
     th.colSpan=3;
+    th.className="file";
     dom_create_append_text(th, i);
 
     if(data[i].order)
     for(var j=0; j<data[i].order.length; j++) {
       var str=data[i].order[j];
       var d=data[i].list[str];
-      var tr=dom_create_append(tab, "tr");
+      var tr=dom_create_append(tbody, "tr");
 
       // column 1
       var td=dom_create_append(tr, "td");
+      td.className="id_help";
+
       var div_id=dom_create_append(td, "div");
+      div_id.className="id";
       dom_create_append_text(div_id, str);
+
       if(d.help) {
 	var div_help=dom_create_append(td, "div");
+	div_help.className="help";
 	dom_create_append_text(div_help, d.help);
       }
 
       // column 2
       var td=dom_create_append(tr, "td");
+      td.className="value";
+
       var input=dom_create_append(td, "input");
       input.value=d.value;
       input.name=str;
 
       // column 3
-//      var td=dom_create_append(tr, "td");
-//      dom_create_append_text(td, str);
+      var td=dom_create_append(tr, "td");
+      td.className="compare";
+      dom_create_append_text(td, "foo");
     }
   }
 }
