01d2ae7a19cf9e47c4d18248bc2f5c7d6eda6f01
Stephan Plepelits <skunk@xover.mud.at>
2011-09-21 07:44:53 +0200

    Plugin 'Translation': Init translation directory, return list of files

--- www/plugins/translation/code.js
+++ www/plugins/translation/code.js
@@ -2,4 +2,13 @@ var translation_win;
 
 function translation_open() {
   translation_win=new win({ title: lang("translation:name") });
+  ajax("translation_files_list", {}, translation_open_next1);
+}
+
+function translation_open_next1(ret) {
+  ret=ret.return_value;
+  alert(print_r(ret));
+}
+
+function translation_files_list() {
 }
--- www/plugins/translation/code.php
+++ www/plugins/translation/code.php
@@ -1,4 +1,36 @@
 <?
+global $translation_path;
+
+function translation_init() {
+  global $root_path;
+  global $translation_path;
+  $translation_path="$root_path/data/translation";
+
+  if(!file_exists("$translation_path")) {
+    mkdir("$translation_path");
+    chdir("$translation_path");
+    system("git clone $root_path $translation_path");
+  }
+}
+
+function ajax_translation_files_list() {
+  global $translation_path;
+
+  translation_init();
+
+  $ret=array();
+  $ret[]="php:www/lang/";
+  $ret[]="php:www/lang/tags_";
+  $d=opendir("$translation_path/www/plugins");
+  while($f=readdir($d)) {
+    if(file_exists("$translation_path/www/plugins/$f/lang_en.php"))
+      $ret[]="php:www/plugins/$f/lang_";
+  }
+  closedir($d);
+
+  return $ret;
+}
+
 function translation_main_links($links) {
   $links[]=array(5, "<a href='javascript:translation_open()'>".lang("translation:name")."</a>");
 }
