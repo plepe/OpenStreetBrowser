11aeb4a44d69b7e0ed9d02c1dfa96809cef074d8
Stephan Plepelits <skunk@xover.mud.at>
2011-09-21 17:43:21 +0200

    Plugin 'Translation': Add save-button, find all changed strings

--- www/plugins/translation/code.js
+++ www/plugins/translation/code.js
@@ -1,8 +1,30 @@
 var translation_win;
+var translation_form;
+
+function translation_submit() {
+  var changed={};
+
+  for(var i=0; i<translation_form.elements.length; i++) {
+    var el=translation_form.elements[i];
+
+    if((el.name)&&
+       (el.value!=el.orig_value)&&
+       (!((el.value=="")&&(!el.orig_value)))) {
+      if(!changed[el.file])
+	changed[el.file]={};
+
+      changed[el.file][el.name]=el.value;
+    }
+  }
+
+  alert(print_r(changed));
+}
 
 function translation_show(data) {
-  var form=dom_create_append(translation_win.content, "form");
-  var tab=dom_create_append(form, "table");
+  translation_form=dom_create_append(translation_win.content, "form");
+  translation_form.action="javascript:translation_submit()";
+
+  var tab=dom_create_append(translation_form, "table");
 
   // header
   var thead=dom_create_append(tab, "thead");
@@ -53,6 +75,8 @@ function translation_show(data) {
 
       var input=dom_create_append(td, "input");
       input.value=d.value;
+      input.orig_value=d.value;
+      input.file=i;
       input.name=str;
 
       // column 3
@@ -61,6 +85,12 @@ function translation_show(data) {
       dom_create_append_text(td, "foo");
     }
   }
+
+  var div=dom_create_append(translation_form, "div");
+  
+  var input=dom_create_append(div, "input");
+  input.type="submit";
+  input.value=lang("save");
 }
 
 function translation_open() {
