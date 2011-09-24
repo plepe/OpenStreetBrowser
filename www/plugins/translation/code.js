30a9e5894c133c9dc79f8bc0fa5d165b40a0094a
Stephan Plepelits <skunk@xover.mud.at>
2011-09-24 08:31:16 +0200

    Plugin 'Translation': Convert translation code to classes

--- www/plugins/translation/code.js
+++ www/plugins/translation/code.js
@@ -1,85 +1,3 @@
-var translation_win;
-var translation_form;
-var translation_compare_select;
-
-function translation_submit() {
-  var changed={};
-
-  for(var i=0; i<translation_form.elements.length; i++) {
-    var el=translation_form.elements[i];
-
-    if(el.mode=="doc") {
-      if(el.value!=el.orig_value) {
-	changed[el.file]=el.value;
-      }
-    }
-    else
-    if((el.name)&&
-       (el.value)&&
-       (el.value!=el.orig_value)&&
-       (!((el.value=="")&&(!el.orig_value)))) {
-      if(!changed[el.file])
-	changed[el.file]={};
-
-      changed[el.file][el.name]=split_semicolon(el.value);
-    }
-  }
-
-  ajax("translation_save", { lang: ui_lang, changed: changed, msg: "Update translation ("+ui_lang+")" }, translation_save_next);
-}
-
-function translation_save_next(ret) {
-  ret=ret.return_value;
-
-  alert(lang("saved"));
-  translation_win.close();
-}
-
-function translation_compare(data) {
-  var tds=translation_form.getElementsByTagName("td");
-  for(var i=0; i<tds.length; i++) {
-    var td=tds[i];
-
-    if(td.className=="compare") {
-      var value;
-
-      dom_clean(td);
-
-      if((data[td.file])&&
-	 (data[td.file].list)&&
-	 (data[td.file].list[td.key])&&
-	 (data[td.file].list[td.key].value)) {
-	value=data[td.file].list[td.key].value;
-	value=document.createTextNode(translation_to_value(value));
-      }
-      else
-      if((data[td.file])&&
-	 (data[td.file].contents)) {
-	value=document.createElement("textarea");
-	value.value=data[td.file].contents;
-	//dom_create_append_text(value, 
-      }
-      else
-	value=document.createTextNode("");
-
-      td.appendChild(value);
-    }
-  }
-}
-
-function translation_compare_recv(ret) {
-  translation_compare(ret.return_value);
-  dom_clean(translation_compare_select.nextSibling);
-}
-
-function translation_compare_change() {
-  var img=dom_create_append(translation_compare_select.nextSibling, "img");
-  img.className="loading";
-  img.src="img/ajax_loader.gif";
-
-  ajax("translation_read", { lang: translation_compare_select.value }, translation_compare_recv);
-}
-
 function translation_to_value(value) {
   if(!value)
     value="";
@@ -91,172 +9,270 @@ function translation_to_value(value) {
   return value;
 }
 
-function translation_print_file_lang_str(file, data, tbody) {
-  if(!data.order)
-    return;
+function translation_submit() {
+}
 
-  for(var j=0; j<data.order.length; j++) {
-    var str=data.order[j];
-    var d=data.list[str];
+function translation(l) {
+  // submit
+  this.submit=function() {
+    var changed={};
+
+    for(var i=0; i<this.form.elements.length; i++) {
+      var el=this.form.elements[i];
+
+      if(el.mode=="doc") {
+	if(el.value!=el.orig_value) {
+	  changed[el.file]=el.value;
+	}
+      }
+      else
+      if((el.name)&&
+	 (el.value)&&
+	 (el.value!=el.orig_value)&&
+	 (!((el.value=="")&&(!el.orig_value)))) {
+	if(!changed[el.file])
+	  changed[el.file]={};
+
+	changed[el.file][el.name]=split_semicolon(el.value);
+      }
+    }
+
+    ajax("translation_save", { lang: this.lang, changed: changed, msg: "Update translation ("+this.lang+")" }, this.save_next.bind(this));
+  }
+
+  // save_next
+  this.save_next=function(ret) {
+    ret=ret.return_value;
+
+    alert(lang("saved"));
+    this.win.close();
+  }
+
+  this.compare=function(data) {
+    var tds=this.form.getElementsByTagName("td");
+    for(var i=0; i<tds.length; i++) {
+      var td=tds[i];
+
+      if(td.className=="compare") {
+	var value;
+
+	dom_clean(td);
+
+	if((data[td.file])&&
+	   (data[td.file].list)&&
+	   (data[td.file].list[td.key])&&
+	   (data[td.file].list[td.key].value)) {
+	  value=data[td.file].list[td.key].value;
+	  value=document.createTextNode(translation_to_value(value));
+	}
+	else
+	if((data[td.file])&&
+	   (data[td.file].contents)) {
+	  value=document.createElement("textarea");
+	  value.value=data[td.file].contents;
+	  //dom_create_append_text(value, 
+	}
+	else
+	  value=document.createTextNode("");
+
+	td.appendChild(value);
+      }
+    }
+  }
+
+  // compare_recv
+  this.compare_recv=function(ret) {
+    this.compare(ret.return_value);
+    dom_clean(this.compare_select.nextSibling);
+  }
+
+  // compare change
+  this.compare_change=function() {
+    var img=dom_create_append(this.compare_select.nextSibling, "img");
+    img.className="loading";
+    img.src="img/ajax_loader.gif";
+
+    ajax("translation_read", { lang: this.compare_select.value }, this.compare_recv.bind(this));
+  }
+
+  // print_file_lang_str
+  this.print_file_lang_str=function(file, data, tbody) {
+    if(!data.order)
+      return;
+
+    for(var j=0; j<data.order.length; j++) {
+      var str=data.order[j];
+      var d=data.list[str];
+      var tr=dom_create_append(tbody, "tr");
+
+      // column 1
+      var td=dom_create_append(tr, "td");
+      td.className="id_help";
+
+      var div_id=dom_create_append(td, "div");
+      div_id.className="id";
+      dom_create_append_text(div_id, str);
+
+      if(d.help) {
+	var div_help=dom_create_append(td, "div");
+	div_help.className="help";
+	dom_create_append_text(div_help, d.help);
+      }
+
+      // column 2
+      var td=dom_create_append(tr, "td");
+      td.className="value";
+
+      var input=dom_create_append(td, "input");
+      input.file=file;
+      input.name=str;
+
+      var value=translation_to_value(d.value);
+      input.value=value;
+      input.orig_value=value;
+
+      // column 3
+      var td=dom_create_append(tr, "td");
+      td.className="compare";
+      td.file=file;
+      td.key=str;
+      dom_create_append_text(td, "");
+    }
+  }
+
+
+  // print_file_doc
+  this.print_file_doc=function(file, data, tbody) {
     var tr=dom_create_append(tbody, "tr");
 
     // column 1
     var td=dom_create_append(tr, "td");
     td.className="id_help";
 
-    var div_id=dom_create_append(td, "div");
-    div_id.className="id";
-    dom_create_append_text(div_id, str);
-
-    if(d.help) {
+    if(data.help) {
       var div_help=dom_create_append(td, "div");
       div_help.className="help";
-      dom_create_append_text(div_help, d.help);
+      dom_create_append_text(div_help, data.help);
     }
+
+    var div=dom_create_append(td, "div");
+    div.className="help";
+    dom_create_append_text(div, "You can use WikiCreole markup on this file");
 
     // column 2
     var td=dom_create_append(tr, "td");
     td.className="value";
 
-    var input=dom_create_append(td, "input");
+    var input=dom_create_append(td, "textarea");
     input.file=file;
-    input.name=str;
+    input.mode="doc";
+    input.name=file;
 
-    var value=translation_to_value(d.value);
-    input.value=value;
-    input.orig_value=value;
+    input.value=data.contents;
+    input.orig_value=data.contents;
 
     // column 3
     var td=dom_create_append(tr, "td");
     td.className="compare";
     td.file=file;
-    td.key=str;
-    dom_create_append_text(td, "");
-  }
-}
+    td.name="_content_";
 
-function translation_print_file_doc(file, data, tbody) {
-  var tr=dom_create_append(tbody, "tr");
-
-  // column 1
-  var td=dom_create_append(tr, "td");
-  td.className="id_help";
-
-  if(data.help) {
-    var div_help=dom_create_append(td, "div");
-    div_help.className="help";
-    dom_create_append_text(div_help, data.help);
+    var input=dom_create_append(td, "textarea");
+    input.disabled=true;
   }
 
-  var div=dom_create_append(td, "div");
-  div.className="help";
-  dom_create_append_text(div, "You can use WikiCreole markup on this file");
 
-  // column 2
-  var td=dom_create_append(tr, "td");
-  td.className="value";
+  // show
+  this.show=function(data) {
+    dom_clean(this.win.content);
 
-  var input=dom_create_append(td, "textarea");
-  input.file=file;
-  input.mode="doc";
-  input.name=file;
+    this.form=dom_create_append(this.win.content, "form");
+    this.form.action="javascript:translation_submit()";
+    this.form.onsubmit=this.submit.bind(this);
 
-  input.value=data.contents;
-  input.orig_value=data.contents;
+    var div=dom_create_append(this.form, "div");
+    div.className="content";
+    var tab=dom_create_append(div, "table");
 
-  // column 3
-  var td=dom_create_append(tr, "td");
-  td.className="compare";
-  td.file=file;
-  td.name="_content_";
-
-  var input=dom_create_append(td, "textarea");
-  input.disabled=true;
-}
-
-function translation_show(data) {
-  dom_clean(translation_win.content);
-
-  translation_form=dom_create_append(translation_win.content, "form");
-  translation_form.action="javascript:translation_submit()";
-
-  var div=dom_create_append(translation_form, "div");
-  div.className="content";
-  var tab=dom_create_append(div, "table");
-
-  // header
-  var thead=dom_create_append(tab, "thead");
-  var tr=dom_create_append(thead, "tr");
-  var th=dom_create_append(tr, "th");
-  th.className="id_help";
-  dom_create_append_text(th, lang("translation:string_id"));
-
-  var th=dom_create_append(tr, "th");
-  th.className="value";
-  dom_create_append_text(th, lang("translation:translation"));
-
-  var th=dom_create_append(tr, "th");
-  th.className="compare";
-  dom_create_append_text(th, lang("translation:compare"));
-
-  var tbody=dom_create_append(tab, "tbody");
-  for(var i in data) {
-    var tr=dom_create_append(tbody, "tr");
+    // header
+    var thead=dom_create_append(tab, "thead");
+    var tr=dom_create_append(thead, "tr");
     var th=dom_create_append(tr, "th");
-    th.colSpan=3;
-    th.className="file";
-    dom_create_append_text(th, i);
+    th.className="id_help";
+    dom_create_append_text(th, lang("translation:string_id"));
 
-    if(!data[i])
-      continue;
+    var th=dom_create_append(tr, "th");
+    th.className="value";
+    dom_create_append_text(th, lang("translation:translation"));
 
-    var mode=i.match(/^([^:]*):/);
-    switch(mode[1]) {
-      case "php":
-      case "category":
-        translation_print_file_lang_str(i, data[i], tbody);
-	break;
-      case "doc":
-        translation_print_file_doc(i, data[i], tbody);
-	break;
-      default:
-        /* should not come here */
+    var th=dom_create_append(tr, "th");
+    th.className="compare";
+    dom_create_append_text(th, lang("translation:compare"));
+
+    var tbody=dom_create_append(tab, "tbody");
+    for(var i in data) {
+      var tr=dom_create_append(tbody, "tr");
+      var th=dom_create_append(tr, "th");
+      th.colSpan=3;
+      th.className="file";
+      dom_create_append_text(th, i);
+
+      if(!data[i])
+	continue;
+
+      var mode=i.match(/^([^:]*):/);
+      switch(mode[1]) {
+	case "php":
+	case "category":
+	  this.print_file_lang_str(i, data[i], tbody);
+	  break;
+	case "doc":
+	  this.print_file_doc(i, data[i], tbody);
+	  break;
+	default:
+	  /* should not come here */
+      }
     }
+
+    var div=dom_create_append(this.form, "div");
+    
+    var input=dom_create_append(div, "input");
+    input.type="submit";
+    input.value=lang("save");
+
+    // Choose compare language
+    dom_create_append_text(div, lang("translation:compare")+":");
+    this.compare_select=dom_create_append(div, "select");
+    this.compare_select.onchange=this.compare_change.bind(this);
+    for(var i=0; i<ui_langs.length; i++) {
+      var opt=dom_create_append(this.compare_select, "option");
+      opt.value=ui_langs[i];
+      dom_create_append_text(opt, lang("lang:"+ui_langs[i]));
+      if(ui_langs[i]=="en")
+	opt.selected=true;
+    }
+    // place holder for loading-indicator
+    dom_create_append(div, "span");
+    // request compare translation
+    this.compare_change();
   }
 
-  var div=dom_create_append(translation_form, "div");
-  
-  var input=dom_create_append(div, "input");
-  input.type="submit";
-  input.value=lang("save");
+  // load_callback
+  this.load_callback=function(ret) {
+    ret=ret.return_value;
 
-  // Choose compare language
-  dom_create_append_text(div, lang("translation:compare")+":");
-  translation_compare_select=dom_create_append(div, "select");
-  translation_compare_select.onchange=translation_compare_change;
-  for(var i=0; i<ui_langs.length; i++) {
-    var opt=dom_create_append(translation_compare_select, "option");
-    opt.value=ui_langs[i];
-    dom_create_append_text(opt, lang("lang:"+ui_langs[i]));
-    if(ui_langs[i]=="en")
-      opt.selected=true;
+    this.show(ret);
   }
-  // place holder for loading-indicator
-  dom_create_append(div, "span");
-  // request compare translation
-  translation_compare_change();
+
+  // constructor
+  this.lang=l;
+  this.win=new win({ title: lang("translation:name"), class: 'translation_win' });
+  ajax("translation_read", { lang: this.lang }, this.load_callback.bind(this));
+  this.win.content.innerHTML="<img class='loading' src='img/ajax_loader.gif'> "+t("loading");
 }
 
 function translation_open() {
-  translation_win=new win({ title: lang("translation:name"), class: 'translation_win' });
-  ajax("translation_read", { lang: ui_lang }, translation_open_next1);
-  translation_win.content.innerHTML="<img class='loading' src='img/ajax_loader.gif'> "+t("loading");
-}
-
-function translation_open_next1(ret) {
-  ret=ret.return_value;
-
-  translation_show(ret);
+  new translation(ui_lang);
 }
 
 function translation_files_list() {
--- www/plugins/translation/code.php
+++ www/plugins/translation/code.php
@@ -13,6 +13,33 @@ function translation_init() {
   }
 }
 
+function translation_print_value($v) {
+  global $lang_genders;
+  $repl=array("\""=>"\\\"", "\\"=>"\\\\");
+
+  if(is_array($v)&&(sizeof($v)==1))
+    $v=$v[0];
+  if(is_string($v))
+    return "\"".strtr($v, $repl)."\"";
+
+  $ret="array(";
+  if($lang_genders[$v[0]]) {
+    $ret="array({$lang_genders[$v[0]]}, ";
+    array_shift($v);
+  }
+  elseif(in_array($v[0], array("M", "F", "N"))) {
+    $ret="array($v[0], ";
+    array_shift($v);
+  }
+
+  foreach($v as $vi=>$vv) {
+    $v[$vi]=strtr($vv, $repl);
+  }
+  $ret.="\"".implode("\", \"", $v)."\")";
+
+  return $ret;
+}
+
 function translation_files_list() {
   global $translation_path;
 
@@ -50,216 +77,197 @@ function ajax_translation_files_list() {
   return translation_files_list();
 }
 
-function translation_read_file_php($lang, $f) {
-  $ret=array();
-  global $translation_path;
-  $file_en="$translation_path/{$f}en.php";
-  $file="$translation_path/$f$lang.php";
+class translation {
+  public $lang;
 
-  if(!file_exists($file))
-    return null;
+  // constructor
+  function __construct($lang) {
+    $this->lang=$lang;
+  }
 
-  include "$file";
+  // save
+  function save($changed, $param) {
+    global $translation_path;
+    global $current_user;
+    translation_init();
 
-  $f=fopen($file_en, "r");
-  while($r=fgets($f)) {
-    if(preg_match("/^( *)\\\$lang_str\[['\"]([^\"]*)['\"]\]/", $r, $m)) {
-      $found=false;
-      if(isset($lang_str[$m[2]]))
-	$ret[$m[2]]=array('value'=>$lang_str[$m[2]]);
-      else
-	$ret[$m[2]]="";
-      if(preg_match("/\\/\\/\s*(.*)$/", $r, $m1)) {
-	$ret[$m[2]]['help']=$m1[1];
+    if(!$changed)
+      return;
+
+    foreach($changed as $f=>$data) {
+      if(preg_match("/^(php|doc|category):(.*)$/", $f, $m)) {
+	$mode=$m[1];
+	$file=$m[2];
+      }
+
+      switch($mode) {
+	case "php":
+	  $this->write_file_php($file, $data);
+	  break;
+	case "doc":
+	  $this->write_file_doc($file, $data);
+	  break;
+	case "category":
+	  $this->write_file_category($file, $data);
+	  break;
       }
     }
-  }
-  fclose($f);
 
-  return array(
-    'list'=>$ret,
-    'order'=>array_keys($ret),
-  );
-}
-
-function translation_print_value($v) {
-  global $lang_genders;
-  $repl=array("\""=>"\\\"", "\\"=>"\\\\");
-
-  if(is_array($v)&&(sizeof($v)==1))
-    $v=$v[0];
-  if(is_string($v))
-    return "\"".strtr($v, $repl)."\"";
-
-  $ret="array(";
-  if($lang_genders[$v[0]]) {
-    $ret="array({$lang_genders[$v[0]]}, ";
-    array_shift($v);
-  }
-  elseif(in_array($v[0], array("M", "F", "N"))) {
-    $ret="array($v[0], ";
-    array_shift($v);
+    chdir($translation_path);
+    $author=$current_user->get_author();
+    $msg=strtr($param['msg'], array("\""=>"\\\""));
+    $p=popen("git commit --message=\"$msg\" --author=\"$author\"", "r");
+    pclose($p);
   }
 
-  foreach($v as $vi=>$vv) {
-    $v[$vi]=strtr($vv, $repl);
+  // read_file_php
+  function read_file_php($f) {
+    $ret=array();
+    global $translation_path;
+    $file_en="$translation_path/{$f}en.php";
+    $file="$translation_path/$f{$this->lang}.php";
+
+    if(!file_exists($file))
+      return null;
+
+    include "$file";
+
+    $f=fopen($file_en, "r");
+    while($r=fgets($f)) {
+      if(preg_match("/^( *)\\\$lang_str\[['\"]([^\"]*)['\"]\]/", $r, $m)) {
+	$found=false;
+	if(isset($lang_str[$m[2]]))
+	  $ret[$m[2]]=array('value'=>$lang_str[$m[2]]);
+	else
+	  $ret[$m[2]]="";
+	if(preg_match("/\\/\\/\s*(.*)$/", $r, $m1)) {
+	  $ret[$m[2]]['help']=$m1[1];
+	}
+      }
+    }
+    fclose($f);
+
+    return array(
+      'list'=>$ret,
+      'order'=>array_keys($ret),
+    );
   }
-  $ret.="\"".implode("\", \"", $v)."\")";
 
-  return $ret;
-}
+  // write_file_php
+  function write_file_php($f, $data) {
+    $lang_str=array();
 
-function translation_write_file_php($lang, $f, $data) {
-  $lang_str=array();
+    global $translation_path;
+    $file="$translation_path/$f{$this->lang}.php";
+    include $file;
+    if(!is_array($data))
+      return;
+    $lang_str=array_merge($lang_str, $data);
 
-  global $translation_path;
-  $file="$translation_path/$f$lang.php";
-  include $file;
-  $lang_str=array_merge($lang_str, $data);
-
-  $f_en=fopen("$translation_path/{$f}en.php", "r");
-  $f_t=fopen($file, "w");
-  while($r=fgets($f_en)) {
-    if(preg_match("/^(#?)\\\$lang_str\[[\"']([^\"']*)[\"']\]\s*=.*;(.*)/", $r, $m)) {
-      if(!$lang_str[$m[2]]) {
-	fputs($f_t, "#$r");
+    $f_en=fopen("$translation_path/{$f}en.php", "r");
+    $f_t=fopen($file, "w");
+    while($r=fgets($f_en)) {
+      if(preg_match("/^(#?)\\\$lang_str\[[\"']([^\"']*)[\"']\]\s*=.*;(.*)/", $r, $m)) {
+	if(!$lang_str[$m[2]]) {
+	  fputs($f_t, "#$r");
+	}
+	else {
+	  $value=translation_print_value($lang_str[$m[2]]);
+	  $str="$m[1]\$lang_str[\"$m[2]\"]=$value;$m[4]\n";
+	  fputs($f_t, $str);
+	}
       }
       else {
-	$value=translation_print_value($lang_str[$m[2]]);
-	$str="$m[1]\$lang_str[\"$m[2]\"]=$value;$m[4]\n";
-	fputs($f_t, $str);
+	fputs($f_t, $r);
       }
     }
-    else {
-      fputs($f_t, $r);
-    }
+    fclose($f_t);
+    fclose($f_en);
+
+    chdir($translation_path);
+    system("git add $file");
   }
-  fclose($f_t);
-  fclose($f_en);
 
-  chdir($translation_path);
-  system("git add $file");
-}
+  // write_file_doc
+  function write_file_doc($f, $data) {
+    $lang_str=array();
 
-function translation_write_file_doc($lang, $f, $data) {
-  $lang_str=array();
+    global $translation_path;
+    $file="$translation_path/$f{$this->lang}.doc";
 
-  global $translation_path;
-  $file="$translation_path/$f$lang.doc";
+    file_put_contents($file, $data);
 
-  file_put_contents($file, $data);
+    chdir($translation_path);
+    system("git add $file");
+  }
 
-  chdir($translation_path);
-  system("git add $file");
-}
+  // read_file_doc
+  function read_file_doc($f) {
+    $ret=array();
+    global $translation_path;
+    $file="$translation_path/$f{$this->lang}.doc";
 
-function translation_read_file_doc($lang, $f) {
-  $ret=array();
-  global $translation_path;
-  $file="$translation_path/$f$lang.doc";
+    if(!file_exists($file))
+      return array(
+	'contents'=>"",
+      );
 
-  if(!file_exists($file))
     return array(
-      'contents'=>"",
-    );
-
-  return array(
-    'contents'=>file_get_contents($file),
-  );
-}
-
-function translation_read_file_category($lang, $f) {
-  $ret=array();
-  if(!preg_match("/^(.*):(.*)$/", $f, $m))
-    return null;
-
-  $category=$m[1];
-  $version=$m[2];
-
-  $res=sql_query("select * from category where version='$version'", $db_central);
-  $elem=pg_fetch_assoc($res);
-  $tags=parse_hstore($elem['tags']);
-  $orig_lang=$tags['lang'];
-  if(!$orig_lang)
-    $orig_lang="en";
-
-  $ret["$category:name"]=array(
-    'value'=>$tags["name:$lang"],
-    'help'=>"Category name",
-  );
-  $ret["$category:description"]=array(
-    'value'=>$tags["description:$lang"],
-    'help'=>"Category description",
-  );
-
-  $res_rule=sql_query("select * from category_rule where category_id='$category' and version='$version'", $db_central);
-  while($elem_rule=pg_fetch_assoc($res_rule)) {
-    $tags=parse_hstore($elem_rule['tags']);
-    $rule_id=$elem_rule['rule_id'];
-
-    $ret["$category:$rule_id:name"]=array(
-      'value'=>($lang==$orig_lang?$tags["name"]:$tags["name:$lang"]),
-      'help'=>"Match: {$tags['match']}",
-    );
-    $ret["$category:$rule_id:description"]=array(
-      'value'=>($lang==$orig_lang?$tags["description"]:$tags["description:$lang"]),
+      'contents'=>file_get_contents($file),
     );
   }
 
-  return array(
-    'list'=>$ret,
-    'help'=>"Please use the form =array([Gender,] \"Singular\", \"Plural\") where appropriate (see top of page for explanation)",
-    'orig_lang'=>$orig_lang,
-    'order'=>array_keys($ret),
-  );
-}
+  // read_file_category
+  function read_file_category($f) {
+    $ret=array();
+    if(!preg_match("/^(.*):(.*)$/", $f, $m))
+      return null;
 
-function translation_read_file($lang, $f) {
-  global $translation_path;
+    $category=$m[1];
+    $version=$m[2];
 
-  if(preg_match("/^(php|doc|category):(.*)$/", $f, $m)) {
-    $mode=$m[1];
-    $file=$m[2];
+    $res=sql_query("select * from category where version='$version'", $db_central);
+    $elem=pg_fetch_assoc($res);
+    $tags=parse_hstore($elem['tags']);
+    $orig_lang=$tags['lang'];
+    if(!$orig_lang)
+      $orig_lang="en";
+
+    $ret["$category:name"]=array(
+      'value'=>$tags["name:{$this->lang}"],
+      'help'=>"Category name",
+    );
+    $ret["$category:description"]=array(
+      'value'=>$tags["description:{$this->lang}"],
+      'help'=>"Category description",
+    );
+
+    $res_rule=sql_query("select * from category_rule where category_id='$category' and version='$version'", $db_central);
+    while($elem_rule=pg_fetch_assoc($res_rule)) {
+      $tags=parse_hstore($elem_rule['tags']);
+      $rule_id=$elem_rule['rule_id'];
+
+      $ret["$category:$rule_id:name"]=array(
+	'value'=>($this->lang==$orig_lang?$tags["name"]:$tags["name:{$this->lang}"]),
+	'help'=>"Match: {$tags['match']}",
+      );
+      $ret["$category:$rule_id:description"]=array(
+	'value'=>($lang==$orig_lang?$tags["description"]:$tags["description:{$this->lang}"]),
+      );
+    }
+
+    return array(
+      'list'=>$ret,
+      'help'=>"Please use the form =array([Gender,] \"Singular\", \"Plural\") where appropriate (see top of page for explanation)",
+      'orig_lang'=>$orig_lang,
+      'order'=>array_keys($ret),
+    );
   }
 
-  switch($mode) {
-    case "php":
-      return translation_read_file_php($lang, $file);
-    case "doc":
-      return translation_read_file_doc($lang, $file);
-    case "category":
-      return translation_read_file_category($lang, $file);
-  }
-}
+  // read_file
+  function read_file($f) {
+    global $translation_path;
 
-function translation_read($lang, $files=null) {
-  $ret=array();
-  if(!$files)
-    $files=translation_files_list();
-
-  foreach($files as $f) {
-    $ret[$f]=translation_read_file($lang, $f);
-  }
-
-  return $ret;
-}
-
-function ajax_translation_read($param) {
-  return translation_read($param['lang'], $param['files']);
-}
-
-function translation_main_links($links) {
-  $links[]=array(5, "<a href='javascript:translation_open()'>".lang("translation:name")."</a>");
-}
-
-function ajax_translation_save($param) {
-  global $translation_path;
-  global $current_user;
-  translation_init();
-
-  $lang=$param['lang'];
-
-  foreach($param['changed'] as $f=>$data) {
     if(preg_match("/^(php|doc|category):(.*)$/", $f, $m)) {
       $mode=$m[1];
       $file=$m[2];
@@ -267,22 +275,41 @@ function ajax_translation_save($param) {
 
     switch($mode) {
       case "php":
-	translation_write_file_php($lang, $file, $data);
-	break;
+	return $this->read_file_php($file);
       case "doc":
-	translation_write_file_doc($lang, $file, $data);
-	break;
+	return $this->read_file_doc($file);
       case "category":
-	translation_write_file_category($lang, $file, $data);
-	break;
+	return $this->read_file_category($file);
     }
   }
 
-  chdir($translation_path);
-  $author=$current_user->get_author();
-  $msg=strtr($param['msg'], array("\""=>"\\\""));
-  $p=popen("git commit --message=\"$msg\" --author=\"$author\"", "r");
-  pclose($p);
+  // read
+  function read($files=null) {
+    $ret=array();
+    if(!$files)
+      $files=translation_files_list();
+
+    foreach($files as $f) {
+      $ret[$f]=$this->read_file($f);
+    }
+
+    return $ret;
+  }
+} // class
+
+function ajax_translation_save($param) {
+  print_r($param);
+  $t=new translation($param['lang']);
+  return $t->save($param['changed'], $param);
+}
+
+function ajax_translation_read($param) {
+  $t=new translation($param['lang']);
+  return $t->read();
+}
+
+function translation_main_links($links) {
+  $links[]=array(5, "<a href='javascript:translation_open()'>".lang("translation:name")."</a>");
 }
 
 register_hook("main_links", "translation_main_links");
