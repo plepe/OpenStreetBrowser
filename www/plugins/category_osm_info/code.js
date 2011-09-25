1e843f1b50b1724780878cfca1cc2014e4074a32
Stephan Plepelits <skunk@xover.mud.at>
2011-09-25 14:50:49 +0200

    Plugin 'Category OSM Info': Improve display of rules

--- www/plugins/category_osm_info/code.js
+++ www/plugins/category_osm_info/code.js
@@ -31,15 +31,49 @@ function category_osm_info_show(cat_win, category) {
     if(imp_list[importance_levels[i]]) {
       var h2=dom_create_append(t.content, "h2");
       dom_create_append_text(h2, lang("importance:name")+": "+importance_lang(importance_levels[i]));
+      var ul=dom_create_append(t.content, "ul");
 
       for(var j=0; j<imp_list[importance_levels[i]].length; j++) {
 	var rule=imp_list[importance_levels[i]][j];
 
-	var div=dom_create_append(t.content, "div");
-	dom_create_append_text(div, rule.name());
+	var div=rule.category_osm_info();
+	if(div)
+	  ul.appendChild(div);
       }
     }
   }
 }
 
+function category_osm_info_rule_construct(ob) {
+  ob.category_osm_info=function() {
+    var x;
+    var div=document.createElement("li");
+
+    if(x=get_icon(this.tags.get("icon"))) {
+      if(x)
+	div.style.listStyleImage="url('"+x.icon_url()+"')";
+    }
+
+    var h3=dom_create_append(div, "h3");
+    dom_create_append_text(h3, this.name());
+
+    if(this.tags.get_lang("description", ui_lang)) {
+      var div1=dom_create_append(div, "div");
+      div1.className="description";
+
+      dom_create_append_text(div1, t("category_rule_tag:description")+": "+this.tags.get_lang("description", ui_lang));
+    }
+
+    if(this.tags.get("match")) {
+      var div1=dom_create_append(div, "div");
+      div1.className="match";
+
+      dom_create_append_text(div1, t("category_rule_tag:match")+": "+this.tags.get("match"));
+    }
+
+    return div;
+  }
+}
+
 register_hook("category_window_show", category_osm_info_show);
+register_hook("category_rule_construct", category_osm_info_rule_construct);
