669f85891b5b387e52cad47dfa7cde62573eee08
Stephan Plepelits <skunk@xover.mud.at>
2011-09-28 08:25:31 +0200

    Several Plugins: Fix dependencies to plugin 'Win'

--- www/plugins/blog/conf.php
+++ www/plugins/blog/conf.php
@@ -1,7 +1,7 @@
 <?
 $blog_active=true;
 
-$blog_depend=array();
+$blog_depend=array("win");
 
 $blog_tags=new tags(array(
   "name"=>"Blog",
--- www/plugins/doc/conf.php
+++ www/plugins/doc/conf.php
@@ -1,7 +1,7 @@
 <?
 $doc_active=true;
 
-$doc_depend=array("creole");
+$doc_depend=array("win", "creole");
 
 $doc_tags=new tags(array(
   "name"=>"Documentation",
