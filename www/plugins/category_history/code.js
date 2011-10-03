1584e3900a8836d52f7d49e99c68b5f178147d88
Stephan Plepelits <skunk@xover.mud.at>
2011-10-03 08:28:25 +0200

    Plugin 'Category History': Show loaded history in window

--- www/plugins/category_history/code.js
+++ www/plugins/category_history/code.js
@@ -1,4 +1,41 @@
 function category_history(id, cat_win) {
+  // show_version
+  this.show_version=function(param) {
+    alert(print_r(param));
+  }
+
+  // show
+  this.show=function() {
+    var ret=this.data;
+
+    dom_clean(this.content_div);
+    dom_create_append(this.content_div, "ul");
+
+    for(var i in ret) {
+      var e=ret[i];
+
+      var li=dom_create_append(this.content_div, "li");
+
+      var a=dom_create_append(li, "a");
+      var text=e.version_tags.date;
+      if(!text)
+	text="?";
+      dom_create_append_text(a, text);
+      a.href="#";
+      a.onclick=this.show_version.bind(this, { page: this.id, version: e.version });
+
+      dom_create_append_text(li, " by ");
+
+      // TODO: as soon as user_show() is implemented change to "a"
+      var a=dom_create_append(li, "span");
+      //a.href="javascript:user_show(\""+e.version_tags.user+"\")";
+      var text=e.version_tags.user;
+      if(!text)
+	text="?";
+      dom_create_append_text(a, text);
+    }
+  }
+
   // load
   this.load=function() {
     var param={
@@ -10,7 +47,8 @@ function category_history(id, cat_win) {
 
   // load_callback
   this.load_callback=function(ret) {
-    alert(ret);
+    this.data=ret.return_value;
+    this.show();
   }
 
   // constructor
@@ -19,6 +57,7 @@ function category_history(id, cat_win) {
     this.id="new";
   this.win=new tab({ class: "category_history", title: lang("category_history:name", 1) });
   this.win.content.innerHTML="<img src='img/ajax_loader.gif' /> "+lang("loading");
+  this.content_div=this.win.content;
   cat_win.tab_manager.register_tab(this.win);
   this.load();
 }
