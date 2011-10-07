2f53429eb6a6af7e23e7e9a6a2ad2fca9fa830aa
Stephan Plepelits <skunk@xover.mud.at>
2011-10-07 06:12:53 +0200

    Plugin 'Recent Changes': Sort returned entries to list

--- www/plugins/recent_changes/code.js
+++ www/plugins/recent_changes/code.js
@@ -2,12 +2,47 @@
 // [ { date: '2011-12-13 14:15:16', user: 'test', msg: 'changed bla', plugin: 'talk', 'href': link to page }, ... ]
 //
 function recent_changes() {
+  function recent_changes_sort(a, b) {
+    return b-a;
+  }
+
+  // list_sort
+  this.list_sort=function() {
+    var tmp_list={};
+
+    // first add all elements to an hash array with index of date
+    for(var i=0; i<this.list.length; i++) {
+      var e=this.list[i];
+      var d=new Date(e.date);
+
+      if(!tmp_list[d.valueOf()])
+	tmp_list[d.valueOf()]=[];
+
+      tmp_list[d.valueOf()].push(e);
+    }
+
+    // get the keys, order them
+    var k=keys(tmp_list);
+    k.sort(recent_changes_sort);
+
+    // write to new_list
+    var new_list=[];
+    for(var i=0; i<k.length; i++) {
+      for(var j=0; j<tmp_list[k[i]].length; j++) {
+	new_list.push(tmp_list[k[i]][j]);
+      }
+    }
+
+    this.list=new_list;
+  }
+
   // close
   this.remove=function() {
   }
 
   // load
   this.load=function() {
+    this.list=[];
     ajax("recent_changes_load", null, this.load_callback.bind(this));
     call_hooks("recent_changes_load", this.list);
   }
@@ -17,6 +52,8 @@ function recent_changes() {
     ret=ret.return_value;
 
     this.list=this.list.concat(ret);
+    this.list_sort();
+    alert(print_r(this.list));
   }
 
   // constructor
