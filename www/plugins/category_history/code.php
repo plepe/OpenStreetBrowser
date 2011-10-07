32b90aa5dc94d382d89781f2272fe6ac835d06e7
Stephan Plepelits <skunk@xover.mud.at>
2011-10-07 06:43:59 +0200

    Plugin 'Category History': Write changes to categories to recent changes list

--- www/plugins/category_history/code.php
+++ www/plugins/category_history/code.php
@@ -2,3 +2,21 @@
 function ajax_category_history($param) {
  return category_history($param['id']);
 }
+
+function category_history_recent_changes($list) {
+  $res=sql_query("select * from category where version_tags ? 'date' order by version_tags->'date' desc limit 50");
+  while($elem=pg_fetch_assoc($res)) {
+    $elem['version_tags']=parse_hstore($elem['version_tags']);
+    $t=new tags(parse_hstore($elem['tags']));
+
+    $entry=$elem['version_tags'];
+    $entry['name']=sprintf("%s \"%s\"", lang("category", 1), $t->get_lang("name"));
+    $entry['msg']=coalesce($elem['version_tags']['comment'], "no message");
+    $entry['plugin']="category";
+    $entry['href']="javascript:category_show(\"osm:{$elem['category_id']}\", { version: \"{$elem['version']}\"})";
+
+    $list[]=$entry;
+  }
+}
+
+register_hook("recent_changes_load", "category_history_recent_changes");
