dc24f64e00f279c42eaa4427d5974a5f737c1d86
Stephan Plepelits <skunk@xover.mud.at>
2011-10-16 11:44:58 +0200

    Ajax: ajax_indicator_dom() returns an ajax loading indicator
    - Backport of original 05b0135bf8f7a4eb6ca3628e1eeb2922f0ebeba0

--- www/ajax.js
+++ www/ajax.js
@@ -321,4 +321,15 @@ function ajax_post(url, getparam, postdata, _callback) {
   return req;
 }
 
+function ajax_indicator_dom() {
+  var span=document.createElement("span");
+  
+  var img=dom_create_append(span, "img");
+  img.src="img/ajax_loader.gif";
 
+  dom_create_append_text(span, " ");
+
+  dom_create_append_text(span, lang("loading"));
+
+  return span;
+}
