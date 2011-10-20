1ada9132ea66af995a74177a99f8a7180c58f9a5
Stephan Plepelits <skunk@xover.mud.at>
2011-10-20 07:07:10 +0200

    Plugin 'Debug Toolbox': Let plugins register to debug toolbox

--- www/plugins/debug_toolbox/code.js
+++ www/plugins/debug_toolbox/code.js
@@ -1,4 +1,13 @@
 var debug_toolbox;
+var debug_toolbox_items=[];
+
+function debug_toolbox_register(param) {
+  var div=document.createElement("div");
+  debug_toolbox.content.appendChild(div);
+
+  debug_toolbox_items.push([ param.weight, div ]);
+  div.appendChild(param.dom);
+}
 
 function debug_toolbox_init() {
   debug_toolbox=new toolbox({
