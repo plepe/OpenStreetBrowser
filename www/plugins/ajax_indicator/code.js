c702a59fb8a858625a49a239ccfe0ee321397992
Stephan Plepelits <skunk@xover.mud.at>
2011-10-23 08:06:54 +0200

    Plugin 'Ajax Indicator': Convert to a class

--- www/plugins/ajax_indicator/code.js
+++ www/plugins/ajax_indicator/code.js
@@ -2,8 +2,14 @@
  * Options:
  * ajax_action object (optional) the running ajax action
  */
-function ajax_indicator_dom(ajax_action) {
+function ajax_indicator(ajax_action) {
+  this.ajax_action=ajax_action;
+}
+
+// ajax_indicator.dom()
+ajax_indicator.prototype.dom=function() {
   var span=document.createElement("span");
+  span.className="ajax_indicator";
   
   var img=dom_create_append(span, "img");
   img.src="img/ajax_loader.gif";
@@ -13,4 +19,12 @@ function ajax_indicator_dom(ajax_action) {
   dom_create_append_text(span, lang("loading"));
 
   return span;
+}
+
+/**
+ * create ajax_indicator and return a dom of it
+ */
+function ajax_indicator_dom(ajax_action) {
+  var ai=new ajax_indicator(ajax_action);
+  return ai.dom();
 }
