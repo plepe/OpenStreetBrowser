f694feb4f53c0057a82883791787557239c3d247
Stephan Plepelits <skunk@xover.mud.at>
2011-11-12 07:42:57 +0100

    Plugin 'Cloudmade Basemap': Use plugins_include_file() to include library

--- www/plugins/cloudmade_basemap/code.php
+++ www/plugins/cloudmade_basemap/code.php
@@ -1,16 +1,9 @@
 <?
-global $design_hidden;
-if(!$design_hidden) {
-  print "<script type='text/javascript' src='plugins/cloudmade_basemap/cloudmade.js'></script>\n";
-}
 
 function cloudmade_basemap_init() {
   global $key_cloudmade_api;
-  global $design_hidden;
 
-  if(!$design_hidden)
-    print "<script type='text/javascript' src='plugins/cloudmade_basemap/cloudmade.js'></script>\n";
   html_export_var(array("key_cloudmade_api"=>$key_cloudmade_api));
 }
 
-register_hook("init", "cloudmade_basemap_init");
+plugins_include_file("cloudmade_basemap", "cloudmade.js");
