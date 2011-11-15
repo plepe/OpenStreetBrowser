b2245529579acc3ba924b22fdcb76764ad36f7ad
Yannis Giftomitros <aitolos@openstreetbrowser.org>
2011-11-15 16:57:23 +0100

    Update translation (el)

---
+++ www/plugins/start_location/lang_el.php
@@ -0,0 +1,9 @@
+<?
+#$lang_str["start_location:name"]="Map Position";
+
+#$lang_str["start_location:choose"]="Choose map view";
+#$lang_str["start_location:geolocation"]="get geolocation";
+#$lang_str["start_location:lastview"]="last view";
+#$lang_str["start_location:savedview"]="last permalink";
+#$lang_str["start_location:startnormal"]="keep view";
+$lang_str["start_location:remember"]="Απομνημόνευση επιλογής";
