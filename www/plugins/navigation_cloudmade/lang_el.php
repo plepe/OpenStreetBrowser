09a6a7daa7caf0885515655c8afb6445e52f4061
Yannis Giftomitros <aitolos@openstreetbrowser.org>
2011-11-15 17:08:06 +0100

    Update translation (el)

---
+++ www/plugins/navigation_cloudmade/lang_el.php
@@ -0,0 +1,5 @@
+<?
+$lang_str["navigation_cloudmade:car"]="αυτοκίνητο";
+$lang_str["navigation_cloudmade:car_shortest"]="αυτοκίνητο (συντομότερη)";
+$lang_str["navigation_cloudmade:bicycle"]="ποδήλατο";
+$lang_str["navigation_cloudmade:foot"]="πεζός";
---
+++ www/plugins/wikipedia_streetnames/lang_el.php
@@ -0,0 +1,2 @@
+<?
+$lang_str["head:wikipedia_streetnames"]="Όνομα οδού";
