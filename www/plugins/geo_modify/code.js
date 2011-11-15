1198f360523ce106651b934fd1c97d9053ef18db
Stephan Plepelits <skunk@xover.mud.at>
2011-11-15 07:52:49 +0100

    Plugin 'Geo Modify': Add some debug-ui-stuff

--- www/plugins/geo_modify/code.js
+++ www/plugins/geo_modify/code.js
@@ -1,17 +1,32 @@
 var geo_modify_current;
+var geo_modify_config;
+var geo_modify_color_list=[ '#009f00', '#9f0000', '#008f8f', '#00009f', '#8f008f', '#8f8f00', '#009f3f', '#003f9f', '#3f009f', '#9f003f', '#3f9f00', '#9f3f00' ];
 
 function geo_modify(info, ob) {
   this.ob=ob;
 
-  this.load();
-
   // create geo_modify chapter in info-box
-  this.div=document.createElement("div");
+  var chapter=document.createElement("div");
   info.push({
-    head: "geo_modify",
+    head: lang("geo_modify:name"),
     weight: 5,
-    content: this.div
+    content: chapter
   });
+
+  this.inputs={};
+
+  var d=dom_create_append(chapter, "div");
+  this.inputs.debug=dom_create_append(d, "input");
+  this.inputs.debug.type="checkbox";
+  this.inputs.debug.checked=true;
+  if(geo_modify_config)
+    this.inputs.debug.checked=geo_modify_config.debug;
+  this.inputs.debug.onchange=this.refresh.bind(this);
+  dom_create_append_text(d, lang("geo_modify:debug"));
+
+  this.div=dom_create_append(chapter, "div");
+
+  this.load();
 }
 
 geo_modify.prototype.load=function() {
@@ -20,30 +35,58 @@ geo_modify.prototype.load=function() {
   param.fun="get_center";
   param.id=this.ob.id;
   param.zoom=map.zoom;
-  param.param={ };
+  param.param={ debug: this.inputs.debug.checked?"true":"false" };
   ajax("geo_modify", param, this.load_callback.bind(this));
 }
 
 geo_modify.prototype.load_callback=function(result) {
+  var debug_colors={};
   var value=result.return_value;
 
   geo_modify_info_hide();
 
-  // show modified geometry
-  var geo=new postgis(value.way);
-  this.features=geo.geo();
-  this.features[0].style= { pointRadius: 3, fillColor: "#ff0000", strokeColor: "#ff0000" };
-  vector_layer.addFeatures(this.features);
+  this.debug_features=[];
+  var debug_colors={ 'result': '#ff0000' };
+  debug_color_index=0;
 
   // show additional #geo_modify-tags
   var ul=dom_create_append(this.div, "ul");
-  for(var i in value.tags) {
+  var ks=keys(value.tags);
+  ks.sort();
+  for(var ki=0; ki<ks.length; ki++) {
+    var i=ks[ki];
     var k;
-    if(k=i.match(/^#geo_modify:(.*)/)) {
+    if((k=i.match(/^#geo_modify:([^:]*):/))&&(!debug_colors[k[1]])) {
+      debug_colors[k[1]]=geo_modify_color_list[debug_color_index++];
+    }
+
+    if(k=i.match(/^#geo_modify:([^:]*):geo/)) {
+      var geo=new postgis(value.tags[i]);
+      var features=geo.geo();
+      for(var j=0; j<features.length; j++) {
+	features[j].style= { pointRadius: 3, fillColor: debug_colors[k[1]], strokeColor: debug_colors[k[1]], stroke: true, strokeWidth: 1, strokeOpacity: 1, fillOpacity: 0.2 };
+      }
+      vector_layer.addFeatures(features);
+      this.debug_features.push(features);
+    }
+    else if(k=i.match(/^#geo_modify:(([^:]*):.*)/)) {
       var li=dom_create_append(ul, "li");
+      li.style.color=debug_colors[k[2]];
       dom_create_append_text(li, k[1]+": "+value.tags[i]);
     }
   }
+
+  // show modified geometry
+  var geo=new postgis(value.way);
+  this.features=geo.geo();
+  this.features[0].style= { pointRadius: 3, fillColor: "#ff0000", strokeColor: "#ff0000", stroke: true, strokeWidth: 1 };
+  vector_layer.addFeatures(this.features);
+
+}
+
+geo_modify.prototype.refresh=function() {
+  this.hide();
+  this.load();
 }
 
 geo_modify.prototype.hide=function() {
@@ -51,6 +94,15 @@ geo_modify.prototype.hide=function() {
     vector_layer.removeFeatures(this.features);
     delete(this.features);
   }
+  if(this.debug_features) {
+    for(var i=0; i<this.debug_features.length; i++) {
+      vector_layer.removeFeatures(this.debug_features[i]);
+    }
+    delete(this.debug_features);
+  }
+  dom_clean(this.div);
+
+  geo_modify_config={ debug: this.inputs.debug.checked };
 }
 
 function geo_modify_info_hide() {
