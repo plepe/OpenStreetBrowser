plepe/OpenStreetBrowser

commit 685e113cc3416c112d77d86fd93b2e7d614ca703

Category: Add a debug-comment at start of sql-queries
- it makes long lasting requests visible in pg_stat_activity

www/inc/categories.php
@@ -660,7 +660,9 @@ function build_mapnik_style($id, $data, $global_tags) {
       $sql_select="\n  ".implode(",\n  ", $sql_select);
       $sql_join="\n  ".implode("\n  ", $sql_join);
 
-      $sql="(select{$sql_select} from ($sql) as t{$sql_join}) as u";
+      $sql ="";
+      $sql.="/* {$id}.mapnik: s!scale_denominator!, {$importance} */ ";
+      $sql.="(select{$sql_select} from ($sql) as t{$sql_join}) as u";
 
       if(in_array($table, array("polygon"))) {
 	$layer=mapnik_get_layer($dom, "{$id}_{$importance}_{$table}_shape", $sql, "polygon");

www/inc/category.php
@@ -251,7 +251,14 @@ class category {
 	    "!bbox!"=>"PolyFromText('POLYGON(($coord[0] $coord[1], $coord[2] $coord[1], $coord[2] $coord[3], $coord[0] $coord[3], $coord[0] $coord[1]))', 900913)",
 	  ));
 
-	  $qryc="select *, astext(ST_Centroid(geo)) as center from (";
+	  // Build Query
+          $qryc ="";
+
+	  // Some debug info, visible in pg_stat_activity
+	  $qryc.="/* {$this->id}.get_list: z{$param['zoom']}, {$imp} */ ";
+
+	  // Main query
+	  $qryc.="select *, astext(ST_Centroid(geo)) as center from (";
 	  $qryc.=$sql;
 	  $qryc.=") as x where $where limit $max_count";
 	  //print "==\n$qryc\n==";

commit 17140a0c8181afcc8c3fbfd733f766f6697dba7a

Categories: Fix bug in creating sql string
- bug introduced in commit 685e113cc3416c112d77d86fd93b2e7d614ca703

www/inc/categories.php
@@ -643,7 +643,7 @@ function build_mapnik_style($id, $data, $global_tags) {
       print "Columns (2): ";
       print_r($columns);
 
-      $sql=$data2['sql'];
+      $sql_tables=$data2['sql'];
       $sql_select=array();
       $sql_join=array();
       $sql_select[]="t.*";
@@ -662,7 +662,7 @@ function build_mapnik_style($id, $data, $global_tags) {
 
       $sql ="";
       $sql.="/* {$id}.mapnik: s!scale_denominator!, {$importance} */ ";
-      $sql.="(select{$sql_select} from ($sql) as t{$sql_join}) as u";
+      $sql.="(select{$sql_select} from ($sql_tables) as t{$sql_join}) as u";
 
       if(in_array($table, array("polygon"))) {
 	$layer=mapnik_get_layer($dom, "{$id}_{$importance}_{$table}_shape", $sql, "polygon");