f189155abba65c516263ae80cff957ef03c5e56a
Yannis Giftomitros <aitolos@openstreetbrowser.org>
2011-11-15 17:37:58 +0100

    Update translation (el)

--- www/plugins/start_location/lang_el.php
+++ www/plugins/start_location/lang_el.php
@@ -1,7 +1,7 @@
 <?
-#$lang_str["start_location:name"]="Map Position";
+$lang_str["start_location:name"]="Θέση Χάρτη";
 
-#$lang_str["start_location:choose"]="Choose map view";
+$lang_str["start_location:choose"]="Επιλογή όψης χάρτη";
 #$lang_str["start_location:geolocation"]="get geolocation";
 #$lang_str["start_location:lastview"]="last view";
 #$lang_str["start_location:savedview"]="last permalink";
---
+++ www/plugins/translation/lang_el.php
@@ -0,0 +1,10 @@
+<?
+$lang_str["translation:name"]="Μετάφραση";
+$lang_str["translation:string_id"]="ID";
+$lang_str["translation:translation"]="Μετάφραση";
+$lang_str["translation:compare"]="Σύγκριση";
+$lang_str["translation:improve"]="Βελτίωση μετάφρασης";
+$lang_str["translation:new"]="Δημιουργία νέας μετάφρασης";
+#$lang_str["translation:enter_lang_code"]="Enter the language code for the new language";
+#$lang_str["translation:enter_lang_code_help"]="e.g. 'en' for English or 'en-gb' for a language variant like 'British English'";
+#$lang_str["translation:add_value"]="Add value for '%s'";
