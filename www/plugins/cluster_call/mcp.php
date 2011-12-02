c23601eba00a57ed6ca856fb888268d0fe0bb77a
Stephan Plepelits <skunk@xover.mud.at>
2011-12-02 07:28:20 +0100

    Plugin 'Cluster Call': Use hook 'mcp_clean_up_master' to remove old entries

--- www/plugins/cluster_call/mcp.php
+++ www/plugins/cluster_call/mcp.php
@@ -4,10 +4,18 @@ global $cluster_call_done;
 $cluster_call_registered=array();
 $cluster_call_done=array();
 
+function cluster_call_clean_up_master() {
+  global $db_central;
+
+  // After some time delete entries in cluster_call
+  sql_query("delete from cluster_call where now<now()-interval '6 hours'", $db_central);
+
+  print "Clean cluster calls\n";
+}
+
 function cluster_call_tick() {
   global $cluster_call_done;
   global $cluster_call_registered;
-  global $cluster_call_last_clean;
   global $root_path;
   global $db_central;
 
@@ -49,14 +57,6 @@ function cluster_call_tick() {
   $cluster_call_file="$root_path/data/cluster_call.save";
   file_put_contents($cluster_call_file, serialize($cluster_call_done));
 
-  // After some time delete entries in cluster_call
-  if((!isset($cluster_call_last_clean))||($cluster_call_last_clean+60<time())) {
-    $cluster_call_last_clean=time();
-    sql_query("delete from cluster_call where now<now()-interval '6 hours'", $db_central);
-
-    print "Clean cluster calls\n";
-  }
-
   // print "Check cluster call: ".sprintf("%.1fms", ((microtime(true)-$t)*1000))."\n";
 }
 
@@ -85,3 +85,4 @@ function cluster_call_start() {
 
 register_hook("mcp_start", "cluster_call_start");
 register_hook("mcp_tick", "cluster_call_tick");
+register_hook("mcp_clean_up_master", "cluster_call_clean_up_master");
