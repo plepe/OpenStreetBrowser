f7ad6f7afba7a2bc37095b34d43333e5b3c4b036
Stephan Plepelits <skunk@xover.mud.at>
2011-12-06 07:57:06 +0100

    Plugin 'Save Actions': Bugfix: Use correct function name (sql_query)

--- www/plugins/save_actions/mcp.php
+++ www/plugins/save_actions/mcp.php
@@ -3,7 +3,7 @@ function save_action_clean_up_master() {
   global $db_central;
 
   print "Clean-Up save_actions\n";
-  sql("delete from save_actions where now<now()-interval '6 hours'");
+  sql_query("delete from save_actions where now<now()-interval '6 hours'");
 }
 
 register_hook("mcp_clean_up_master", "save_action_clean_up_master");
