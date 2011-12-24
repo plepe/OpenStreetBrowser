3cf6d94244bc1078a835b54a3d6e5bcaf6df0b50
bazgal <bazgal@openstreetbrowser.org>
2011-12-24 18:38:01 +0100

    Update translation (fr)

--- www/lang/fr.php
+++ www/lang/fr.php
@@ -56,7 +56,7 @@ $lang_str["action_browse"]="visualiser dans OSM";
 $lang_str["action_edit"]="éditer dans OSM";
 
 $lang_str["geo_click_pos"]="Cliquez sur votre position sur la carte";
-$lang_str["geo_set_pos"]="Définir ma localisation";
+$lang_str["geo_set_pos"]="Définir ma position";
 $lang_str["geo_change_pos"]="Modifier ma position";
 
 $lang_str["routing_type_car"]="Voiture";
@@ -78,61 +78,61 @@ $lang_str["main:options"]="Options";
 $lang_str["main:about"]="À Propos de";
 $lang_str["main:donate"]="Donner";
 $lang_str["main:licence"]="Données : <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">cc-by-sa</a> <a href=\"http://www.openstreetmap.org\">OpenStreetMap</a> contributeurs | OSB: <a href=\"http://wiki.openstreetmap.org/wiki/User:Skunk\">Stephan Plepelits</a> and <a href=\"http://wiki.openstreetmap.org/wiki/OpenStreetBrowser#People_involved\">contributeurs</a>";
-$lang_str["main:permalink"]="Permalink";
+$lang_str["main:permalink"]="Lien permanent";
 
 $lang_str["help:no_object"]="<div class='obj_actions'><a class='zoom' href='#'></a></div><h1>Objet non trouvé</h1>Aucun objet avec le ID \"%s\" n'a pu être trouvé.Ceci peut être du à une (ou plus) des raisons suivantes::<ul><li>Le ID is inexistant.</li><li>L'objet a été identifié par un site tiers et n'est pas encore disponible dans OpenStreetBrowser.</li><li>L'objet n'est pas dans la zone supportée.</li><li>Le lien que vous suivez est ancien et a été effacé de from OpenStreetMap.</li></ul>";
 
 $lang_str["options:autozoom"]="Réglage Autozoom";
-$lang_str["help:autozoom"]="Lors de la sélection d'un objet, la fenêtre d'affichage focus sur cet objet. Le niveau de zoom peut aussi être modifié. Avec cette option vous pouvez sélectionner à partir de different modes.";
+$lang_str["help:autozoom"]="Lors de la sélection d'un objet, la fenêtre d'affichage focus sur cet objet, le niveau de zoom peut aussi être modifié. Avec cette option vous pouvez sélectionner à partir de différent modes.";
 $lang_str["options:autozoom:pan"]="Basculer vers l'objet courant (+ doux)";
 $lang_str["options:autozoom:move"]="Se déplacer vers l'objet courant (+ rapide)";
 $lang_str["options:autozoom:stay"]="Ne jamais modifier la fenêtre automatiquement";
 
 $lang_str["options:language_support"]="Langue supportée";
-$lang_str["help:language_support"]="Vous pouvez sélectionner votre langue préférée avec ces options. La première option modifie la langue de l'interface. La deuxième  option modifie la langue des données. À date, de nombreux objets géographiques ont été traduits dans plusieurs langues. Si aucune traduction n'est disponible ou que l'option \"Langue locale\" est sélectionnée, la langue principale de l'objet est affichée.";
-$lang_str["options:ui_lang"]="Langue de l'Interface";
+$lang_str["help:language_support"]="Vous pouvez sélectionner votre langue préférée avec ces options. La première option modifie la langue de l'interface. La deuxième option modifie la langue des données. À ce jour, de nombreux objets géographiques ont été traduits dans différentes langues. Si aucune traduction n'est disponible ou que l'option \"Langue locale\" est sélectionnée, la langue principale de l'objet est affichée.";
+$lang_str["options:ui_lang"]="Langue de l'interface";
 $lang_str["options:data_lang"]="Langue des données";
 $lang_str["lang:"]="Langue locale";
-#$lang_str["lang:auto"]="Same as interface language";
+$lang_str["lang:auto"]="Même lanque que pour l'interface";
 
 $lang_str["overlay:data"]="Données";
 $lang_str["overlay:draggable"]="Repères";
 
-$lang_str["user:no_auth"]="Code usager ou mot de passe incorret!";
-$lang_str["user:login_text"]="Connexion à OpenStreetBrowser:";
-$lang_str["user:create_user"]="Ajouter un nouvel usager:";
-$lang_str["user:username"]="Code usager";
-$lang_str["user:email"]="Adresse de courriel";
+$lang_str["user:no_auth"]="Nom d'utilisateur ou mot de passe incorrect !";
+$lang_str["user:login_text"]="Connexion à OpenStreetBrowser :";
+$lang_str["user:create_user"]="Créer un nouvel utilisateur :";
+$lang_str["user:username"]="Nom utilisateur";
+$lang_str["user:email"]="Adresse e-mail";
 $lang_str["user:password"]="Mot de passe";
 $lang_str["user:password_verify"]="Vérifier le mot de passe";
 $lang_str["user:old_password"]="Ancien mot de passe";
-$lang_str["user:no_username"]="SVP fournir un code usager!";
-$lang_str["user:password_no_match"]="Les mots de passe diffèrent!";
+$lang_str["user:no_username"]="SVP fournir un nom d'utilisateur !";
+$lang_str["user:password_no_match"]="Les mots de passe ne correspondent pas !";
 $lang_str["user:full_name"]="Nom complet";
-$lang_str["user:user_exists"]="le Code usager existe déjà";
+$lang_str["user:user_exists"]="Ce nom d'utilisateur existe déjà";
 $lang_str["user:login"]="Connexion";
 $lang_str["user:logged_in_as"]="Connexion avec identifiant ";
 $lang_str["user:logout"]="Déconnexion";
 
-#$lang_str["attention"]="Attention: ";
-$lang_str["error"]="Une erreur est survenue: ";
+$lang_str["attention"]="Attention :";
+$lang_str["error"]="Une erreur est survenue :";
 $lang_str["error:not_logged_in"]="vous n'êtes pas connecté";
 
-#$lang_str["category"]=array("Category", "Categories");
+$lang_str["category"]=array("Catégorie", "Catégories");
 $lang_str["more_categories"]="Plus de catégories";
 $lang_str["category:status"]="Statut";
 $lang_str["category:data_status"]="Statut";
 $lang_str["category:old_version"]="Une nouvelle version de cette catégorie est en préparation.";
 $lang_str["category:not_compiled"]="Une nouvelle catégorie est en préparation.";
 
-#$lang_str["category:new_rule"]="New Rule";
+$lang_str["category:new_rule"]="Nouvelle règle";
 $lang_str["category_rule_tag:match"]="Correspondance";
 $lang_str["category_rule_tag:description"]="Description";
 $lang_str["category_chooser:choose"]="Choisissez une catégorie";
 $lang_str["category_chooser:new"]="Nouvelle catégorie";
-#$lang_str["category:sub_category"]=array("Sub-category", "Sub-categories");
+$lang_str["category:sub_category"]=array("Sous-catégorie", "Sous-catégories");
 
 $lang_str["basemap:osb"]="OpenStreetBrowser";
 $lang_str["basemap:mapnik"]="Standard (Mapnik)";
 $lang_str["basemap:osmarender"]="Standard (OsmaRender)";
-$lang_str["basemap:cyclemap"]="CycleMap";
+$lang_str["basemap:cyclemap"]="Carte cyclable";
