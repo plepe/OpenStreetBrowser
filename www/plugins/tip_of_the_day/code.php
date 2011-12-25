d8802481c089e3be736b1f2b68a3c095fce9247f
Stephan Plepelits <skunk@xover.mud.at>
2011-12-25 21:53:57 +0100

    Plugin 'Tip of the day': Show a tip in the right bar

--- www/plugins/tip_of_the_day/code.php
+++ www/plugins/tip_of_the_day/code.php
@@ -24,3 +24,10 @@ function tip_of_the_day_html() {
 
   return creole_html(lang("tip_of_the_day:$i"));
 }
+
+function tip_of_the_day_right_bar($content) {
+  $content[]=array(0,
+    "<h1>".lang("tip_of_the_day:name")."</h1>".tip_of_the_day_html());
+}
+
+register_hook("right_bar_content", "tip_of_the_day_right_bar");
