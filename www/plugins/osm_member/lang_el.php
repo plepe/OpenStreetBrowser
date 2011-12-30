e3ba63c9d912b5b4b8c36e9bf4c68936d2cc6c7b
Yannis Giftomitros <aitolos@openstreetbrowser.org>
2011-12-30 16:43:52 +0100

    Update translation (el)

--- www/plugins/osm_member/lang_el.php
+++ www/plugins/osm_member/lang_el.php
@@ -2,4 +2,4 @@
 $lang_str["osm_member-members"]="Μέλη";
 $lang_str["osm_member-member_of"]="Μέλος του";
 $lang_str["osm_member-members-help"]="Όλα τα μέλη της σχέσης, με το ρόλο μέλους σε παρένθεση";
-#$lang_str["osm_member-member_of-help"]="All relations this object is a member of, with its role in brackets.";
+$lang_str["osm_member-member_of-help"]="Όλες οι σχέσεις που αυτό το αντικείμενο είναι μέλος με το ρόλο του σε παρένθεση.";
