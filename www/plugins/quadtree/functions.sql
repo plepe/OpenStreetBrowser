0fd355b815c1c4a00ee5f78489e7e98e4d344bce
Stephan Plepelits <skunk@xover.mud.at>
2012-01-03 04:54:59 +0100

    Plugin 'Quadtree': Simpler table-specific query function

--- www/plugins/quadtree/functions.sql
+++ www/plugins/quadtree/functions.sql
@@ -42,7 +42,7 @@ BEGIN
   execute 'create or replace function quadtree_get_way('||table_name||') returns geometry as $f$ select $1.way $f$ language sql;';
 
   -- create query function
-  execute 'create or replace function '||table_name||'_query(in boundary geometry, in _where text default '''', in options hstore default ''''::hstore) returns setof '||table_name||' as $f$ declare r '||table_name||'%rowtype; sql text; begin sql:=quadtree_compile_query('''||table_name||''', boundary, _where, options); for r in execute sql loop return next r; end loop; return; end; $f$ language plpgsql;';
+  execute 'create or replace function '||table_name||'_query(in boundary geometry, in _where text default '''', in options hstore default ''''::hstore) returns setof '||table_name||' as $f$ declare r '||table_name||'%rowtype; sql text; begin sql:=quadtree_compile_query('''||table_name||''', boundary, _where, options); return query execute sql; return; end; $f$ language plpgsql;';
 
   -- save list of current indexes
   for r in execute 'select * from pg_indexes where tablename='''||table_name||'''' loop
