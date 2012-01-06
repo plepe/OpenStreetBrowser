311337e1c086a2ff99f72bb9fbdf0704036c5275
Stephan Plepelits <skunk@xover.mud.at>
2012-01-06 11:41:52 +0100

    Plugin 'Layer Toolbox': Show list of basemaps and overlays

--- www/plugins/layer_toolbox/code.js
+++ www/plugins/layer_toolbox/code.js
@@ -11,7 +11,51 @@ function layer_toolbox_class(options) {
   this.inheritFrom(options);
 
   register_toolbox(this);
+
+  this.show();
 }
+
+layer_toolbox_class.prototype.show=function() {
+  dom_clean(this.content);
+
+  var div=dom_create_append(this.content, "div");
+  div.className="layer_toolbox";
+
+  var h2=dom_create_append(div, "h2");
+  dom_create_append_text(h2, lang("layer_toolbox:head:base_layer"));
+
+  var ul=dom_create_append(div, "ul");
+  for(var i in basemaps) {
+    var li=dom_create_append(ul, "li");
+
+    var input=document.createElement("input");
+    input.type="radio";
+    input.name="layer_toolbox_base_layer";
+    input.value=i;
+    li.appendChild(input);
+
+    dom_create_append_text(li, basemaps[i].name);
+  }
+
+  if(overlays_layers&&(keys(overlays_layers).length)) {
+    var h2=dom_create_append(div, "h2");
+    dom_create_append_text(h2, lang("layer_toolbox:head:overlays"));
+
+    var ul=dom_create_append(div, "ul");
+    for(var i in overlays_layers) {
+      var li=dom_create_append(ul, "li");
+
+      var input=document.createElement("input");
+      input.type="checkbox";
+      input.name="layer_toolbox_overlays_"+i;
+      input.checked=true;
+      li.appendChild(input);
+
+      dom_create_append_text(li, overlays_layers[i].name);
+    }
+  }
+}
+
 function layer_toolbox_init() {
   layer_toolbox=new layer_toolbox_class();
 }
