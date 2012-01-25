928dc5261af45f27f17113f2783bce61b602888f
Stephan Plepelits <skunk@xover.mud.at>
2012-01-25 21:00:16 +0100

    Plugin 'Layer Grid': Rename from 'Measure Grid'

---
+++ www/plugins/layer_grid/conf.php
@@ -0,0 +1,8 @@
+<?
+$layer_grid_active=true;
+
+$layer_grid_depend=array("toolbox");
+
+$layer_grid_tags=new tags(array(
+  "name"=>"Show a grid on degrees",
+));
--- www/plugins/measure_grid/conf.php
+++
@@ -1,8 +0,0 @@
-<?
-$measure_grid_active=true;
-
-$measure_grid_depend=array("toolbox");
-
-$measure_grid_tags=new tags(array(
-  "name"=>"Show a grid on degrees",
-));
