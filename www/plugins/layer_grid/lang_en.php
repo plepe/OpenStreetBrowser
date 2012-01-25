15586ff342cc8fd8270140289af4f0c0f87905d5
Stephan Plepelits <skunk@xover.mud.at>
2012-01-25 21:30:29 +0100

    Plugin 'Layer Grid': Add a 'Tip of the Day'

--- www/plugins/layer_grid/lang_en.php
+++ www/plugins/layer_grid/lang_en.php
@@ -1,3 +1,4 @@
 <?
 $lang_str['layer_grid:name']="Map Grid";
 $lang_str['layer_grid:help']="Show a grid of meridians and latitudes on the map";
+$lang_str['tip_of_the_day:layer_grid']="Activate a map grid for better orientation from the layer toolbox";
