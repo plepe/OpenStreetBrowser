b58bce23ef559c2b1d245ba8ed5995d77b7b4b7d
Stephan Plepelits <skunk@xover.mud.at>
2012-01-29 08:36:01 +0100

    Plugin 'Translation Statistics': Count $lang_str of language

--- www/plugins/translation_statistics/code.php
+++ www/plugins/translation_statistics/code.php
@@ -1,4 +1,19 @@
 <?
+function translation_statistics_load_lang($lang) {
+  global $plugins_list;
+
+  $lang_str=array();
+
+  @include("lang/{$lang}.php");
+  @include("lang/lang_{$lang}.php");
+  @include("lang/tags_{$lang}.php");
+  foreach($plugins_list as $plugin=>$dummy) {
+    @include("plugins/$plugin/lang_{$lang}.php");
+  }
+
+  return $lang_str;
+}
+
 function translation_statistics_lang($lang) {
   global $language_list;
   global $ui_lang;
@@ -6,6 +21,9 @@ function translation_statistics_lang($lang) {
   $ret=array();
   $ret['name']=$language_list[$lang];
   $ret["name:$ui_lang"]=lang("lang:$lang");
+  $lang_str=translation_statistics_load_lang($lang);
+  $ret['base_language']=coalesce($lang_str['base_language'], "en");
+  $ret['lang_str_count']=sizeof($lang_str);
 
   return $ret;
 }
