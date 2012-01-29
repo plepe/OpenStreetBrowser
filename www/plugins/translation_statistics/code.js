af626b7b6a8779a0eeddd26d3fa658deff411696
Stephan Plepelits <skunk@xover.mud.at>
2012-01-29 09:14:37 +0100

    Plugin 'Translation Statistics': Show window with information

---
+++ www/plugins/translation_statistics/code.js
@@ -0,0 +1,56 @@
+function translation_statistics() {
+  this.win=new win({ title: lang("translation_statistics:name") });
+  ajax("translation_statistics", {}, this.load_callback.bind(this));
+
+  this.table=dom_create_append(this.win.content, "table");
+  var tr=dom_create_append(this.table, "tr");
+
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation_statistics:local"));
+
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation_statistics:current"));
+
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation_statistics:lang_code"));
+
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation_statistics:base_lang"));
+
+  var th=dom_create_append(tr, "th");
+  dom_create_append_text(th, lang("translation_statistics:ui"));
+}
+
+translation_statistics.prototype.load_callback=function(data) {
+  data=data.return_value;
+  var max_ui=data.en.lang_str_count;
+
+  for(var i in data) {
+    var tr=dom_create_append(this.table, "tr");
+
+    var th=dom_create_append(tr, "td");
+    dom_create_append_text(th, data[i].name);
+
+    var th=dom_create_append(tr, "td");
+    dom_create_append_text(th, data[i]["name:"+ui_lang]);
+
+    var th=dom_create_append(tr, "td");
+    dom_create_append_text(th, i);
+
+    var th=dom_create_append(tr, "td");
+    dom_create_append_text(th, data[i].base_language);
+
+    var th=dom_create_append(tr, "td");
+    dom_create_append_text(th, sprintf("%d (%.0f%%)", data[i].lang_str_count, data[i].lang_str_count/max_ui*100.0));
+  }
+}
+
+function translation_statistics_win() {
+  new translation_statistics();
+}
+
+function translation_statistics_options(add) {
+  add.push("<a href='javascript:translation_statistics_win()'>"+lang("translation_statistics:name")+"</a>");
+}
+
+register_hook("options_lang", translation_statistics_options);
---
+++ www/plugins/translation_statistics/lang_en.php
@@ -0,0 +1,8 @@
+<?
+$lang_str['translation_statistics:name']="Translation Statistics";
+
+$lang_str['translation_statistics:local']="Local language name";
+$lang_str['translation_statistics:current']="Current language name";
+$lang_str['translation_statistics:lang_code']="Language code";
+$lang_str['translation_statistics:base_lang']="Base language";
+$lang_str['translation_statistics:ui']="User Interface";
