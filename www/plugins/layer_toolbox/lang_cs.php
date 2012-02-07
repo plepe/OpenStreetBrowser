3e80174a7c80f4708b1a22f4dedddd6165eb66a3
Michal Pustějovský <Pusty@openstreetbrowser.org>
2012-02-07 20:14:00 +0100

    Update translation (cs)

--- www/plugins/layer_toolbox/lang_cs.php
+++ www/plugins/layer_toolbox/lang_cs.php
@@ -1,4 +1,4 @@
 <?
 $lang_str["layer_toolbox:name"]="Mapové vrstvy";
 $lang_str["layer_toolbox:head:base_layer"]="Základní vrstva";
-#$lang_str["layer_toolbox:head:overlays"]="Overlays";
+$lang_str["layer_toolbox:head:overlays"]="Překrývací vrstvy";
---
+++ www/plugins/translation_statistics/lang_cs.php
@@ -0,0 +1,11 @@
+<?
+$lang_str["translation_statistics:name"]="Statistiky překladů";
+
+$lang_str["translation_statistics:local"]="Název místního jazyka";
+$lang_str["translation_statistics:current"]="Nynější název jazyka";
+$lang_str["translation_statistics:lang_code"]="Kód jazyka";
+$lang_str["translation_statistics:base_lang"]="Základní jazyk";
+$lang_str["translation_statistics:ui"]="Uživatelské rozhraní";
+$lang_str["translation_statistics:tags"]="Tagy";
+$lang_str["translation_statistics:category"]="Kategorie";
+$lang_str["translation_statistics:total"]="Celkem";
