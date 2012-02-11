4da7a0e7b3055573d4525b1fb58758a3b711c6af
Shu Higashi <higa432@gmail.com>
2012-02-12 02:01:05 +0900

    Update www/plugins/contextmenu/lang_ja.php

--- www/plugins/contextmenu/lang_ja.php
+++ www/plugins/contextmenu/lang_ja.php
@@ -1,6 +1,6 @@
 <?
-#$lang_str["contextmenu:head"]="Context Menu";
-#$lang_str["contextmenu:help"]="Should the context menu appear after click with the left or the right mousebutton?";
+#$lang_str["contextmenu:head"]="コンテキストメニュー";
+#$lang_str["contextmenu:help"]="コンテキストメニューはどちらのマウスボタンをクリックして表示させますか？";
 
-#$lang_str["options:contextmenu_mouse_button:right"]="Right Mouse Button";
-#$lang_str["options:contextmenu_mouse_button:left"]="Left Mouse Button";
+#$lang_str["options:contextmenu_mouse_button:right"]="右マウスボタン";
+#$lang_str["options:contextmenu_mouse_button:left"]="左マウスボタン";
