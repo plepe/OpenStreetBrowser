73723b37d39bd6e4f7f7cc2934d73f8778749c1f
Shu Higashi <higa432@gmail.com>
2012-02-12 02:16:43 +0900

    Update www/plugins/marker/lang_ja.php

--- www/plugins/marker/lang_ja.php
+++ www/plugins/marker/lang_ja.php
@@ -1,5 +1,5 @@
 <?
-#$lang_str["marker:name"]=array("Marker", "Markers");
-#$lang_str["marker:action_remove"]="remove marker";
-#$lang_str["marker:add_marker"]="add marker";
-#$lang_str["marker:place"]="place marker on this object";
+#$lang_str["marker:name"]=array("マーカー", "マーカー");
+#$lang_str["marker:action_remove"]="マーカーを削除";
+#$lang_str["marker:add_marker"]="マーカーを追加";
+#$lang_str["marker:place"]="このオブエクト上にマーカーを置く";
