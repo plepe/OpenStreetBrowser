9bba62123beecc2779b8c45aaa497b2990f1de3e
Shu Higashi <higa432@gmail.com>
2012-02-12 02:36:06 +0900

    Update www/plugins/whats_here/lang_ja.php

--- www/plugins/whats_here/lang_ja.php
+++ www/plugins/whats_here/lang_ja.php
@@ -1,4 +1,4 @@
 <?
-#$lang_str["whats_here:contextmenu"]="What's here?";
-#$lang_str["head:whats_here"]="What's here?";
-#$lang_str["whats_here:name"]="Position: %.5f/%.5f";
+#$lang_str["whats_here:contextmenu"]="ここは何？";
+#$lang_str["head:whats_here"]="ここは何？";
+#$lang_str["whats_here:name"]="位置: %.5f/%.5f";
