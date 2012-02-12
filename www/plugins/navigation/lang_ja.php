cb76526c51fcbaa20fa056d918ac3b6b2960c67d
Shu Higashi <higa432@gmail.com>
2012-02-12 23:19:43 +0900

    Update www/plugins/navigation/lang_ja.php

--- www/plugins/navigation/lang_ja.php
+++ www/plugins/navigation/lang_ja.php
@@ -1,9 +1,9 @@
 <?
 $lang_str["navigation:name"]="ルート";
 
-$lang_str["navigation:set_home"]="ホーム(出発地)をセット";
+$lang_str["navigation:set_home"]="ホーム(出発地)にセット";
 $lang_str["navigation:add_via"]="経由地を追加";
-$lang_str["navigation:set_destination"]="目的地をセット";
+$lang_str["navigation:set_destination"]="目的地にセット";
 
 $lang_str["navigation:toolbox_title"]="経路案内";
 $lang_str["navigation:toolbox_help"]="マップ上でホーム(出発地)と目的地を選んでください!";
