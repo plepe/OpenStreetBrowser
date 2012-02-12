0eb094a92c05f134f2940a5be0ba1f254318da1c
Shu Higashi <higa432@gmail.com>
2012-02-12 18:28:38 +0900

    Update www/plugins/units/lang_ja.php

--- www/plugins/units/lang_ja.php
+++ www/plugins/units/lang_ja.php
@@ -5,9 +5,9 @@
 #$lang_str["units:ha"]="%s ha";
 #$lang_str["units:m2"]="%s m²";
 
-#$lang_str["units:sec"]="%s secs";
-#$lang_str["units:min_sec"]="%s:%02s mins";
-#$lang_str["units:min"]="%s mins";
-#$lang_str["units:hour_min"]="%s:%02s hours";
-#$lang_str["units:hour"]="%s hours";
-#$lang_str["units:day"]="%s days";
+$lang_str["units:sec"]="%s 秒";
+$lang_str["units:min_sec"]="%s:%02s 分";
+$lang_str["units:min"]="%s 分";
+$lang_str["units:hour_min"]="%s:%02s 時間";
+$lang_str["units:hour"]="%s 時間";
+$lang_str["units:day"]="%s 日";
