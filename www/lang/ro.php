99ddb187207dd8c8ec632cc5e55b2589c7e69eda
ciprian <c1pr1an@openstreetbrowser.org>
2012-02-19 21:45:33 +0100

    Update translation (ro)

--- www/lang/ro.php
+++ www/lang/ro.php
@@ -2,9 +2,9 @@
 // In this editor you can translate all strings. In the third column you can compare the strings to another language (set it in the select box on the bottom of the window). Please note that changes will not appear right away, they need to be imported by a developer.
 // Every language string can have a singular and plural variant by separating them by ";", e.g. "Restaurant;Restaurants". The first string is the singular form, the second the plural form.  Optionally you can define the Gender (F, M, N) for the word by prepending one of those characters, e.g. "N;Büro;Büros" (German for "office").
 // When translating a language variant (e.g. 'British English', code 'en-gb') please translate only strings which are different from the base language.
-$lang_str["base_language"]="ro"; // Set the language code for a base language which should be used if a string has not been translated to this language. Usually you want to set it to 'en' (English), but for a language variants and dialects set it to the main language. Some world regions might also consider another base language as more appropriate.
+$lang_str["base_language"]="en"; // Set the language code for a base language which should be used if a string has not been translated to this language. Usually you want to set it to 'en' (English), but for a language variants and dialects set it to the main language. Some world regions might also consider another base language as more appropriate.
 
-$lang_str["lang:current"]="English"; // The name of the current language in the native tongue (e.g. "Deutsch" for German)
+$lang_str["lang:current"]="Romanian"; // The name of the current language in the native tongue (e.g. "Deutsch" for German)
 
 // General
 $lang_str["general_info"]="Informatii generale";
@@ -49,7 +49,7 @@ $lang_str["general_info"]="Informatii generale";
 #$lang_str["head:actions"]=array("Action", "Actions");
 #$lang_str["head:location"]="Location";
 #$lang_str["head:tags"]=array("Tag", "Tags");
-#$lang_str["head:whats_here"]="What's here?";
+$lang_str["head:whats_here"]="Ce se afla aici ?";
 
 #$lang_str["action_browse"]="browse in OSM";
 #$lang_str["action_edit"]="edit in OSM";
