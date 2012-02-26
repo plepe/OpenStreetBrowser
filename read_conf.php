1760502c82b3dc5653cd4076bc1479949e4a8248
Stephan Plepelits <skunk@xover.mud.at>
2012-02-26 10:00:32 +0200

    Clean-Up obsolete code

--- read_conf.php
+++
@@ -1,7 +0,0 @@
-<?
-include "conf.php";
-print "export ROOT_PATH=$root_path\n";
-print "export DB_NAME=$db_name\n";
-print "export DB_USER=$db_user\n";
-print "export DB_PASSWD=$db_passwd\n";
-print "export DB_HOST=$db_host\n";
