8a823bf2a235f769e1719193b9efd1923859b11c
Stephan Plepelits <skunk@xover.mud.at>
2012-02-26 09:36:24 +0200

    CLI: handle relative path

--- www/inc/cli.php
+++ www/inc/cli.php
@@ -1,6 +1,10 @@
 <?
 if(is_array($argv)) {
   // for command line tools (in the bin/-directory) find $root_path
-  $cli_dir=substr($argv[0], 0, strrpos($argv[0], "/bin/"));
+  $path=$argv[0];
+  if(substr($path, 0, 1)!="/")
+    $path=getcwd()."/".$argv[0];
+
+  $cli_dir=substr($path, 0, strrpos($path, "/bin/"));
   $root_path=$cli_dir;
 }
