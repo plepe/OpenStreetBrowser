012104ebc06336d4e4c3ae9cc43a4806065f7f6d
Stephan Plepelits <skunk@xover.mud.at>
2012-02-28 14:53:51 +0100

    Debug: Include debug-level; save in assoc. array

--- www/inc/debug.php
+++ www/inc/debug.php
@@ -1,11 +1,37 @@
 <?
 unset($debug_list);
 
-function debug($text, $category="unknown") {
+define('D_ERROR', 3);
+define('D_WARNING', 2);
+define('D_NOTICE', 1);
+define('D_DEBUG', 0);
+
+$debug_levels=array(
+  D_DEBUG	=>"debug",
+  D_NOTICE	=>"notice",
+  D_WARNING	=>"warning",
+  D_ERROR	=>"error",
+);
+
+$debug_levels_abbr=array(
+  D_DEBUG	=>"D",
+  D_NOTICE	=>"N",
+  D_WARNING	=>"W",
+  D_ERROR	=>"E",
+);
+
+function debug($text, $category="unknown", $level=D_NOTICE) {
   global $debug_list;
 
-  $debug_list[]=array(time(), $category, $text);
-  call_hooks("debug", $text, $category);
+  $debug_entry=array(
+    'time'	=>time(),
+    'category'	=>$category,
+    'text'	=>$text,
+    'level'	=>$level,
+  );
+
+  $debug_list[]=$debug_entry;
+  call_hooks("debug", &$debug_entry);
 }
 
 function debug_write($xml) {
