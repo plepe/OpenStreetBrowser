959f9424a83353eb981b4579565499393811bcab
Stephan Plepelits <skunk@xover.mud.at>
2012-03-22 06:29:53 +0100

    Plugin 'Osmosis': schema osmosis also for web ; don't use $db_osmosis

--- www/plugins/osmosis/mcp.php
+++ www/plugins/osmosis/mcp.php
@@ -1,20 +1,13 @@
 <?
 function osmosis_sql_schema_path($conn) {
-  global $db_osmosis;
-  global $osm_schema;
+  global $osmosis_schema;
 
-  if(!isset($db_osmosis)) {
-    debug("\$db_osmosis not defined in conf.php", "osmosis");
-    exit;
-  }
+  if(!$osmosis_schema)
+    $osmosis_schema="osmosis";
 
-  sql_create_schema($conn, $db_osmosis['user'], "authorization {$db_osmosis['user']}");
-  sql_query("grant all on all tables in schema public to {$db_osmosis['user']}");
-  if(!$osm_schema)
-    $osm_schema="osm_schema";
-  sql_query("grant all on all tables in schema $osm_schema to {$db_osmosis['user']}");
+  sql_create_schema($conn, $conn['user']);
 
-  sql_register_schema($conn, "osmosis", $db_osmosis['user'], 0);
+  sql_register_schema($conn, "osmosis", $osmosis_schema, 0);
 }
 
 register_hook("sql_schema_path", "osmosis_sql_schema_path");
