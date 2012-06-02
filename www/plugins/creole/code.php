7bf9854e7fbb48e5e1ac875fcfdadc14375b6044
Stephan Plepelits <skunk@xover.mud.at>
2012-06-02 17:15:46 +0200

    Plugin 'Creole': Expect simplewiki in lib/ ; create debug message when missing

--- www/plugins/creole/code.php
+++ www/plugins/creole/code.php
@@ -7,14 +7,22 @@ function creole_init() {
   if($creole_engine)
     return;
 
-  include "simplewiki/muster_simplewiki_docnode.php";
-  include "simplewiki/simplewiki_docnode.php";
-  include "simplewiki/muster_simplewiki_parser.php";
-  include "simplewiki/simplewiki_parser.php";
-  include "simplewiki/muster_simplewiki_emitter.php";
-  include "simplewiki/simplewiki_emitter.php";
-  include "simplewiki/muster_simplewiki.php";
-  include "simplewiki/simplewiki.php";
+  global $root_path;
+
+  if((!file_exists("$root_path/www/lib/simplewiki/"))||
+     (!file_exists("$root_path/www/lib/simplewiki/simplewiki.php"))) {
+    debug("Library 'Simplewiki' not found! Please install.", "creole", D_ERROR);
+    return;
+  }
+
+  include "$root_path/www/lib/simplewiki/muster_simplewiki_docnode.php";
+  include "$root_path/www/lib/simplewiki/simplewiki_docnode.php";
+  include "$root_path/www/lib/simplewiki/muster_simplewiki_parser.php";
+  include "$root_path/www/lib/simplewiki/simplewiki_parser.php";
+  include "$root_path/www/lib/simplewiki/muster_simplewiki_emitter.php";
+  include "$root_path/www/lib/simplewiki/simplewiki_emitter.php";
+  include "$root_path/www/lib/simplewiki/muster_simplewiki.php";
+  include "$root_path/www/lib/simplewiki/simplewiki.php";
 
   $creole_engine=new SimpleWiki();
 }
@@ -24,6 +32,8 @@ function creole_html($text) {
 
   if(!$creole_engine)
     creole_init();
+  if(!$creole_engine)
+    return $text;
   
   return $creole_engine->get_html($text);
 }
