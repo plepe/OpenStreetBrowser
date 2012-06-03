6d9fcda1f3f6675bc092edb4ebad4c83f54507f0
Stephan Plepelits <skunk@xover.mud.at>
2012-06-03 08:26:03 +0200

    DOM (php): Get DOMDocument from DOMNode

--- www/inc/dom.php
+++ www/inc/dom.php
@@ -1,13 +1,15 @@
 <?
-function dom_create_append($parent, $type, $xml) {
-  if(!$xml) {
-    print "dom_create_append: No root xml defined\n";
-    return;
-  }
-
+function dom_create_append($parent, $type, $xml=null) {
   if(!$parent) {
     print "dom_create_append: No parent defined\n";
     return;
+  }
+
+  if($xml==null) {
+    if(get_class($parent)=="DOMDocument")
+      $xml=$parent;
+    else
+      $xml=$parent->ownerDocument;
   }
 
   $x=$xml->createElement($type);
@@ -15,15 +17,17 @@ function dom_create_append($parent, $type, $xml) {
   return $x;
 }
 
-function dom_create_append_text($parent, $text, $xml) {
-  if(!$xml) {
-    print "dom_create_append_text: No root xml defined\n";
-    return;
-  }
-
+function dom_create_append_text($parent, $text, $xml=null) {
   if(!$parent) {
     print "dom_create_append_text: No parent defined\n";
     return;
+  }
+
+  if($xml==null) {
+    if(get_class($parent)=="DOMDocument")
+      $xml=$parent;
+    else
+      $xml=$parent->ownerDocument;
   }
 
   $x=$xml->createTextNode($text);
@@ -32,14 +36,16 @@ function dom_create_append_text($parent, $text, $xml) {
 }
 
 function dom_clean($parent) {
-  if(!$xml) {
-    print "dom_clean: No root xml defined\n";
-    return;
-  }
-
   if(!$parent) {
     print "dom_clean: No parent defined\n";
     return;
+  }
+
+  if($xml==null) {
+    if(get_class($parent)=="DOMDocument")
+      $xml=$parent;
+    else
+      $xml=$parent->ownerDocument;
   }
 
   while($parent->firstChild)
