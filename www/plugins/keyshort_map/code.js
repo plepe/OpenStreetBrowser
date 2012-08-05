6c051a48d681577e4ac03d6816caf5c74730c8fb
Stephan Plepelits <skunk@xover.mud.at>
2012-08-05 17:36:03 +0200

    Plugin 'Keyshort Map': Rewrote to use new register_keyshort()

--- www/plugins/keyshort_map/code.js
+++ www/plugins/keyshort_map/code.js
@@ -1,53 +1,57 @@
-function keyshort_map_keydown(done, ev) {
-  var action=false;
+function keyshort_map(id) {
+  var x=0, y=0;
+  var multi=10;
+  var m;
 
-  // another hook reacted on the keyboard shortcut
-  if(done.length)
-    return;
-
-  var x=0;
-  var y=0;
-
-  // check if we have an action defined for this event
-  switch(ev.keyCode) {
-    case 33: // PgUp
-      map.zoomIn();
-      action=true;
-      break;
-    case 34: // PgDn
-      map.zoomOut();
-      action=true;
-      break;
-    case 37: // Left
-      x=-1;
-      break;   
-    case 38: // Up
-      y=-1;
-      break;
-    case 39: // Right
-      x=1;
-      break;
-    case 40: // Down
-      y=1;
-      break;
+  if(m=id.match(/^(keyshort_map:[^:]*):fast/)) {
+    id=m[1];
+    multi=25;
+  }
+  else if(m=id.match(/^(keyshort_map:[^:]*):slow/)) {
+    id=m[1];
+    multi=5;
   }
 
-  // check special keys
-  var multi=10;
-  if(ev.ctrlKey)
-    multi=25;
-  if(ev.shiftKey)
-    multi=5;
+  switch(id) {
+    case "keyshort_map:zoom_in":
+      map.zoomIn();
+      break;
+    case "keyshort_map:zoom_out":
+      map.zoomOut();
+      break;
+    case "keyshort_map:scroll_north":
+      y=-1;
+      break;
+    case "keyshort_map:scroll_east":
+      x=+1;
+      break;
+    case "keyshort_map:scroll_south":
+      y=+1;
+      break;
+    case "keyshort_map:scroll_west":
+      x=-1;
+      break;
+  }
 
   // move map
   if((x!=0)||(y!=0)) {
     map.moveByPx(x*multi, y*multi);
-    action=true;
   }
 
-  // tell following hooks that an action has already been taken
-  if(action)
-    done.push("keyshort_map");
+  return false;
 }
 
-register_hook("keyshort_keydown", keyshort_map_keydown);
+register_keyshort("keyshort_map:zoom_in", keyshort_map, "pageup");
+register_keyshort("keyshort_map:zoom_out", keyshort_map, "pagedown");
+register_keyshort("keyshort_map:scroll_north", keyshort_map, "up");
+register_keyshort("keyshort_map:scroll_east", keyshort_map, "right");
+register_keyshort("keyshort_map:scroll_south", keyshort_map, "down");
+register_keyshort("keyshort_map:scroll_west", keyshort_map, "left");
+register_keyshort("keyshort_map:scroll_north:fast", keyshort_map, "ctrl_up");
+register_keyshort("keyshort_map:scroll_east:fast", keyshort_map, "ctrl_right");
+register_keyshort("keyshort_map:scroll_south:fast", keyshort_map, "ctrl_down");
+register_keyshort("keyshort_map:scroll_west:fast", keyshort_map, "ctrl_left");
+register_keyshort("keyshort_map:scroll_north:slow", keyshort_map, "shift_up");
+register_keyshort("keyshort_map:scroll_east:slow", keyshort_map, "shift_right");
+register_keyshort("keyshort_map:scroll_south:slow", keyshort_map, "shift_down");
+register_keyshort("keyshort_map:scroll_west:slow", keyshort_map, "shift_left");
