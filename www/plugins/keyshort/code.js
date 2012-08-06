f3c9828757bb8892e26d94435246679074f9e06e
Stephan Plepelits <skunk@xover.mud.at>
2012-08-06 07:31:20 +0200

    Plugin 'Keyshort': Specify name of shortcut; list of shortcuts in Options

--- www/plugins/keyshort/code.js
+++ www/plugins/keyshort/code.js
@@ -9,7 +9,7 @@ function keyshort_call(id) {
   keyshort_list[id].callback(id);
 }
 
-function register_keyshort(id, callback, default_keyshort, keydir) {
+function register_keyshort(id, callback, default_keyshort, name, keydir) {
   var options={};
 
   if(typeof id=="object")
@@ -19,14 +19,37 @@ function register_keyshort(id, callback, default_keyshort, keydir) {
       id: id,
       callback: callback,
       default_keyshort: default_keyshort,
+      name: name,
       keydir: keydir
     };
 
   if(!options.keydir)
     options.keydir="keydown";
+  if(!options.name)
+    options.name=options.id;
 
   keyshort_list[options.id]=options;
 
   $(document).bind(options.keydir+"."+options.default_keyshort,
     keyshort_call.bind(this, options.id));
 }
+
+function keyshort_options_show(list) {
+  var ret="";
+
+  ret+="<h4>"+lang("keyshort")+"</h4>\n";
+  ret+="<div class='options_help'>"+lang("keyshort:help")+"</div>\n";
+  ret+="<table>\n";
+  for(var k in keyshort_list) {
+    ret+="  <tr>\n";
+    ret+="    <td>"+keyshort_list[k].default_keyshort+"</td>\n";
+    ret+="    <td>"+keyshort_list[k].name+"</td>\n";
+    ret+="  </tr>\n";
+  }
+  ret+="</table>\n";
+
+  list.push([ 2, ret ]);
+}
+
+register_hook("options_show", keyshort_options_show);
+//register_hook("options_save", keyshort_options_save);
---
+++ www/plugins/keyshort/lang_en.php
@@ -0,0 +1,5 @@
+<?
+$lang_str['keyshort']="Keyboard Shortcuts";
+$lang_str['keyshort:help']="This is a list of available keyboard shortcuts. In a future version this list may be editable.";
+
+$lang_str['tip_of_the_day:keyshort:0']="Control OpenStreetBrowser via keyboard! See the Options for a list of available shortcuts.";
