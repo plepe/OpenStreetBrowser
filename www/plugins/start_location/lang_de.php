004c27f78196bfee2ed4104c5c277bbe0cf8912d
Stephan Plepelits <skunk@xover.mud.at>
2012-08-12 07:37:28 +0200

    Plugin 'Start Location': rename 'keep view' to 'default view'

--- www/plugins/start_location/lang_de.php
+++ www/plugins/start_location/lang_de.php
@@ -1,9 +1,10 @@
 <?
 $lang_str["start_location:name"]="Kartenposition";
+$lang_str["start_location:options_help"]="Auf welcher Position soll die Karte beim Laden zentriert werden?";
 
 $lang_str["start_location:choose"]="Wähle einen Kartenausschnitt";
 $lang_str["start_location:geolocation"]="automatische Geolokalisierung";
 $lang_str["start_location:lastview"]="letzter Kartenausschnitt";
 $lang_str["start_location:savedview"]="letzter Permalink";
-$lang_str["start_location:startnormal"]="behalte Ansicht";
+$lang_str["start_location:startnormal"]="Standardansicht";
 $lang_str["start_location:remember"]="Auswahl merken";
--- www/plugins/start_location/lang_en.php
+++ www/plugins/start_location/lang_en.php
@@ -6,5 +6,5 @@ $lang_str["start_location:choose"]="Choose map view";
 $lang_str["start_location:geolocation"]="get geolocation";
 $lang_str["start_location:lastview"]="last view";
 $lang_str["start_location:savedview"]="last permalink";
-$lang_str["start_location:startnormal"]="keep view";
+$lang_str["start_location:startnormal"]="default view";
 $lang_str["start_location:remember"]="remember decision";
