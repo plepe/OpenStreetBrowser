4aeabd5b6c47ef947a8192d61b16e28e91cafefb
Michal Pustějovský <Pusty@openstreetbrowser.org>
2012-09-07 11:02:46 +0200

    Update translation (cs)

---
+++ www/plugins/keyshort_map/lang_cs.php
@@ -0,0 +1,5 @@
+<?
+$lang_str["tip_of_the_day:keyshort_map:0"]="K pohybu na mapě můžete použít šipky, Page Up, Page Down. Ke změně rychlsoti pohybu použijte Ctrl nebo Shift";
+
+$lang_str["keyshort_map:fast"]="(rychle)";
+$lang_str["keyshort_map:slow"]="(pomalu)";
--- www/plugins/units/lang_cs.php
+++ www/plugins/units/lang_cs.php
@@ -11,3 +11,23 @@ $lang_str["units:min"]="%s minut";
 $lang_str["units:hour_min"]="%s:%02s hodin";
 $lang_str["units:hour"]="%s hodin";
 $lang_str["units:day"]="%s dnů";
+
+$lang_str["units:speed:km/h"]="%s km/h";
+
+$lang_str["units:altitude:m"]="%s m";
+
+#$lang_str["units:heading:deg-windrose"]="%2\$s (%1\$s°)"; // first value is degree as number, second is windrose value like 'NE'. "%1\$s" and "%\2$s" denote first and second parameter, see http://php.net/manual/en/function.sprintf.php
+
+$lang_str["units:heading:windrose:0"]="S";
+$lang_str["units:heading:windrose:1"]="SV";
+$lang_str["units:heading:windrose:2"]="V";
+$lang_str["units:heading:windrose:3"]="JV";
+$lang_str["units:heading:windrose:4"]="J";
+$lang_str["units:heading:windrose:5"]="JZ";
+$lang_str["units:heading:windrose:6"]="Z";
+$lang_str["units:heading:windrose:7"]="SZ";
+
+$lang_str["units:latitude:N"]="S"; // north (of equator)
+$lang_str["units:latitude:S"]="J"; // south (of equator)
+$lang_str["units:longitude:E"]="V"; // east (of greenwich)
+$lang_str["units:longitude:W"]="Z"; // west (of greenwich)
