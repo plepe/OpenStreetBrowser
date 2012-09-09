3d9f4456d6c8c4007c55f1d7931382727aede758
Aivo <Aivo@openstreetbrowser.org>
2012-09-09 15:42:43 +0200

    Update translation (et)

---
+++ www/lang/lang_et.php
@@ -0,0 +1,23 @@
+<?
+$lang_str["lang:de"]="saksa";
+$lang_str["lang:bg"]="bulgaaria";
+$lang_str["lang:cs"]="tšehhi";
+$lang_str["lang:en"]="inglise";
+$lang_str["lang:es"]="hispaania";
+$lang_str["lang:et"]="eesti";
+$lang_str["lang:it"]="itaalia";
+$lang_str["lang:fr"]="prantsuse";
+$lang_str["lang:uk"]="ukraina";
+$lang_str["lang:ru"]="vene";
+$lang_str["lang:ja"]="jaapani";
+$lang_str["lang:hu"]="ungari";
+$lang_str["lang:nl"]="hollandi";
+$lang_str["lang:ast"]="astuuria";
+$lang_str["lang:el"]="kreeka";
+$lang_str["lang:pl"]="poola";
+$lang_str["lang:ca"]="katalaani";
+$lang_str["lang:sr"]="serbia";
+$lang_str["lang:ro"]="rumeenia";
+$lang_str["lang:da"]="taani";
+$lang_str["lang:pt"]="portugali";
+$lang_str["lang:pt-br"]="portugali (Brasiilia)";
