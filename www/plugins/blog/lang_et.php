d8b434ba8df98bef19a6318ed459ac4bd701ebcc
Aivo <Aivo@openstreetbrowser.org>
2012-09-09 18:33:39 +0200

    Update translation (et)

---
+++ www/plugins/blog/lang_et.php
@@ -0,0 +1,4 @@
+<?
+$lang_str["blog:name"]="Blogi";
+$lang_str["blog:header"]="Mis on uudist?";
+$lang_str["blog:visit"]="Külasta blogi";
---
+++ www/plugins/marker/lang_et.php
@@ -0,0 +1,5 @@
+<?
+$lang_str["marker:name"]=array("Marker", "Markerid");
+$lang_str["marker:action_remove"]="eemalda marker";
+$lang_str["marker:add_marker"]="lisa marker";
+$lang_str["marker:place"]="aseta marker sellele objektile";
