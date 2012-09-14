6954f327a35123d51cb866c6fd5bead67969a16e
Aivo <Aivo@openstreetbrowser.org>
2012-09-14 17:00:37 +0200

    Update translation (et)

---
+++ www/plugins/talk/lang_et.php
@@ -0,0 +1,3 @@
+<?
+#$lang_str["talk:name"]="Talk Page";
+$lang_str["talk:default"]="Sellel arutelu lehel teksti ei ole. Sa võid kasutada seda lehte, et  välja pakkuda ja arutleda soovituste üle või jätta oma kommentaare.";
