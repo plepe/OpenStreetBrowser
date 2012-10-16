0b767dc849f40c58fd5c1ccc502a633c4d5e5897
Stephan Plepelits <skunk@xover.mud.at>
2012-10-16 14:47:45 +0200

    Modulekit: Remove deprecated 'plugins' code

--- inc/plugins.js
+++
@@ -1,8 +0,0 @@
-/**
- * plugins_loaded - Checks if a plugin is loaded
- * @param string ID of plugin
- * @return boolean true if plugin has been loaded
- */
-function plugins_loaded(plugin) {
-  return in_array(plugin, plugins);
-}
