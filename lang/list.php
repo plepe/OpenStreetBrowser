fb4e40ff14b98b18cfc3ebd63fdf25fa84c23799
Stephan Plepelits <skunk@xover.mud.at>
2012-10-19 07:47:09 +0200

    Modulekit: Move list of available translations to main modulekit.php

--- lang/list.php
+++
@@ -1,4 +0,0 @@
-<?
-// The UI has been translated to following languages
-$languages=array("en", "de", "it", "ja", "uk", "fr", "ru", "es", "cs", "hu", "nl", "ast", "el", "pl", "ca", "sr", "ro", "da", "pt-br", "et");
-
--- modulekit.php
+++ modulekit.php
@@ -56,3 +56,6 @@ $include=array(
     "inc/*.css",
   ),
 );
+
+// The UI has been translated to following languages
+$languages=array("en", "de", "it", "ja", "uk", "fr", "ru", "es", "cs", "hu", "nl", "ast", "el", "pl", "ca", "sr", "ro", "da", "pt-br", "et");
