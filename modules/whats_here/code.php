38b1d8e821c9e050a385a1979c74352ef1dd9e8b
Stephan Plepelits <skunk@xover.mud.at>
2012-11-18 21:39:51 +0100

    What's here: adapt to new db layout

--- modules/whats_here/code.php
+++ modules/whats_here/code.php
@@ -3,38 +3,41 @@
 function whats_here_find($param) {
   global $load_xml;
 //  global $query;
+  $srid=900913;
+  if(isset($param['srid']))
+    $srid=$param['srid'];
 
   $dist_mul=(19-$param[zoom])*(19-$param[zoom]);
   $dist=3*$dist_mul;
 
-  $poly="PolyFromText('POLYGON((".
+  $poly="ST_Transform(PolyFromText('POLYGON((".
     ($param[lon]-$dist)." ".($param[lat]-$dist).",".
     ($param[lon]-$dist)." ".($param[lat]+$dist).",".
     ($param[lon]+$dist)." ".($param[lat]+$dist).",".
     ($param[lon]+$dist)." ".($param[lat]-$dist).",".
-    ($param[lon]-$dist)." ".($param[lat]-$dist)."))', 900913)";
-  $distance="Distance(osm_way, GeometryFromText('POINT($param[lon] $param[lat])', 900913))";
+    ($param[lon]-$dist)." ".($param[lat]-$dist)."))', $srid), 4326)";
+  $distance="ST_Distance(way, ST_Transform(GeometryFromText('POINT($param[lon] $param[lat])', $srid), 4326))";
 
-  $qry="select *, astext(ST_Centroid(osm_way)) as \"#geo:center\" from (".
+  $qry="select *, astext(ST_Centroid(way)) as \"#geo:center\" from (".
     "select *, $distance-\"#dist_modi\"*$dist_mul as \"#distance\" from (".
-    "select osm_id, osm_tags, osm_way, 1 as \"#area\", 4 as \"#dist_modi\" from osm_point where osm_way&&$poly".
+    "select id, tags, way, 1 as \"#area\", 4 as \"#dist_modi\" from osm_point($poly)".
     " union all ".
-    "select osm_id, osm_tags, osm_way, ST_Length(osm_way) as \"#area\", 1.5 as \"#dist_modi\" from osm_line where osm_way&&$poly".
+    "select id, tags, way, ST_Length(way) as \"#area\", 1.5 as \"#dist_modi\" from osm_line($poly)".
     " union all ".
-    "select osm_id, osm_tags, osm_way, ST_Area(osm_way) as \"#area\", 1 as \"#dist_modi\" from osm_polygon where osm_way&&$poly".
+    "select id, tags, way, ST_Area(way) as \"#area\", 1 as \"#dist_modi\" from osm_polygon($poly)".
     " union all ".
-    "select osm_id, osm_tags, osm_way, ST_Area(osm_way) as \"#area\", 1 as \"#dist_modi\" from osm_rel where osm_way&&$poly".
+    "select id, tags, way, ST_Area(way) as \"#area\", 1 as \"#dist_modi\" from osm_rel($poly)".
     ") x1 offset 0) x2 where \"#distance\"<$dist order by \"#distance\" asc, \"#area\" asc";
 
   $res=sql_query($qry);
   while($elem=pg_fetch_assoc($res)) {
-    $osm_tags=parse_hstore($elem['osm_tags']);
+    $osm_tags=parse_hstore($elem['tags']);
     foreach($elem as $k=>$v) {
       if(substr($k, 0, 1)=="#")
         $osm_tags[$k]=$v;
     }
 
-    $ret[]=array("id"=>$elem['osm_id'], "tags"=>new tags($osm_tags));
+    $ret[]=array("id"=>$elem['id'], "tags"=>new tags($osm_tags));
   }
 
   return $ret;
