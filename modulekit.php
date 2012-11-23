a913a915b17635062a02494422a43f3bb271462a
Stephan Plepelits <skunk@xover.mud.at>
2012-11-23 08:04:20 +0100

    Fix dependencies (parse_number)

--- modulekit.php
+++ modulekit.php
@@ -9,6 +9,7 @@ $depend=array("hooks", "lang", "pg_sql",
   "category",		// previously from inc/category.php
   "load_object",	// previously from inc/object.php
   "pg_array",		// depend inc/object.php
+  "parse_number",	// depend inc/number.php
 );
 
 // include these files from modules:
