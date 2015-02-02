0eaacfa2c1ae6b8f6b2b27947747eec59e3d1e41
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-02-02 23:58:41 +0100

    layer ol4pgm: register as category

--- modules/layer_ol4pgm/code.js
+++ modules/layer_ol4pgm/code.js
@@ -1,7 +1,16 @@
-register_hook("init", function() {
-  new ol4pgmLayer({
-    url: "osb.py?x={x}&y={y}&z={z}&format=geojson-separate&tilesize=1024&srs=3857",
+function layer_ol4pgm_category(id) {
+  this.inheritFrom=category;
+  this.inheritFrom(id);
+
+  this.ol4pgm = new ol4pgmLayer({
+    url: id + ".py?x={x}&y={y}&z={z}&format=geojson-separate&tilesize=1024&srs=3857",
     maxZoom: 17,
     tileSize: 1024
   }, map);
+
+  this.tags.set("name", "OSB");
+}
+
+register_hook("init", function() {
+  category_root.register_sub_category(new layer_ol4pgm_category("osb"));
 });
