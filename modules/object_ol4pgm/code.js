37949fe85697d992c9256efd38cabafb04f7627f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-03-17 14:25:56 +0100

    Object ol4pgm: set id; href; type (from property 'type')

--- modules/object_ol4pgm/code.js
+++ modules/object_ol4pgm/code.js
@@ -1,12 +1,15 @@
 function object_ol4pgm(feature, category) {
   this.inheritFrom=geo_object;
   this.inheritFrom();
-  this.type="object_ol4pgm";
 
   this.feature = feature;
   this.category = category;
 
   this.name = this.feature.getProperties().results[0]['text'] || lang("unnamed");
+  this.type = this.feature.getProperties().results[0]['type'];
+  this.id = this.feature.getProperties()['osm:id'];
+  this.href = url({ obj: category.id + "/" + this.id });
+
   this.highlight = new ol.format.WKT().writeFeature(this.feature);
   this.highlight_center=new ol.format.WKT().writeGeometry(new ol.geom.Point(ol.extent.getCenter(this.feature.getGeometry().getExtent())));
 
