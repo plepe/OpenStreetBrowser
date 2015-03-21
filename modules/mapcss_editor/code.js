dd84c54e01e90a51fd177f70fd0c38f7a87f9046
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-03-21 06:34:27 +0100

    MapCSS Editor: load existing category

--- modules/mapcss_editor/code.js
+++ modules/mapcss_editor/code.js
@@ -1,5 +1,4 @@
 function mapcss_editor(id) {
-  var form_def = {};
   var title;
 
   this.id = id;
@@ -9,11 +8,6 @@ function mapcss_editor(id) {
   }
   else {
     title = "New category";
-    form_def['id'] = {
-      'type': 'text',
-      'name': "ID",
-      'req': true
-    };
   }
 
   this.win = new win({
@@ -21,13 +15,40 @@ function mapcss_editor(id) {
     'class': 'mapcss_editor'
   });
 
+  if(this.id) {
+    this.win.content.innerHTML = "Loading ...";
+    ajax("mapcss_editor_load", { id: id }, null, function(data) {
+      data = data.return_value;
+      this.open(data);
+    }.bind(this));
+  }
+  else {
+    this.open(null);
+  }
+}
+
+mapcss_editor.prototype.open = function(data) {
+  var form_def = {};
+
+  if(!this.id) {
+    form_def['id'] = {
+      'type': 'text',
+      'name': "ID",
+      'req': true
+    };
+  }
+
   form_def['content'] = {
     'type': 'textarea',
     'name': "MapCSS code",
     'req': true
   };
 
+  dom_clean(this.win.content);
+
   this.form = new form('data', form_def);
+  if(data)
+    this.form.set_data(data);
   this.form.show(this.win.content);
 
   var div = document.createElement("div");
--- modules/mapcss_editor/code.php
+++ modules/mapcss_editor/code.php
@@ -3,6 +3,21 @@ register_hook("main_links", function(&$list) {
   $list[] = array(5, "<a href='javascript:mapcss_editor_open(\"foo\")'>MapCSS</a>");
 });
 
+function ajax_mapcss_editor_load($param, $document, $data) {
+  global $data_path;
+
+  $id = $param['id'];
+
+  if(!preg_match("/^[a-zA-Z0-9_]+$/", $id, $m))
+    return array("error" => "Illegal ID");
+
+  $ret = array();
+  $ret['id'] = $id;
+  $ret['content'] = file_get_contents("{$data_path}/categories/{$id}.mapcss");
+
+  return $ret;
+}
+
 function ajax_mapcss_editor_save($param, $document, $data) {
   global $data_path;
 
