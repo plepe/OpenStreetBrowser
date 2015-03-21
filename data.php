e7fa72d249095ca8e55b5e38a9c1ff038d1a4eca
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-03-21 13:12:27 +0100

    data.php: cache data

--- data.php
+++ data.php
@@ -12,28 +12,50 @@ if(!preg_match("/^[a-zA-Z0-9_]+$/", $_REQUEST['id'], $m)) {
   exit;
 }
 
-$script = "{$data_path}/categories/{$_REQUEST['id']}.py";
+$cache_path = "{$data_path}/cache/{$_REQUEST['id']}/{$_REQUEST['z']}/{$_REQUEST['x']}/{$_REQUEST['y']}.json";
 
-$descriptorspec = array(
-   0 => array("pipe", "r"),
-   1 => array("pipe", "w"),
-   2 => array("pipe", "w")
-);
+// TODO: invalidate cache
+if(file_exists($cache_path)) {
+  $fp = fopen($cache_path, "r");
+  $cache_fp = null;
+}
+else {
+  mkdir(dirname($cache_path), 0777, true);
+  $cache_fp = fopen($cache_path, "w");
 
-// execute external script as CGI
-$process = proc_open($script, $descriptorspec, $pipes, getcwd(), $_SERVER);
+  $script = "{$data_path}/categories/{$_REQUEST['id']}.py";
+
+  $descriptorspec = array(
+     0 => array("pipe", "r"),
+     1 => array("pipe", "w"),
+     2 => array("pipe", "w")
+  );
+
+  // execute external script as CGI
+  $process = proc_open($script, $descriptorspec, $pipes, getcwd(), $_SERVER);
+  $fp = $pipes[1];
+}
 
 // first read and set headers
-while($r = chop(fgets($pipes[1]))) {
-  if($r == "")
-    break;
-
+while($r = trim(fgets($fp))) {
   Header($r);
+
+  if($cache_fp)
+    fwrite($cache_fp, "$r\n");
 }
+
+if($cache_fp)
+  fwrite($cache_fp, "\n");
 
 // now print the body
-while($r = fread($pipes[1], 1024*1024)) {
+while($r = fread($fp, 1024*1024)) {
   print $r;
+
+  if($cache_fp)
+    fwrite($cache_fp, $r);
 }
 
-proc_close($process);
+if(isset($process))
+  proc_close($process);
+if($cache_fp)
+  fclose($cache_fp);
