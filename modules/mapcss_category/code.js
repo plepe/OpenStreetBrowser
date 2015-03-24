24ff3639a360a6c082b3a9d3288d2158fff5996b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-03-24 22:18:44 +0100

    MapCSS Category: data(), edit(), template for save()

--- modules/mapcss_category/code.js
+++ modules/mapcss_category/code.js
@@ -19,14 +19,58 @@ function mapcss_Category(repo, id, branch) {
   this.repo = get_category_repository(repo, branch);
   this.id = id;
 
-  ajax_json("mapcss_category_load", {
-    repo: this.repo.id,
-    id: this.id,
-    branch: this.repo.branch
-  }, this.load.bind(this));
+  this.data_callbacks = [];
+
+  this.load();
 }
 
 mapcss_Category.prototype.load = function(data) {
-  this._data = data;
-  alert(JSON.stringify(this._data));
+  new ajax_json("mapcss_category_load", { repo: this.repo.id, id: this.id, branch: this.repo.branch }, function(data) {
+    this._data = data;
+
+    for(var i = 0; i < this.data_callbacks.length; i++) {
+      this.data_callbacks[i](this._data);
+    }
+    this.data_callbacks = [];
+  }.bind(this));
+}
+
+mapcss_Category.prototype.data = function(callback, force) {
+  if(this._data) {
+    if(force) {
+      this.load();
+    }
+    else {
+      callback(this._data);
+      return;
+    }
+  }
+
+  this.data_callbacks.push(callback);
+}
+
+mapcss_Category.prototype.edit = function() {
+  var form_def = {
+    'content': {
+      'type': 'textarea',
+      'name': "MapCSS code",
+      'req': true
+    },
+    'commit_msg': {
+      'type': 'text',
+      'name': "Commit message"
+    }
+  };
+
+  this.editor = new editor({
+    form_def: form_def,
+    title: "Edit category '" + this.id + "'",
+    onsave: this.save.bind(this)
+  });
+
+  this.data(this.editor.set_data.bind(this.editor));
+}
+
+mapcss_Category.prototype.save = function(data) {
+  alert(JSON.stringify(data));
 }
