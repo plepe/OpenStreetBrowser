a49d67e0f6ac4647fe339a6d194b8d5ef546d686
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-05-04 07:06:23 +0200

    OSM member: adapt to changes in object::members() ::member_of()

--- modules/osm_member/code.php
+++ modules/osm_member/code.php
@@ -4,7 +4,16 @@ function osm_member_info(&$chapters, $ob) {
 
   if($members) {
     $content=array();
-    foreach($members as $member_id=>$role) {
+
+    // load all objects, so they exist in cache
+    load_objects(array_map(function($x) {
+      return $x['id'];
+    }, $members));
+
+    foreach($members as $m) {
+      $member_id = $m['id'];
+      $role = array_key_exists('role', $m) ? $m['role'] : null;
+
       $member=load_object($member_id);
       if($member) {
 	$member->tags->set("#role", $role);
@@ -22,9 +31,17 @@ function osm_member_info(&$chapters, $ob) {
 
   $member_of=$ob->member_of();
 
+  // load all objects, so they exist in cache
+  load_objects(array_map(function($x) {
+    return $x['id'];
+  }, $member_of));
+
   if(sizeof($member_of)) {
     $content=array();
-    foreach($member_of as $of_id=>$role) {
+    foreach($member_of as $m) {
+      $of_id = $of['id'];
+      $role = $of['role'];
+
       $of=load_object($of_id);
       if($of) {
 	$of->tags->set("#role", $role);
