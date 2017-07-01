0dcefc55b386fe84a907ba62a503e10ed170c80a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-07-01 07:12:44 +0200

    Change order of including JS files

--- index.php
+++ index.php
@@ -18,12 +18,12 @@ html_export_var(array(
   <link rel="stylesheet" href="node_modules/leaflet-geosearch/assets/css/leaflet.css" />
   <link rel="stylesheet" href="node_modules/leaflet.locatecontrol/dist/L.Control.Locate.min.css" />
   <script src="node_modules/leaflet/dist/leaflet.js"></script>
-  <script src="dist/openstreetbrowser.js"></script>
   <script src="node_modules/leaflet.locatecontrol/dist/L.Control.Locate.min.js"></script>
   <?php print modulekit_to_javascript(); /* pass modulekit configuration to JavaScript */ ?>
   <?php print modulekit_include_js(); /* prints all js-includes */ ?>
   <?php print modulekit_include_css(); /* prints all css-includes */ ?>
   <?php print_add_html_headers(); /* print additional html headers */ ?>
+  <script src="dist/openstreetbrowser.js"></script>
 </head>
 <body>
   <div id='map'></div>
