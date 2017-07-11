35d2c9e1a7c11c06be991fb45fa0edab76301b11
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-07-11 10:48:39 +0200

    Session handling

--- ajax.php
+++ ajax.php
@@ -6,6 +6,7 @@
 ?>
 <?php include "conf.php"; /* load a local configuration */ ?>
 <?php include "modulekit/loader.php"; /* loads all php-includes */ ?>
+<?php session_start(); ?>
 <?php
 call_hooks("ajax_start");
 
