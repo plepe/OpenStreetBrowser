9e85d2df316a642c18104ed3696b9975a216b696
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-07-11 08:58:36 +0200

    Language: list of available languages; reload after language change

--- src/language.js
+++ src/language.js
@@ -6,8 +6,29 @@ register_hook('init_callback', function (callback) {
 })
 
 register_hook('options_form', function (def) {
+  var languages = {}
+  for (var k in lang_str) {
+    var m
+    if (m = k.match(/^lang:(.*)$/)) {
+      if (m[1] === 'current') {
+        continue
+      }
+
+      languages[m[1]] = lang_str['lang_native:' + m[1]] + ' (' + lang_str[k] + ')'
+    }
+  }
+
   def.ui_lang = {
     'name': 'UI Language',
-    'type': 'text'
+    'type': 'select',
+    'values': languages,
+    'req': true,
+    'default': ui_lang
+  }
+})
+
+register_hook('options_save', function (data) {
+  if ('ui_lang' in data && data.ui_lang !== ui_lang) {
+    location.reload()
   }
 })
