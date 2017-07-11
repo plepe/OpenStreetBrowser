11865a210310d777d4a55f9d3f9193afc375cb06
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-07-11 13:35:33 +0200

    Options: save options into session

--- src/options.php
+++ src/options.php
@@ -2,5 +2,11 @@
 function ajax_options_save($get_param, $post_param) {
   call_hooks('options_save', $post_param);
 
+  $_SESSION['options'] = $post_param;
+
   return array('success' => true);
+}
+
+if (!array_key_exists('options', $_SESSION)) {
+  $_SESSION['options'] = array();
 }
