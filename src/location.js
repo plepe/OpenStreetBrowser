6ff8730e7cf3b8f07723b0d03f6ec88c1bd31400
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-07-24 07:15:49 +0200

    Shall ipLocation be used?

--- src/location.js
+++ src/location.js
@@ -1,6 +1,11 @@
 var ipLocation = require('./ip-location')
 
 register_hook('init_callback', function (callback) {
+  if ('checkIpLocation' in config && !config.checkIpLocation) {
+    map.setView(config.defaultView, 'zoom' in config.defaultView ? config.defaultView.zoom : 14)
+    return callback()
+  }
+
   ipLocation('', function (err, ipLoc) {
     if (typeof ipLoc === 'object' && 'latitude' in ipLoc) {
       map.setView([ ipLoc.latitude, ipLoc.longitude ], 14)
