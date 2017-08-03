0cbdfb9969af6937ce76fc6bdd9ce71612c1541a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-08-03 10:39:21 +0200

    Fullscreen mode

---
+++ src/fullscreen.js
@@ -0,0 +1,23 @@
+var fullscreenControl = L.Control.extend({
+  options: {
+    position: 'topleft' 
+    //control position - allowed: 'topleft', 'topright', 'bottomleft', 'bottomright'
+  },
+  onAdd: function (map) {
+    var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control-fullscreen')
+    container.innerHTML = "<a href='#'><i class='fa fa-arrows-alt'></i></a>"
+    container.title = lang('toggle_fullscreen')
+
+    container.onclick = function () {
+      document.body.classList.toggle('fullscreen')
+      map.invalidateSize()
+      return false
+    }
+
+    return container
+  }
+})
+
+register_hook('init', function (callback) {
+  map.addControl(new fullscreenControl())
+})
