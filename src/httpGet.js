ed6ed7473d496ae84993eefde0f4275ca420e932
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-09-19 21:06:21 +0200

    Forgot file httpGet.js

---
+++ src/httpGet.js
@@ -0,0 +1,17 @@
+function httpGet (url, callback) {
+  var xhr = new XMLHttpRequest()
+  xhr.open('get', url, true)
+  xhr.responseType = 'text'
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback(null, { body: xhr.responseText })
+      } else {
+        callback(xhr.responseText)
+      }
+    }
+  }
+  xhr.send()
+}
+
+module.exports = httpGet
