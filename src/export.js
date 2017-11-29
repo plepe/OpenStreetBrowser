68ea3a460ff0e26837ad505d8a90cdc366e1390e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2017-11-29 21:25:22 +0100

    src/export.js exports Categories to thirdparty apps

---
+++ src/export.js
@@ -0,0 +1,8 @@
+require('./twigFunctions')
+require('./tagTranslations')
+require('./markers')
+
+module.exports = {
+  CategoryIndex: require('./CategoryIndex'),
+  CategoryOverpass: require('./CategoryOverpass')
+}
