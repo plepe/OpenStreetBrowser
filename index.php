5ad0fa9199cc59d5a40ded5b71a1a6c742f1affa
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-02-13 20:51:56 +0100

    favicon

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ html_export_var(array(
   <title>OpenStreetBrowser</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="manifest" href="manifest.json" />
+  <link rel="icon" type="image/png" href="img/osb-192.png" />
   <link rel="stylesheet" href="node_modules/leaflet/dist/leaflet.css" />
   <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css" />
   <link rel="stylesheet" href="node_modules/leaflet-geosearch/assets/css/leaflet.css" />
