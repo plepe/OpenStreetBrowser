3b92ec86162f08a15fc4613bb298cbd7387743d4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-01-31 10:47:48 +0100

    Locales: fix french locale

--- locales/fr.js
+++ locales/fr.js
@@ -1,7 +1,7 @@
 global.locale = {
   id: 'fr',
   moment: require('moment'),
-  osmDateFormatTemplates: require('openstreetmap-date-format/templates/en')
+  osmDateFormatTemplates: require('openstreetmap-date-format/templates/fr')
 }
 
 require('moment/locale/fr')
