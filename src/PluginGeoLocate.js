7855be88e67a4f2533ef3ab71fc9503f7212a689
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-02-24 20:33:46 +0100

    GeoLocate: unfin

--- src/PluginGeoLocate.js
+++ src/PluginGeoLocate.js
@@ -2,7 +2,7 @@ register_hook('init', function () {
   // Geo location
   L.control.locate({
     keepCurrentZoomLevel: true,
-    drawCircle: false,
+    drawCircle: true,
     drawMarker: false,
     showPopup: false
   }).addTo(map)
