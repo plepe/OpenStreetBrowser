c4b0c74489801e25c0adf7db05a6db4340c03f6f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-09-08 10:17:57 +0200

    Export: also exports Filter for CategoryOverpass

--- src/export.js
+++ src/export.js
@@ -2,6 +2,7 @@ require('./twigFunctions')
 require('./tagTranslations')
 require('./markers')
 require('./category.css')
+require('./CategoryOverpassFilter')
 
 module.exports = {
   CategoryIndex: require('./CategoryIndex'),
