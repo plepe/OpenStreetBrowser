552d7d44651ebeebb75dc87ee2952aa610a54b8b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-10-08 06:23:34 +0200

    Bugfix: default value of config option urlOpenStreetMap

--- src/defaults.php
+++ src/defaults.php
@@ -1,4 +1,4 @@
 <?php
 if (!array_key_exists('urlOpenStreetMap', $config)) {
-  $config['urlOpenStreetMap'] = 'https://www.openstreetmap.arg';
+  $config['urlOpenStreetMap'] = 'https://www.openstreetmap.org';
 }
