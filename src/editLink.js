525b161fb8d64070a9671237148ea02b0d14939a
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2020-01-31 10:35:11 +0100

    editLink: choose editor (only 'id' available)

--- src/editLink.js
+++ src/editLink.js
@@ -1,3 +1,19 @@
 module.exports = function editLink (object) {
-  return '<a target="_blank" class="editLink" href="' + global.config.urlOpenStreetMap + '/edit?editor=id&' + object.object.type + '=' + object.object.osm_id + '">' + lang('edit') + '</a>'
+  switch (global.options.editor) {
+    case 'id':
+    default:
+      return '<a target="_blank" class="editLink" href="' + global.config.urlOpenStreetMap + '/edit?editor=id&' + object.object.type + '=' + object.object.osm_id + '">' + lang('edit') + '</a>'
+  }
 }
+
+register_hook('options_form', function (def) {
+  def.editor = {
+    'name': lang('options:chooseEditor'),
+    'type': 'select',
+    'values': {
+      'id': lang('editor:id')
+    },
+    'default': 'id',
+    'weight': 5
+  }
+})
