5ae2a109e611cfd2b3171fc81ff9a95874f2558a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-02-02 17:53:10 +0100

    GeoInfo: formatUnits.coord: use module formatcoords

--- src/formatUnits.js
+++ src/formatUnits.js
@@ -1,9 +1,8 @@
 const formatUnit = require('format-unit').default
+const formatcoords = require('formatcoords')
 
 module.exports = {
   distance: value => formatUnit('length')(value)(['mm', 'cm', 'm', 'km']),
   area: value => formatUnit('area')(value)(['cm2', 'm2', 'ha', 'km2']),
-  coord: value => {
-    return value.lat.toFixed(5) + ' ' + value.lng.toFixed(5)
-  }
+  coord: value => formatcoords(value).format()
 }
