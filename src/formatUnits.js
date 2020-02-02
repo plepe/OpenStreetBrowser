b927a5eab1bb79bba8f98a2b775422b21634be52
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-02-02 17:44:03 +0100

    GeoInfo: format length and area with module format-unit

--- src/formatUnits.js
+++ src/formatUnits.js
@@ -1,12 +1,8 @@
+const formatUnit = require('format-unit').default
+
 module.exports = {
-  distance: value => {
-    return value.toFixed(0) + 'm'
-  },
-
-  area: value => {
-    return value.toFixed(0) + 'm²'
-  },
-
+  distance: value => formatUnit('length')(value)(['mm', 'cm', 'm', 'km']),
+  area: value => formatUnit('area')(value)(['cm2', 'm2', 'ha', 'km2']),
   coord: value => {
     return value.lat.toFixed(5) + ' ' + value.lng.toFixed(5)
   }
