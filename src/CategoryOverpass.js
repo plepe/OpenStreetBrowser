0e90e01ca9e0384eadf769aef6501b59a8813941
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-09-26 11:33:08 +0200

    CategoryOverpass: override layouts (with the original values)

--- src/CategoryOverpass.js
+++ src/CategoryOverpass.js
@@ -30,6 +30,27 @@ var defaultValues = {
     listMarkerSymbol: '{{ markerCircle({})|raw }}',
     preferredZoom: 16
   },
+  layouts: {
+    popup:
+      '<h1>{{ object.popupTitle|default(object.title) }}</h1>' +
+      '{% if object.popupDescription or object.description %}<div class="description">{{ object.popupDescription|default(object.description) }}</div>{% endif %}' +
+      '{% if object.popupBody or object.body %}<div class="body">{{ object.popupBody|default(object.body) }}</div>{% endif %}',
+    list:
+      '<div class="marker">' +
+      '{% if object.listMarkerSymbol or object.markerSymbol %}' +
+      '<div class="symbol">{{ object.listMarkerSymbol|default(object.markerSymbol) }}</div>' +
+      '{% elseif object.marker and object.marker.iconUrl %}' +
+      '<img class="symbol" src="{{ object.marker.iconUrl|e }}">' +
+      '{% endif %}' +
+      '{% if object.listMarkerSign or object.markerSign %}' +
+      '<div class="sign">{{ object.listMarkerSign|default(object.markerSign) }}</div>' +
+      '{% endif %}' +
+      '</div>' +
+      '<div class="content">' +
+      '<a class="title" href="{{ object.appUrl|default("#") }}">{{ object.listTitle|default(object.title) }}</a>' +
+      '{% if object.listDescription or object.description %}<div class="description">{{ object.listDescription|default(object.description) }}</div>{% endif %}' +
+      '</div>'
+  },
   queryOptions: {
   }
 }
