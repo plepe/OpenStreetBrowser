ecc9460f77840d64c8b17ce06a092aae12d4f592
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-12-11 22:11:29 +0100

    Update dependencies

--- locales/nl.js
+++ locales/nl.js
@@ -1,7 +1,7 @@
 global.locale = {
   id: 'nl',
   moment: require('moment'),
-  osmDateFormatTemplates: require('openstreetmap-date-format/templates/en')
+  osmDateFormatTemplates: require('openstreetmap-date-format/templates/nl')
 }
 
 require('moment/locale/nl')
