23e80e20e2150a250d582e836e8d4ba474abd3cc
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-03-03 22:57:12 +0100

    Markers: pass additional options to functions

--- src/markers.js
+++ src/markers.js
@@ -1,10 +1,10 @@
 const markers = require('openstreetbrowser-markers')
 var OverpassLayer = require('overpass-layer')
 
-OverpassLayer.twig.extendFunction('markerLine', (data) => OverpassLayer.twig.filters.raw(markers.line(data)))
-OverpassLayer.twig.extendFunction('markerCircle', (data) => OverpassLayer.twig.filters.raw(markers.circle(data)))
-OverpassLayer.twig.extendFunction('markerPointer', (data) => OverpassLayer.twig.filters.raw(markers.pointer(data)))
-OverpassLayer.twig.extendFunction('markerPolygon', (data) => OverpassLayer.twig.filters.raw(markers.polygon(data)))
+OverpassLayer.twig.extendFunction('markerLine', (data, options) => OverpassLayer.twig.filters.raw(markers.line(data, options)))
+OverpassLayer.twig.extendFunction('markerCircle', (data, options) => OverpassLayer.twig.filters.raw(markers.circle(data, options)))
+OverpassLayer.twig.extendFunction('markerPointer', (data, options) => OverpassLayer.twig.filters.raw(markers.pointer(data, options)))
+OverpassLayer.twig.extendFunction('markerPolygon', (data, options) => OverpassLayer.twig.filters.raw(markers.polygon(data, options)))
 
 module.exports = {
   line: markers.line,
