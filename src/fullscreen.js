217f06c799697b3f27957d35e541c8eb7a3e6986
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2025-02-21 13:30:02 +0100

    Fullscreen: use Fullscreen API (disableable via Options)

--- src/fullscreen.js
+++ src/fullscreen.js
@@ -8,8 +8,23 @@ var FullscreenControl = L.Control.extend({
     container.innerHTML = "<a href='#'><i class='fa fa-arrows-alt'></i></a>"
     container.title = lang('toggle_fullscreen')
 
+    const mapElement = document.body.querySelector('#map')
+    mapElement.addEventListener('fullscreenchange', () => {
+      if (document.fullscreenElement !== mapElement) {
+        call_hooks('fullscreen-deactivate')
+        document.body.classList.remove('fullscreen')
+      }
+    })
+
     container.onclick = function () {
       document.body.classList.toggle('fullscreen')
+
+      if (options.fullscreenMode !== 'window') {
+        document.body.classList.contains('fullscreen') ?
+          mapElement.requestFullscreen() :
+          document.exitFullscreen()
+      }
+
       map.invalidateSize()
 
       call_hooks('fullscreen-' + (document.body.classList.contains('fullscreen') ? 'activate' : 'deactivate'))
@@ -28,5 +43,17 @@ register_hook('init', function (callback) {
 register_hook('show', function (url, options) {
   if (options.showDetails) {
     document.body.classList.remove('fullscreen')
+  }
+})
+
+register_hook('options_form', (def) => {
+  def.fullscreenMode = {
+    name: lang('options:fullscreenMode'),
+    type: 'select',
+    placeholder: lang('default'),
+    values: {
+      'screen': lang('options:fullscreenMode:screen'),
+      'window': lang('options:fullscreenMode:window'),
+    }
   }
 })
