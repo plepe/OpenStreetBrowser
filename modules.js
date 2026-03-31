982ae614b9029240b34e905cb6a764f70446a89c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-03-31 21:42:24 +0200

    Use geowiki-module-fullscreen instead of own module

--- modules.js
+++ modules.js
@@ -1,4 +1,7 @@
 module.exports = [
   // Enable support for color functions
   require('geowiki-module-color'),
+
+  // Enable support for fullscreen mode
+  require('@geowiki-net/geowiki-module-fullscreen'),
 ]
