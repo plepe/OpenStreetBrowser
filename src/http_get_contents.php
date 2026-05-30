0d401a4c051fac8b9fca30f6da968b7b0432e6ff
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-05-30 17:41:45 +0200

    http_get_contents: print result headers; follow redirects

--- src/http_get_contents.php
+++ src/http_get_contents.php
@@ -6,5 +6,17 @@ function http_get_contents ($url, $options = []) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_USERAGENT, "{$config['app']['name']} ({$config['app']['url']}; {$config['app']['contact']})");
 
-  return curl_exec($ch);
+  $result = curl_exec($ch);
+
+  $request = curl_getinfo($ch);
+  if ($request['http_code'] === 301 && ($options['redirect_count'] ?? 0) < 5) {
+    $options['redirect_count'] = ($options['redirect_count'] ?? 0) + 1;
+    return http_get_contents($request['redirect_url'], $options);
+  }
+
+  Header("HTTP/1.1 {$request['http_code']}");
+  Header("Content-Type: {$request['content_type']}");
+  Header("Content-Length: {$request['download_content_length']}");
+
+  return $result;
 }
